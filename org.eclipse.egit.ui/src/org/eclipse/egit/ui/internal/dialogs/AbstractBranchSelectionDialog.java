9f57c54adaa3568bbdc9d4af70d0603777f3df02
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:53:21 +0000

    First commited as before_17855_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/AbstractBranchSelectionDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/AbstractBranchSelectionDialog.java
@@ -24,8 +24,7 @@ import java.util.List;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.repository.RepositoriesViewContentProvider;
-import org.eclipse.egit.ui.internal.repository.RepositoriesViewStyledCellLabelProvider;
-import org.eclipse.egit.ui.internal.repository.tree.AdditionalRefNode;
+import org.eclipse.egit.ui.internal.repository.RepositoriesViewLabelProvider;
 import org.eclipse.egit.ui.internal.repository.tree.AdditionalRefsNode;
 import org.eclipse.egit.ui.internal.repository.tree.LocalNode;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
@@ -38,7 +37,7 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -269,10 +268,9 @@ public abstract class AbstractBranchSelectionDialog extends TitleAreaDialog {
 		FilteredTree tree = new FilteredTree(composite, selectionModel | SWT.BORDER,
 				new PatternFilter(), true);
 		branchTree = tree.getViewer();
-		branchTree
-				.setLabelProvider(new RepositoriesViewStyledCellLabelProvider());
+		branchTree.setLabelProvider(new DelegatingStyledCellLabelProvider(
+				new RepositoriesViewLabelProvider()));
 		branchTree.setContentProvider(new RepositoriesViewContentProvider());
-		ColumnViewerToolTipSupport.enableFor(branchTree);
 
 		GridDataFactory.fillDefaults().grab(true, true).hint(500, 300).applyTo(
 				tree);
@@ -297,7 +295,7 @@ public abstract class AbstractBranchSelectionDialog extends TitleAreaDialog {
 						&& type != RepositoryTreeNodeType.ADDITIONALREF)
 					branchTree.setExpandedState(node,
 							!branchTree.getExpandedState(node));
-				else if (isOkButtonEnabled())
+				else if (getButton(Window.OK).isEnabled())
 					buttonPressed(OK);
 			}
 		});
@@ -328,7 +326,7 @@ public abstract class AbstractBranchSelectionDialog extends TitleAreaDialog {
 		// complete after the dialog is first shown. If automatic selections
 		// happen after this (making the user inputs complete), the button will
 		// be enabled.
-		setOkButtonEnabled(false);
+		getButton(Window.OK).setEnabled(false);
 
 		List<RepositoryTreeNode> roots = new ArrayList<RepositoryTreeNode>();
 		if ((settings & SHOW_LOCAL_BRANCHES) != 0)
@@ -364,30 +362,6 @@ public abstract class AbstractBranchSelectionDialog extends TitleAreaDialog {
 	}
 
 	/**
-	 * Enables the OK button. No-op in case Dialog#createButtonsForButtonBar has
-	 * been overridden and the button has not been created.
-	 * 
-	 * @param enabled
-	 * 
-	 * @see org.eclipse.jface.dialogs.Dialog#createButtonsForButtonBar(Composite)
-	 */
-	protected void setOkButtonEnabled(boolean enabled) {
-		if (getButton(Window.OK) != null)
-			getButton(Window.OK).setEnabled(enabled);
-	}
-
-	/**
-	 * Returns <code>true</code> if the OK button has been created and is
-	 * enabled.
-	 * 
-	 * @return the OK button's enabled state or <code>false</code> if the button
-	 *         has not been created.
-	 */
-	protected boolean isOkButtonEnabled() {
-		return getButton(Window.OK) != null && getButton(Window.OK).isEnabled();
-	}
-
-	/**
 	 * Set the selection to a {@link Ref} if possible
 	 *
 	 * @param refName
@@ -412,11 +386,6 @@ public abstract class AbstractBranchSelectionDialog extends TitleAreaDialog {
 				if (ref == null)
 					return false;
 				node = new RefNode(remoteBranches, repo, ref);
-			} else if (Constants.HEAD.equals(refName)) {
-				Ref ref = repo.getRef(refName);
-				if (ref == null)
-					return false;
-				node = new AdditionalRefNode(references, repo, ref);
 			} else {
 				String mappedRef = Activator.getDefault().getRepositoryUtil()
 						.mapCommitToRef(repo, refName, false);
