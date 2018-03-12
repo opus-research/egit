d0a172d0acbb809b05d667667b888781a275c00a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:17:18 +0000

    First commited as before_1244_rev5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1303,9 +1303,6 @@ public class UIText extends NLS {
 	public static String CommitDialog_AddChangeIdTooltip;
 
 	/** */
-	public static String CommitDialog_ConfigureLink;
-
-	/** */
 	public static String ConfigurationEditorComponent_AddButton;
 
 	/** */
@@ -1793,15 +1790,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String BranchSelectionDialog_Rename;
-
-	/** */
-	public static String CommitDialogPreferencePage_title;
-
-	/** */
-	public static String CommitDialogPreferencePage_hardWrapMessage;
-
-	/** */
-	public static String CommitDialogPreferencePage_hardWrapMessageTooltip;
 
 	/** */
 	public static String Decorator_exceptionMessage;
@@ -2360,7 +2348,13 @@ public class UIText extends NLS {
 	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
 
 	/** */
+	public static String CommitFileDiffViewer_CompareMenuLabel;
+
+	/** */
 	public static String CommitFileDiffViewer_MergeCommitMultiAncestorMessage;
+
+	/** */
+	public static String CommitFileDiffViewer_OpenInEditorMenuLabel;
 
 	/** */
 	public static String CommitFileDiffViewer_notContainedInCommit;
