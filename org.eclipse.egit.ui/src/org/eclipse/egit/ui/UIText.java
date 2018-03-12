e0cf17a71c92c0564391f9272e1d05d9c25ecef2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:47:51 +0000

    First commited as before_3968_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3827,9 +3827,6 @@ public class UIText extends NLS {
 	public static String StagingView_UnstagedChanges;
 
 	/** */
-	public static String StagingView_ShowFileNamesFirst;
-
-	/** */
 	public static String StagingView_StagedChanges;
 
 	/** */
@@ -3870,9 +3867,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_OpenNewCommits;
-
-	/** */
-	public static String StagingView_ColumnLayout;
 
 	/** */
 	public static String StagingView_IndexDiffReload;
