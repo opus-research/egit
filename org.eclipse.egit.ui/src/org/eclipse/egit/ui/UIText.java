d37a9531d9fc14b9f799f308478913f763699891
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:46:28 +0000

    First commited as before_3870_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2257,7 +2257,13 @@ public class UIText extends NLS {
 	public static String FetchResultDialog_title;
 
 	/** */
+	public static String FetchResultTable_collapseAll;
+
+	/** */
 	public static String FetchResultTable_counterCommits;
+
+	/** */
+	public static String FetchResultTable_expandAll;
 
 	/** */
 	public static String FetchResultTable_statusDetailCouldntLock;
@@ -2957,12 +2963,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String UIIcons_errorLoadingPluginImage;
-
-	/** */
-	public static String UIUtils_CollapseAll;
-
-	/** */
-	public static String UIUtils_ExpandAll;
 
 	/** */
 	public static String UIUtils_PressShortcutMessage;
