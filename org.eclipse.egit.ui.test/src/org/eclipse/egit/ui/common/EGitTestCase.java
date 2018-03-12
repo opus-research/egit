cc21d6aeee026777998b271554b0cebc7a0ba264
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:13:30 +0000

    First commited as before_577_rev12

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/EGitTestCase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/EGitTestCase.java
@@ -13,12 +13,9 @@ package org.eclipse.egit.ui.common;
 import org.eclipse.egit.ui.test.Eclipse;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
-import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.junit.After;
 import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
 
-@RunWith(SWTBotJunit4ClassRunner.class)
 public class EGitTestCase {
 
 	static {
