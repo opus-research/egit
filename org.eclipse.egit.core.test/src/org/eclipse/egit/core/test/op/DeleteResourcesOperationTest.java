0ae6c6ee614758b16cec5cb1af38f1c3c7a3614d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:19:03 +0000

    First commited as before_6919_rev6

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/DeleteResourcesOperationTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/DeleteResourcesOperationTest.java
@@ -17,12 +17,12 @@ import java.util.Collection;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.JobFamilies;
-import org.eclipse.egit.core.op.DeletePathsOperation;
+import org.eclipse.egit.core.op.DeleteResourcesOperation;
 import org.eclipse.egit.core.test.DualRepositoryTestCase;
 import org.eclipse.egit.core.test.TestRepository;
 import org.eclipse.jgit.lib.Constants;
@@ -30,13 +30,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class DeletePathsOperationTest extends DualRepositoryTestCase {
+public class DeleteResourcesOperationTest extends DualRepositoryTestCase {
 
 	File workdir;
 
 	IProject project;
 
-	String projectName = "DeletePathsOperationTest";
+	String projectName = "DeleteResourcesOperationTest";
 
 	@Before
 	public void setUp() throws Exception {
@@ -62,7 +62,7 @@ public class DeletePathsOperationTest extends DualRepositoryTestCase {
 	public void testDeleteResourceOfProject() throws Exception {
 		IResource resource = testUtils.addFileToProject(project, "file.txt", "Hello world 1");
 
-		deletePaths(Arrays.asList(resource.getLocation()));
+		deleteResources(Arrays.asList(resource));
 
 		File file = resource.getFullPath().toFile();
 		assertFalse("File should have been deleted", file.exists());
@@ -73,17 +73,19 @@ public class DeletePathsOperationTest extends DualRepositoryTestCase {
 		File outsideOfProject = new File(workdir, "outside-of-project.txt");
 		outsideOfProject.createNewFile();
 
-		IPath path = new Path(outsideOfProject.getAbsolutePath());
+		Path path = new Path(outsideOfProject.getAbsolutePath());
+		IWorkspaceRoot root = project.getWorkspace().getRoot();
+		IResource resource = root.getFile(path);
 
-		deletePaths(Arrays.asList(path));
+		deleteResources(Arrays.asList(resource));
 
 		assertFalse("File should have been deleted", outsideOfProject.exists());
 		Job[] indexDiffCacheJobs = Job.getJobManager().find(JobFamilies.INDEX_DIFF_CACHE_UPDATE);
 		assertTrue("Should cause an index diff cache job", indexDiffCacheJobs.length > 0);
 	}
 
-	private void deletePaths(Collection<IPath> paths) throws CoreException {
-		DeletePathsOperation operation = new DeletePathsOperation(paths);
+	private void deleteResources(Collection<IResource> resources) throws CoreException {
+		DeleteResourcesOperation operation = new DeleteResourcesOperation(resources);
 		operation.execute(null);
 	}
 }
