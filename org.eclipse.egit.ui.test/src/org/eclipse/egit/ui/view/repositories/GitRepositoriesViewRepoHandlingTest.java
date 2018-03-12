848ea6a3233afd117bc2f6363d8e0bf2b4eec18f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:46:32 +0000

    First commited as before_15799_rev1

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewRepoHandlingTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewRepoHandlingTest.java
@@ -286,7 +286,8 @@ public class GitRepositoriesViewRepoHandlingTest extends
 								.getPluginLocalizedValue("RepoViewCloneRepository.tooltip"))
 				.click();
 		SWTBotShell shell = bot.shell(UIText.GitCloneWizard_title);
-		shell.bot().tree().select("Clone URI");
+		shell.bot().tree().select("URI");
+
 		shell.bot().button("Next >").click();		// for some reason, textWithLabel doesn't seem to work
 		shell.bot()
 				.textInGroup(UIText.RepositorySelectionPage_groupLocation, 0)
