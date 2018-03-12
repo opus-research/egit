d07c36403cdc576ca76a7d2e643a6a9989539e8a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:30:50 +0000

    First commited as before_49727_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingViewLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingViewLabelProvider.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
  * Copyright (C) 2011, 2014 Bernard Leach <leachbj@bouncycastle.org> and others.
- * Copyright (C) 2015, Steven Spungin <steven@spungin.tv>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,12 +10,9 @@ package org.eclipse.egit.ui.internal.staging;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.egit.core.internal.storage.GitFileRevision;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.UIIcons;
@@ -30,13 +26,10 @@ import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.jface.viewers.DecorationOverlayIcon;
 import org.eclipse.jface.viewers.IDecoration;
-import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jgit.util.FS;
-import org.eclipse.jgit.util.RelativeDateFormatter;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
@@ -44,8 +37,7 @@ import org.eclipse.ui.model.WorkbenchLabelProvider;
 /**
  * Label provider for {@link StagingEntry} objects
  */
-public class StagingViewLabelProvider extends LabelProvider implements
-		ITableLabelProvider {
+public class StagingViewLabelProvider extends LabelProvider {
 
 	private StagingView stagingView;
 
@@ -62,24 +54,15 @@ public class StagingViewLabelProvider extends LabelProvider implements
 	private final DecorationHelper decorationHelper = new DecorationHelper(
 			Activator.getDefault().getPreferenceStore());
 
+
 	private boolean fileNameMode = false;
-
-	private boolean isStaged;
-
-	boolean showRelativeDate = false;
-
-	final private SimpleDateFormat absoluteFormatter = new SimpleDateFormat(
-			"yyyy-MM-dd HH:mm:ss"); //$NON-NLS-1$
 
 	/**
 	 * @param stagingView
-	 * @param isStaged
-	 *            true if in index, false if in working directory
 	 */
-	public StagingViewLabelProvider(StagingView stagingView, boolean isStaged) {
+	public StagingViewLabelProvider(StagingView stagingView) {
 		super();
 		this.stagingView = stagingView;
-		this.isStaged = isStaged;
 	}
 
 	/**
@@ -208,62 +191,6 @@ public class StagingViewLabelProvider extends LabelProvider implements
 			styled.append(stagingEntry.getName());
 		}
 		return styled.toString();
-	}
-
-	@Override
-	public Image getColumnImage(Object element, int columnIndex) {
-		if (columnIndex == 0) {
-			return getImage(element);
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public String getColumnText(Object element, int columnIndex) {
-		switch (columnIndex) {
-		case 0:
-			return getText(element);
-		case 1:
-			try {
-				if (element instanceof StagingFolderEntry) {
-					return null;
-				}
-				StagingEntry stagingEntry = (StagingEntry) element;
-				Date modified;
-
-				if (isStaged) {
-					IFileRevision fileRevision = GitFileRevision.inIndex(
-							stagingEntry.getRepository(),
-							stagingEntry.getPath());
-					modified = new Date(fileRevision.getTimestamp());
-				} else {
-					File file = new File(stagingEntry.getRepository()
-							.getWorkTree(), stagingEntry.getPath());
-					modified = new Date(file.lastModified());
-				}
-
-				if (showRelativeDate) {
-					return RelativeDateFormatter.format(modified);
-				}
-				else {
-					return absoluteFormatter.format(modified);
-				}
-			} catch (Exception e) {
-				Activator.handleError(e.getCause().getMessage(),
-						e.getCause(), false);
-				return null;
-			}
-		default:
-			return null;
-		}
-	}
-
-	/**
-	 * @param showRelativeDate
-	 */
-	public void setShowRelativeDate(boolean showRelativeDate) {
-		this.showRelativeDate = showRelativeDate;
 	}
 
 }
