fc185c24ba5ea3049b6d4a0f9dde8e328e4d4632
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:57:09 +0000

    First commited as before_4627_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingEntry.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingEntry.java
@@ -69,13 +69,6 @@ public class StagingEntry implements IAdaptable {
 	}
 
 	/**
-	 * @return the repository for this node
-	 */
-	public Repository getRepisotry() {
-		return repository;
-	}
-
-	/**
 	 * @return the state for this node
 	 */
 	public State getState() {
