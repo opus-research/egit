eae765a863918c013c0dafbcd46cb5ce55bb12af
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:36:04 +0000

    First commited as before_11868_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/SimplePushAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/SimplePushAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, 2013 Mathias Kinzler <mathias.kinzler@sap.com> and others.
+ * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,14 +9,13 @@
 package org.eclipse.egit.ui.internal.actions;
 
 /**
- * Action for "Push to Upstream" or "Push Branch..." if not configured
+ * Action for "simple" push
  */
-public class PushUpstreamOrBranchAction extends RepositoryAction {
+public class SimplePushAction extends RepositoryAction {
 	/**
 	 *
 	 */
-	public PushUpstreamOrBranchAction() {
-		super(ActionCommands.SIMPLE_PUSH_ACTION,
-				new PushUpstreamOrBranchActionHandler());
+	public SimplePushAction() {
+		super(ActionCommands.SIMPLE_PUSH_ACTION, new SimplePushActionHandler());
 	}
 }
