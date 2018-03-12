45a633a1b609f834ec40a8a0732f4dc0fbcac457
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:15:14 +0000

    First commited as before_888_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/ConfigureRemoteWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/ConfigureRemoteWizard.java
@@ -16,6 +16,7 @@ import java.net.URISyntaxException;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.components.RefSpecPage;
 import org.eclipse.egit.ui.internal.components.RepositorySelection;
+import org.eclipse.egit.ui.internal.components.RepositorySelectionPage;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jgit.lib.Repository;
@@ -25,9 +26,13 @@ import org.eclipse.jgit.transport.URIish;
 import org.eclipse.osgi.util.NLS;
 
 /**
- *
+ * Allows to configure a "Remote".
+ * <p>
+ * Asks for a name and whether to configure fetch, push, or both. Depending on
+ * the user's decision about what to configure, the fetch, push, or both
+ * configurations are performed.
  */
-class ConfigureRemoteWizard extends Wizard {
+public class ConfigureRemoteWizard extends Wizard {
 
 	final RepositoryConfig myConfiguration;
 
@@ -155,6 +160,16 @@ class ConfigureRemoteWizard extends Wizard {
 
 		try {
 			myConfiguration.save();
+
+			// Save the uris in the RepositorySelectionPage preferences
+
+			for (URIish uri: myRemoteConfiguration.getURIs())
+				RepositorySelectionPage.saveUriInPrefs(uri.toPrivateString());
+
+			for (URIish uri: myRemoteConfiguration.getPushURIs())
+				RepositorySelectionPage.saveUriInPrefs(uri.toPrivateString());
+
+
 			return true;
 		} catch (IOException e) {
 			// TODO better Exception handling
