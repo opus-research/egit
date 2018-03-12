4dbacf3956c970a678dcfb92ca32230febfee6c2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:51:59 +0000

    First commited as before_4239_rev5

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
@@ -21,10 +21,12 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.Activator;
@@ -53,6 +55,8 @@ import org.eclipse.team.core.Team;
  *
  */
 public class IndexDiffCacheEntry {
+
+	private static final String GITIGNORE_NAME = ".gitignore"; //$NON-NLS-1$
 
 	private static final int RESOURCE_LIST_UPDATE_LIMIT = 1000;
 
@@ -140,9 +144,14 @@ public class IndexDiffCacheEntry {
 	private void scheduleReloadJob() {
 		if (reloadJob != null)
 			reloadJob.cancel();
+		if (!checkRepository())
+			return;
 		reloadJob = new Job(getReloadJobName()) {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
+				waitForWorkspaceLock();
+				if (monitor.isCanceled())
+					return Status.CANCEL_STATUS;
 				lock.lock();
 				try {
 					long startTime = System.currentTimeMillis();
@@ -162,6 +171,14 @@ public class IndexDiffCacheEntry {
 					}
 					notifyListeners();
 					return Status.OK_STATUS;
+				} catch (RuntimeException e) {
+					if (GitTraceLocation.INDEXDIFFCACHE.isActive()) {
+						GitTraceLocation.getTrace().trace(
+								GitTraceLocation.INDEXDIFFCACHE.getLocation(),
+								"Calculating IndexDiff failed", e); //$NON-NLS-1$
+					}
+					scheduleReloadJob();
+					return Status.OK_STATUS;
 				} finally {
 					lock.unlock();
 				}
@@ -178,11 +195,42 @@ public class IndexDiffCacheEntry {
 		reloadJob.schedule();
 	}
 
+	private boolean checkRepository() {
+		if (Activator.getDefault() == null)
+			return false;
+		if (!repository.getDirectory().exists())
+			return false;
+		return true;
+	}
+
+	private void waitForWorkspaceLock() {
+		// Wait for the workspace lock to avoid starting the calculation
+		// of an IndexDiff while the workspace changes (e.g. due to a
+		// branch switch).
+		// The index diff calculation jobs do not lock the workspace
+		// during execution to avoid blocking the workspace.
+		try {
+			ResourcesPlugin.getWorkspace().run(new IWorkspaceRunnable() {
+
+				public void run(IProgressMonitor monitor) throws CoreException {
+					// empty
+				}
+			}, new NullProgressMonitor());
+		} catch (CoreException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	private void scheduleUpdateJob(final Collection<String> filesToUpdate,
 			final Collection<IFile> fileResourcesToUpdate) {
+		if (!checkRepository())
+			return;
 		Job job = new Job(getReloadJobName()) {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
+				waitForWorkspaceLock();
+				if (monitor.isCanceled())
+					return Status.CANCEL_STATUS;
 				lock.lock();
 				try {
 					long startTime = System.currentTimeMillis();
@@ -204,6 +252,14 @@ public class IndexDiffCacheEntry {
 								.toString());
 					}
 					notifyListeners();
+					return Status.OK_STATUS;
+				} catch (RuntimeException e) {
+					if (GitTraceLocation.INDEXDIFFCACHE.isActive()) {
+						GitTraceLocation.getTrace().trace(
+								GitTraceLocation.INDEXDIFFCACHE.getLocation(),
+								"Calculating IndexDiff failed", e); //$NON-NLS-1$
+					}
+					scheduleReloadJob();
 					return Status.OK_STATUS;
 				} finally {
 					lock.unlock();
@@ -278,19 +334,24 @@ public class IndexDiffCacheEntry {
 			public void resourceChanged(IResourceChangeEvent event) {
 				final Collection<String> filesToUpdate = new HashSet<String>();
 				final Collection<IFile> fileResourcesToUpdate = new HashSet<IFile>();
+				final boolean[] gitIgnoreChanged = new boolean[1];
+				gitIgnoreChanged[0] = false;
 
 				try {
 					event.getDelta().accept(new IResourceDeltaVisitor() {
 						public boolean visit(IResourceDelta delta)
 								throws CoreException {
+							final IResource resource = delta.getResource();
+							// Don't include ignored resources
+							if (Team.isIgnoredHint(resource))
+								return false;
+
 							// If the file has changed but not in a way that we
 							// care about (e.g. marker changes to files) then
 							// ignore
 							if (delta.getKind() == IResourceDelta.CHANGED
 									&& (delta.getFlags() & INTERESTING_CHANGES) == 0)
 								return true;
-
-							final IResource resource = delta.getResource();
 
 							// skip any non-FILE resources
 							if (resource.getType() != IResource.FILE)
@@ -305,9 +366,10 @@ public class IndexDiffCacheEntry {
 								// Ignore the change
 								return true;
 
-							// Don't include ignored resources
-							if (Team.isIgnoredHint(resource))
+							if (resource.getName().equals(GITIGNORE_NAME)) {
+								gitIgnoreChanged[0] = true;
 								return false;
+							}
 
 							String repoRelativePath = mapping
 									.getRepoRelativePath(resource);
@@ -322,7 +384,9 @@ public class IndexDiffCacheEntry {
 					return;
 				}
 
-				if (!filesToUpdate.isEmpty())
+				if (gitIgnoreChanged[0] || indexDiffData == null)
+					scheduleReloadJob();
+				else if (!filesToUpdate.isEmpty())
 					if (filesToUpdate.size() < RESOURCE_LIST_UPDATE_LIMIT)
 						scheduleUpdateJob(filesToUpdate, fileResourcesToUpdate);
 					else
