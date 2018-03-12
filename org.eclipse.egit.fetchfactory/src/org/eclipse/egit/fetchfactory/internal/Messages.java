1612600750455b11c4b26e8e0f00a6d0b40349f3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:20:59 +0000

    First commited as before_1737_rev2

--- org.eclipse.egit.fetchfactory/src/org/eclipse/egit/fetchfactory/internal/Messages.java
+++ org.eclipse.egit.fetchfactory/src/org/eclipse/egit/fetchfactory/internal/Messages.java
@@ -16,7 +16,9 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.releng.git.fetch.internal.messages";//$NON-NLS-1$
 
 	public static String error_incorrectDirectoryEntry;
+
 	public static String error_incorrectDirectoryEntryKeyValue;
+
 	public static String error_directoryEntryRequiresRepo;
 
 	static {
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1045,6 +1045,24 @@ public class UIText extends NLS {
 	public static String HistoryPreferencePage_title;
 
 	/** */
+	public static String PullCurrentBranchActionHandler_PullCanceledMessage;
+
+	/** */
+	public static String PullCurrentBranchActionHandler_PullCanceledTitle;
+
+	/** */
+	public static String PullCurrentBranchActionHandler_PullJobname;
+
+	/** */
+	public static String PullResultDialog_DialogTitle;
+
+	/** */
+	public static String PullResultDialog_FetchResultGroupHeader;
+
+	/** */
+	public static String PullResultDialog_MergeResultGroupHeader;
+
+	/** */
 	public static String PushAction_wrongURIDescription;
 
 	/** */
@@ -1786,6 +1804,12 @@ public class UIText extends NLS {
 	public static String DecoratorPreferencesPage_iconsShowAssumeValid;
 
 	/** */
+	public static String DeleteBranchCommand_CannotDeleteCheckedOutBranch;
+
+	/** */
+	public static String DeleteBranchCommand_UnmergedData;
+
+	/** */
 	public static String IgnoreActionHandler_addToGitignore;
 
 	/** */
@@ -2197,6 +2221,12 @@ public class UIText extends NLS {
 	public static String CreateTagDialog_CreateTagOnCommitTitle;
 
 	/** */
+	public static String CreateTagDialog_GetTagJobName;
+
+	/** */
+	public static String CreateTagDialog_LoadingMessageText;
+
+	/** */
 	public static String CreateTagDialog_Message;
 
 	/** */
@@ -2229,6 +2259,12 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitActionHandler_repository;
+
+	/** */
+	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
+
+	/** */
+	public static String CommitFileDiffViewer_MergeCommitMultiAncestorMessage;
 
 	/** */
 	public static String CommitFileDiffViewer_notContainedInCommit;
