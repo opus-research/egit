ed46ff0b09309279f9b0b8063cc5313147049473
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:51:54 +0000

    First commited as before_68567_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchDialog.java
@@ -42,7 +42,6 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.RepositoryCache.FileKey;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FileUtils;
@@ -403,41 +402,33 @@ public class RepositorySearchDialog extends WizardPage {
 	}
 
 	private void findGitDirsRecursive(File root, Set<File> strings,
-			IProgressMonitor monitor, boolean lookForNestedRepositories,
-			boolean firstCall) {
+			IProgressMonitor monitor, boolean lookForNestedRepositories) {
 
 		if (!root.exists() || !root.isDirectory()) {
 			return;
 		}
+		File[] children = root.listFiles();
+		// simply ignore null
+		if (children == null)
+			return;
 
-		// check the root first
-		File resolved = FileKey.resolve(root, FS.DETECTED);
-		if (resolved != null) {
-			strings.add(resolved.getAbsoluteFile());
-			monitor.setTaskName(NLS.bind(
-					UIText.RepositorySearchDialog_RepositoriesFound_message,
-					Integer.valueOf(strings.size())));
-		}
-
-		// check nested only if enabled or for first call and we are not in
-		// private git folder ".git" itself
-		if ((lookForNestedRepositories || firstCall)
-				&& !root.equals(resolved)) {
-			File[] children = root.listFiles();
-			// simply ignore null
-			if (children == null)
+		for (File child : children) {
+			if (monitor.isCanceled())
 				return;
-			for (File child : children) {
-				if (monitor.isCanceled())
-					return;
-				// skip files and .git subfolders of root, because already added
-				// above, see 'check the root first'
-				if (child.isDirectory()
-						&& !child.equals(new File(root, Constants.DOT_GIT))) {
-					monitor.subTask(child.getPath());
-					findGitDirsRecursive(child, strings, monitor,
-							lookForNestedRepositories, false);
-				}
+			if (!child.isDirectory())
+				continue;
+
+			File resolved = FileKey.resolve(child, FS.DETECTED);
+			if (resolved != null) {
+				strings.add(resolved.getAbsoluteFile());
+				monitor.setTaskName(NLS.bind(
+						UIText.RepositorySearchDialog_RepositoriesFound_message,
+						Integer.valueOf(strings.size())));
+			}
+			else if (lookForNestedRepositories) {
+				monitor.subTask(child.getPath());
+				findGitDirsRecursive(child, strings, monitor,
+						lookForNestedRepositories);
 			}
 		}
 	}
@@ -478,7 +469,7 @@ public class RepositorySearchDialog extends WizardPage {
 						IProgressMonitor.UNKNOWN);
 				try {
 					findGitDirsRecursive(file, directories, monitor,
-							lookForNested, true);
+							lookForNested);
 				} catch (Exception ex) {
 					throw new InvocationTargetException(ex);
 				}
