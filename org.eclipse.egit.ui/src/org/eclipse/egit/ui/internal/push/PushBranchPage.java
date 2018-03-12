0b77eebc717db5c09fb15e1ecfe46cc49aa42f63
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:11:58 +0000

    First commited as before_30929_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushBranchPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushBranchPage.java
@@ -36,7 +36,7 @@ import org.eclipse.jface.layout.RowLayoutFactory;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.jgit.lib.BranchConfig;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
@@ -334,9 +334,9 @@ public class PushBranchPage extends WizardPage {
 		remoteSelectionCombo.setItems(remoteConfigs);
 		if (this.ref != null) {
 			String branchName = Repository.shortenRefName(this.ref.getName());
-			BranchConfig branchConfig = new BranchConfig(
-					repository.getConfig(), branchName);
-			String remoteName = branchConfig.getRemote();
+			String remoteName = repository.getConfig().getString(
+					ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
+					ConfigConstants.CONFIG_KEY_REMOTE);
 			if (remoteName != null) {
 				for (RemoteConfig rc : remoteConfigs) {
 					if (remoteName.equals(rc.getName()))
@@ -352,12 +352,14 @@ public class PushBranchPage extends WizardPage {
 	private void setDefaultUpstreamConfig() {
 		if (this.ref != null) {
 			String branchName = Repository.shortenRefName(ref.getName());
-			BranchConfig branchConfig = new BranchConfig(
-					repository.getConfig(), branchName);
-			boolean alreadyConfigured = branchConfig.getMerge() != null;
+			boolean alreadyConfigured = repository.getConfig()
+					.getSubsections(ConfigConstants.CONFIG_BRANCH_SECTION)
+					.contains(branchName);
 			UpstreamConfig config;
 			if (alreadyConfigured) {
-				boolean rebase = branchConfig.isRebase();
+				boolean rebase = repository.getConfig().getBoolean(
+						ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
+						ConfigConstants.CONFIG_KEY_REBASE, false);
 				config = rebase ? UpstreamConfig.REBASE : UpstreamConfig.MERGE;
 			} else {
 				config = UpstreamConfig.getDefault(repository, Constants.R_REMOTES
@@ -429,9 +431,15 @@ public class PushBranchPage extends WizardPage {
 			StoredConfig config = repository.getConfig();
 			String branchName = Repository.shortenRefName(ref.getName());
 
-			BranchConfig branchConfig = new BranchConfig(config, branchName);
-			String merge = branchConfig.getMerge();
-			if (!branchConfig.isRemoteLocal() && merge != null
+			String remoteName = config.getString(
+					ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
+					ConfigConstants.CONFIG_KEY_REMOTE);
+			boolean isLocal = ".".equals(remoteName); //$NON-NLS-1$
+
+			String merge = config.getString(
+					ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
+					ConfigConstants.CONFIG_KEY_MERGE);
+			if (!isLocal && merge != null
 					&& merge.startsWith(Constants.R_HEADS))
 				return Repository.shortenRefName(merge);
 
@@ -450,22 +458,25 @@ public class PushBranchPage extends WizardPage {
 	}
 
 	private boolean hasDifferentUpstreamConfiguration() {
+		StoredConfig config = repository.getConfig();
 		String branchName = Repository.shortenRefName(ref.getName());
-		BranchConfig branchConfig = new BranchConfig(repository.getConfig(),
-				branchName);
 
-		String remote = branchConfig.getRemote();
+		String remote = config.getString(ConfigConstants.CONFIG_BRANCH_SECTION,
+				branchName, ConfigConstants.CONFIG_KEY_REMOTE);
 		// No upstream config -> don't show warning
 		if (remote == null)
 			return false;
 		if (!remote.equals(remoteConfig.getName()))
 			return true;
 
-		String merge = branchConfig.getMerge();
+		String merge = config.getString(ConfigConstants.CONFIG_BRANCH_SECTION,
+				branchName, ConfigConstants.CONFIG_KEY_MERGE);
 		if (merge == null || !merge.equals(getFullRemoteReference()))
 			return true;
 
-		boolean rebase = branchConfig.isRebase();
+		boolean rebase = config.getBoolean(
+				ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
+				ConfigConstants.CONFIG_KEY_REBASE, false);
 		if (rebase != isRebaseSelected())
 			return true;
 
