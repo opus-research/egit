3eccf75babfca66fc5d818de25ff51d61f8e6ef0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:35:00 +0000

    First commited as before_3010_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/branch/BranchOperationUI.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/branch/BranchOperationUI.java
@@ -178,28 +178,13 @@ public class BranchOperationUI {
 	}
 
 	private void showResultDialog() {
-		String displayedRef = createDisplayedRef();
-		BranchResultDialog.show(bop.getResult(), repository, displayedRef);
+		BranchResultDialog.show(bop.getResult(), repository, refName);
 		try {
 			if (ObjectId.isId(repository.getFullBranch()) && bop.getResult().getStatus() == CheckoutResult.Status.OK)
 				showDetachedHeadWarning();
 		} catch (IOException e) {
 			// Don't show warning then.
 		}
-	}
-
-	private String createDisplayedRef() {
-		String refToCheckOut = ""; //$NON-NLS-1$
-		if (refName != null)
-			refToCheckOut = refName;
-		else if (commitId != null) {
-			if (commitId != null)
-				refToCheckOut = commitId.getName().substring(0, 7)+ "... "; //$NON-NLS-1$
-		}
-		else
-			throw new IllegalStateException(
-					"Both refName and commitId must not be null"); //$NON-NLS-1$
-		return refToCheckOut;
 	}
 
 	private void showDetachedHeadWarning() {
