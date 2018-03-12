4280be77dff18441a8236d9b8dc9568b280064ff
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:17:19 +0000

    First commited as before_6479_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/DecoratableResourceAdapter.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/DecoratableResourceAdapter.java
@@ -69,6 +69,7 @@ class DecoratableResourceAdapter extends DecoratableResource {
 				break;
 			case IResource.PROJECT:
 				tracked = true;
+				//$FALL-THROUGH$
 			case IResource.FOLDER:
 				extractContainerProperties();
 				break;
@@ -148,8 +149,10 @@ class DecoratableResourceAdapter extends DecoratableResource {
 		// locally modified / untracked
 		Set<String> modified = indexDiffData.getModified();
 		Set<String> untracked = indexDiffData.getUntracked();
+		Set<String> missing = indexDiffData.getMissing();
 		dirty = containsPrefix(modified, repoRelativePath)
-				|| containsPrefix(untracked, repoRelativePath);
+				|| containsPrefix(untracked, repoRelativePath)
+				|| containsPrefix(missing, repoRelativePath);
 	}
 
 	private String makeRepoRelative(IResource res) {
