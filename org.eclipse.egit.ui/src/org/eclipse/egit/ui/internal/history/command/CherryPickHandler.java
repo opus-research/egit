ffc8ada024f72b39f8b3f5257eeb89c68a24ca0a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:32:58 +0000

    First commited as before_2888_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
@@ -17,7 +17,6 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.api.CherryPickCommand;
-import org.eclipse.jgit.api.CherryPickResult;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -35,9 +34,8 @@ public class CherryPickHandler extends AbstractHistoryCommandHandler {
 		Git git = new Git(repo);
 		try {
 			cherryPick = git.cherryPick().include(commit.getId());
-			CherryPickResult res = cherryPick.call();
-			newHead = res.getNewHead();
-			if (newHead != null && res.getCherryPickedRefs().isEmpty())
+			newHead = cherryPick.call();
+			if (newHead != null && cherryPick.getCherryPickedRefs().isEmpty())
 				MessageDialog.openWarning(getPart(event).getSite().getShell(),
 						UIText.CherryPickHandler_NoCherryPickPerformedTitle,
 						UIText.CherryPickHandler_NoCherryPickPerformedMessage);
