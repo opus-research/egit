cb65595e0340a6d830d147189e32fde7f537fe53
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:29:30 +0000

    First commited as before_48542_rev3

--- org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/factories/GitFlowAdapterFactory.java
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/factories/GitFlowAdapterFactory.java
@@ -10,32 +10,28 @@ package org.eclipse.egit.gitflow.ui.internal.factories;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdapterFactory;
-import static org.eclipse.egit.core.project.RepositoryMapping.getMapping;
-import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
+import org.eclipse.core.runtime.PlatformObject;
+import org.eclipse.egit.core.internal.Utils;
+import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.team.ui.history.IHistoryPage;
-import org.eclipse.team.ui.history.IHistoryView;
 
 /**
  * Get JGit repository for element selected in Git Flow UI.
  */
 public class GitFlowAdapterFactory implements IAdapterFactory {
+	@SuppressWarnings("unchecked")
 	@Override
-	public Repository getAdapter(Object adaptableObject, Class adapterType) {
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
 		Repository repository = null;
 		if (adaptableObject instanceof IResource) {
 			IResource resource = (IResource) adaptableObject;
-			repository = getRepository(resource);
-		} else if (adaptableObject instanceof IHistoryView) {
-			IHistoryView historyView = (IHistoryView) adaptableObject;
-			IHistoryPage historyPage = historyView.getHistoryPage();
-			Object input = historyPage.getInput();
-			if (input instanceof RepositoryNode) {
-				RepositoryNode node = (RepositoryNode) input;
-				repository = node.getRepository();
-			} else if (input instanceof IResource) {
-				repository = getRepository((IResource) input);
-			}
+			RepositoryMapping repositoryMapping = RepositoryMapping
+					.getMapping(resource.getProject());
+			repository = repositoryMapping.getRepository();
+		} else if (adaptableObject instanceof PlatformObject) {
+			PlatformObject platformObject = (PlatformObject) adaptableObject;
+			repository = Utils.getAdapter(platformObject, Repository.class);
 		} else {
 			throw new IllegalStateException();
 		}
@@ -43,12 +39,10 @@ public class GitFlowAdapterFactory implements IAdapterFactory {
 		return repository;
 	}
 
-	private Repository getRepository(IResource resource) {
-		return getMapping(resource.getProject()).getRepository();
-	}
-
+	@SuppressWarnings("unchecked")
 	@Override
 	public Class[] getAdapterList() {
-		return new Class[] { IResource.class, IHistoryView.class };
+		return new Class[] { IResource.class, RepositoryTreeNode.class };
 	}
+
 }
