13d0452c3e722f55591c1d52906f6ff492abfbb5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:55:41 +0000

    First commited as before_18791_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1713,12 +1713,6 @@ public class UIText extends NLS {
 	public static String RefSpecPage_annotatedTagsNoTags;
 
 	/** */
-	public static String RefSpecWizard_pushTitle;
-
-	/** */
-	public static String RefSpecWizard_fetchTitle;
-
-	/** */
 	public static String QuickDiff_failedLoading;
 
 	/** */
@@ -3976,7 +3970,7 @@ public class UIText extends NLS {
 	public static String GitBranchSynchronizeWizardPage_description;
 
 	/** */
-	public static String GitBranchSynchronizeWizardPage_repository;
+	public static String GitBranchSynchronizeWizardPage_repositories;
 
 	/** */
 	public static String GitBranchSynchronizeWizardPage_destination;
