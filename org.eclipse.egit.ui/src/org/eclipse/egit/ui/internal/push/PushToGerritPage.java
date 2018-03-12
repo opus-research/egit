300ba454b1e1271dc90e8883280d83cc9436f1f8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:55:03 +0000

    First commited as before_71188_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushToGerritPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushToGerritPage.java
@@ -59,8 +59,10 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbenchCommandConstants;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * Push the current HEAD to Gerrit
@@ -259,8 +261,18 @@ class PushToGerritPage extends WizardPage {
 					}
 				}
 			});
-			PushResultDialog.show(repository, result[0],
-					op.getDestinationString(), false, false);
+			getShell().getDisplay().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					Shell shell = PlatformUI.getWorkbench()
+							.getActiveWorkbenchWindow().getShell();
+					PushResultDialog dlg = new PushResultDialog(shell,
+							repository, result[0], op.getDestinationString(),
+							false);
+					dlg.showConfigureButton(false);
+					dlg.open();
+				}
+			});
 			storeLastUsedUri(uriCombo.getText());
 			storeLastUsedBranch(branchText.getText());
 		} catch (URISyntaxException e) {
