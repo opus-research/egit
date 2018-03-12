65a009cde21cde42b11299c378098d9527321881
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:55:55 +0000

    First commited as before_18897_rev7

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -43,12 +43,6 @@ public class UIText extends NLS {
 	public static String AbstractRebaseCommand_DialogTitle;
 
 	/** */
-	public static String AbstractRebaseCommandHandler_cleanupDialog_text;
-
-	/** */
-	public static String AbstractRebaseCommandHandler_cleanupDialog_title;
-
-	/** */
 	public static String AbstractReflogCommandHandler_NoInput;
 
 	/** */
