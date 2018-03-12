3dfe0d52c1dfa96564c727676340dc9849ca83af
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:27:40 +0000

    First commited as before_2372_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitImportWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitImportWizard.java
@@ -26,7 +26,6 @@ import org.eclipse.egit.core.op.ConnectProviderOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIIcons;
 import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.internal.ConfigurationChecker;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
@@ -66,7 +65,6 @@ public class GitImportWizard extends Wizard implements ProjectCreator,
 				.getProjects();
 		selectRepoPage.setWizard(this);
 		setNeedsProgressMonitor(true);
-		ConfigurationChecker.checkConfiguration();
 	}
 
 	@Override
