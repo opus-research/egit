35dc27c3446ba02a05f8ca3fa091789ca730a98c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:54:38 +0000

    First commited as before_4417_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSyncCache.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSyncCache.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org> and others.
+ * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,8 +9,6 @@
 package org.eclipse.egit.core.synchronize;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,10 +26,7 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
 import org.eclipse.jgit.treewalk.filter.NotIgnoredFilter;
-import org.eclipse.jgit.treewalk.filter.PathFilterGroup;
-import org.eclipse.jgit.treewalk.filter.TreeFilter;
 
 /**
  * Simple and thin tree cache for git meta data about resources in repository.
@@ -42,14 +37,7 @@ class GitSyncCache {
 
 	public static GitSyncCache getAllData(GitSynchronizeDataSet gsds,
 			IProgressMonitor monitor) {
-		return getAllData(gsds, Collections.<String> emptyList(), monitor);
-	}
-
-	public static GitSyncCache getAllData(GitSynchronizeDataSet gsds,
-			Collection<String> paths, IProgressMonitor monitor) {
 		GitSyncCache cache = new GitSyncCache();
-		TreeFilter filter = paths.isEmpty() ? null : PathFilterGroup
-				.createFromStrings(paths);
 
 		SubMonitor m = SubMonitor.convert(monitor, gsds.size());
 		for (GitSynchronizeData gsd : gsds) {
@@ -59,15 +47,7 @@ class GitSyncCache {
 			GitSyncObjectCache repoCache = cache
 					.put(repo, baseTree, remoteTree);
 
-			TreeFilter gsdFilter = gsd.getPathFilter();
-			if (filter == null)
-				loadDataFromGit(gsd, gsdFilter, repoCache);
-			else if (gsdFilter == null)
-				loadDataFromGit(gsd, filter, repoCache);
-			else
-				loadDataFromGit(gsd, AndTreeFilter.create(filter, gsdFilter),
-						repoCache);
-
+			loadDataFromGit(gsd, repoCache);
 			m.worked(1);
 		}
 		m.done();
@@ -76,21 +56,17 @@ class GitSyncCache {
 	}
 
 	private static void loadDataFromGit(GitSynchronizeData gsd,
-			TreeFilter filter, GitSyncObjectCache repoCache) {
+			GitSyncObjectCache repoCache) {
 		Repository repo = gsd.getRepository();
 		TreeWalk tw = new TreeWalk(repo);
-		if (filter != null)
-			tw.setFilter(filter);
+		if (gsd.getPathFilter() != null)
+			tw.setFilter(gsd.getPathFilter());
 
 		try {
 			// setup local tree
 			if (gsd.shouldIncludeLocal()) {
 				tw.addTree(new FileTreeIterator(repo));
-				if (filter != null)
-					tw.setFilter(AndTreeFilter.create(filter,
-							new NotIgnoredFilter(0)));
-				else
-					tw.setFilter(new NotIgnoredFilter(0));
+				tw.setFilter(new NotIgnoredFilter(0));
 			} else if (gsd.getSrcRevCommit() != null)
 				tw.addTree(gsd.getSrcRevCommit().getTree());
 			else
