8293c995ae61827c7cfe0495ba4edbd18661999b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:14:18 +0000

    First commited as before_730_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
@@ -1570,9 +1570,6 @@ public class RepositoriesView extends ViewPart implements ISelectionProvider,
 	 */
 	private void scheduleRefresh() {
 
-		if (scheduledJob != null && scheduledJob.getState() == Job.RUNNING)
-			return;
-
 		Job job = new Job("Refreshing Git Repositories view") { //$NON-NLS-1$
 
 			@SuppressWarnings("unchecked")
