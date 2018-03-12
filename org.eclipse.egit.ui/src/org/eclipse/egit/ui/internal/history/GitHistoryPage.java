d1893865ef5e7cd19f957a4ef9eec5e949263cfc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:11:44 +0000

    First commited as before_5489_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -813,9 +813,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			revInfoSplit.setMaximizedControl(commentViewer.getControl());
 		} else if (!showComment && showFiles) {
 			graphDetailSplit.setMaximizedControl(null);
-			// the parent of the control!
-			revInfoSplit.setMaximizedControl(fileViewer.getControl()
-					.getParent());
+			revInfoSplit.setMaximizedControl(fileViewer.getControl());
 		} else if (!showComment && !showFiles)
 			graphDetailSplit.setMaximizedControl(graph.getControl());
 		if (showFindToolbar)
@@ -1655,8 +1653,6 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 
 	private void createNewWalk(Repository db, AnyObjectId headId) {
 		currentHeadId = headId;
-		if (currentWalk != null)
-			currentWalk.release();
 		currentWalk = new SWTWalk(db);
 		try {
 			if (store
@@ -1685,9 +1681,8 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 				markStartAllRefs(Constants.R_TAGS);
 			}
 			if (store
-					.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS)) {
+					.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS))
 				markStartAdditionalRefs();
-			}
 			if (store
 					.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_NOTES))
 				markStartAllRefs(Constants.R_NOTES);
@@ -1779,13 +1774,13 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 	}
 
 	private void scheduleNewGenerateHistoryJob() {
-		final SWTCommitList list = new SWTCommitList(graph.getControl());
-		list.source(currentWalk);
-		final GenerateHistoryJob rj = new GenerateHistoryJob(this, list);
+		final GenerateHistoryJob rj = new GenerateHistoryJob(this,
+				graph.getControl(), currentWalk);
 		rj.setRule(this);
 		rj.addJobChangeListener(new JobChangeAdapter() {
 			@Override
 			public void done(final IJobChangeEvent event) {
+				rj.getWalk().release();
 				final Control graphctl = graph.getControl();
 				if (job != rj || graphctl.isDisposed())
 					return;
