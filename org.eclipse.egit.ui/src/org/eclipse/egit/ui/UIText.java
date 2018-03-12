7bfd7059f27bd693879da0d0e92ce43fd3386340
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:17:06 +0000

    First commited as before_1210_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -166,9 +166,6 @@ public class UIText extends NLS {
 	public static String GenerateHistoryJob_errorComputingHistory;
 
 	/** */
-	public static String EgitUiUtils_CouldNotOpenEditorMessage;
-
-	/** */
 	public static String ExistingOrNewPage_CreateButton;
 
 	/** */
