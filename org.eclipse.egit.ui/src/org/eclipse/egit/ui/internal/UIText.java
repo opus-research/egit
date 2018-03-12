3aae8d0530afa46d42d7514c1adee84d146b7dbe
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:18:52 +0000

    First commited as before_37622_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -3013,9 +3013,6 @@ public class UIText extends NLS {
 	public static String BranchOperationUI_RunningLaunchTitle;
 
 	/** */
-	public static String BranchOperationUI_SearchLaunchConfiguration;
-
-	/** */
 	public static String BranchRenameDialog_Message;
 
 	/** */
