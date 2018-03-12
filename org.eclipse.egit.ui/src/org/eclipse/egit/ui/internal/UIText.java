f411ecbd4dc3c3d5dec7288c27233a3b10fd6755
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:22:40 +0000

    First commited as before_42168_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -5045,9 +5045,6 @@ public class UIText extends NLS {
 	public static String StagingView_Refresh;
 
 	/** */
-	public static String StagingView_GetRepo;
-
-	/** */
 	public static String StagingView_ReplaceWith;
 
 	/** */
