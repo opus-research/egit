4e9905b1e3fec421d534e61f66a047f0836af032
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:04:47 +0000

    First commited as before_5018_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -1014,8 +1014,6 @@ public class RepositorySelectionPage extends WizardPage implements IRepositorySe
 	public GitRepositoryInfo getGitRepositoryInfo() {
 		GitRepositoryInfo info = new GitRepositoryInfo(uri.toString());
 		info.setCredentials(user, password);
-		info.setShouldSaveCredentialsInSecureStore(true);
-		uriProposalHandler.updateProposals();
 		return info;
 	}
 
