c2f3b8af233e3d39ba01e3e0a738bf8a4d1259ee
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:12:29 +0000

    First commited as before_319_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -108,12 +108,6 @@ public class UIText extends NLS {
 	public static String GitCloneWizard_errorCannotCreate;
 
 	/** */
-	public static String GitHistoryPage_CompareVersions;
-
-	/** */
-	public static String GitHistoryPage_CompareWithWorking;
-
-	/** */
 	public static String GitHistoryPage_FileNotInCommit;
 
 	/** */
