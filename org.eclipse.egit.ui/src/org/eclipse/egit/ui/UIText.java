003c04f18b89ad179e988d2ad3a0e466b17e31fb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:23:48 +0000

    First commited as before_8558_rev6

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1018,9 +1018,6 @@ public class UIText extends NLS {
 	public static String CompareTreeView_WorkspaceVersionText;
 
 	/** */
-	public static String CompareTreeView_ShowIn_label;
-
-	/** */
 	public static String CompareUtils_errorGettingEncoding;
 
 	/** */
@@ -3493,6 +3490,9 @@ public class UIText extends NLS {
 	public static String UIUtils_StartTypingForPreviousValuesMessage;
 
 	/** */
+	public static String UIUtils_ShowInMenuLabel;
+
+	/** */
 	public static String UnmergedBranchDialog_Message;
 
 	/** */
@@ -3710,9 +3710,6 @@ public class UIText extends NLS {
 	public static String CommitFileDiffViewer_OpenInEditorMenuLabel;
 
 	/** */
-	public static String CommitFileDiffViewer_ShowInMenuLabel;
-
-	/** */
 	public static String CommitFileDiffViewer_notContainedInCommit;
 
 	/** */
@@ -3813,6 +3810,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String LocalFileRevision_currentVersionTag;
+
+	/** */
+	public static String LocalNonWorkspaceTypedElement_errorWritingContents;
 
 	/** */
 	public static String LoginDialog_changeCredentials;
