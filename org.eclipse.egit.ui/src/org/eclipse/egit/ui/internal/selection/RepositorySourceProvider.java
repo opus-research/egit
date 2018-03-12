e3094b913cea192a1ddff0d2171d066d95be718f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:51:27 +0000

    First commited as before_67701_rev9

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/RepositorySourceProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/RepositorySourceProvider.java
@@ -8,7 +8,6 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.selection;
 
-import java.lang.ref.WeakReference;
 import java.util.Collections;
 import java.util.Map;
 
@@ -26,13 +25,7 @@ import org.eclipse.ui.services.IServiceLocator;
 /**
  * An {@link AbstractSourceProvider} that provides the current repository (based
  * on the current selection) as a variable in an Eclipse
- * {@link org.eclipse.core.expressions.IEvaluationContext}. To avoid interfering
- * with repository removal, which in EGit relies on {@link WeakReference}
- * semantics, the variable provides not the {@link Repository} directly but its
- * git directory as a string, which can then be used to obtain the
- * {@link Repository} instance via
- * {@link org.eclipse.egit.core.RepositoryCache#getRepository(java.io.File)}. If
- * no repository can be determined, the variable's value is the empty string.
+ * {@link org.eclipse.core.expressions.IEvaluationContext}.
  */
 public class RepositorySourceProvider extends AbstractSourceProvider
 		implements ISelectionListener, IWindowListener {
@@ -44,10 +37,7 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	 */
 	public static final String REPOSITORY_PROPERTY = "org.eclipse.egit.ui.currentRepository"; //$NON-NLS-1$
 
-	/**
-	 * Use a weak reference here to not preclude repository removal.
-	 */
-	private WeakReference<Repository> repositoryRef;
+	private Repository repository;
 
 	@Override
 	public void initialize(IServiceLocator locator) {
@@ -58,19 +48,12 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	@Override
 	public void dispose() {
 		PlatformUI.getWorkbench().removeWindowListener(this);
-		repositoryRef = null;
+		repository = null;
 	}
 
 	@Override
 	public Map getCurrentState() {
-		@SuppressWarnings("resource")
-		Repository repository = repositoryRef == null ? null
-				: repositoryRef.get();
-		if (repository == null) {
-			return Collections.singletonMap(REPOSITORY_PROPERTY, ""); //$NON-NLS-1$
-		}
-		return Collections.singletonMap(REPOSITORY_PROPERTY,
-				repository.getDirectory().getAbsolutePath());
+		return Collections.singletonMap(REPOSITORY_PROPERTY, repository);
 	}
 
 	@Override
@@ -87,29 +70,11 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 			newRepository = SelectionUtils.getRepository(
 					SelectionUtils.getStructuredSelection(selection));
 		}
-		@SuppressWarnings("resource")
-		Repository currentRepository = repositoryRef == null ? null
-				: repositoryRef.get();
-		if (currentRepository == null && repositoryRef != null) {
-			repositoryRef = null;
-			if (newRepository == null) {
-				// Last evaluation was non-null, but that repo has since gone.
-				fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
-						REPOSITORY_PROPERTY, ""); //$NON-NLS-1$
-				return;
-			}
-		}
-		if (currentRepository != newRepository) {
-			if (newRepository != null) {
-				repositoryRef = new WeakReference<>(newRepository);
-				fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
-						REPOSITORY_PROPERTY,
-						newRepository.getDirectory().getAbsolutePath());
-			} else {
-				repositoryRef = null;
-				fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
-						REPOSITORY_PROPERTY, ""); //$NON-NLS-1$
-			}
+		if (repository != newRepository) {
+			repository = newRepository;
+			fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
+					REPOSITORY_PROPERTY,
+					repository);
 		}
 	}
 
