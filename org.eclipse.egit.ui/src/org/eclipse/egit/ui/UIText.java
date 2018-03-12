c8e43ab14638e829c36c9131d5e80217560e0fec
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:23:28 +0000

    First commited as before_8487_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2166,9 +2166,6 @@ public class UIText extends NLS {
 	public static String CompareWithPreviousActionHandler_TitleRevisionNotFound;
 
 	/** */
-	public static String CompareUtils_errorCommonAncestor;
-
-	/** */
 	public static String ConfirmationPage_cantConnectToAnyTitle;
 
 	/** */
