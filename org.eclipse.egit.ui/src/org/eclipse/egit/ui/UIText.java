f3cb14f8172992a6d77a297ecc571d344e57b326
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:16:43 +0000

    First commited as before_1144_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -385,6 +385,9 @@ public class UIText extends NLS {
 	public static String RepositoryAction_multiRepoSelectionTitle;
 
 	/** */
+	public static String RepositoryActionHandler_CouldNotGetSelection_message;
+
+	/** */
 	public static String RepositorySearchDialog_browse;
 
 	/** */
@@ -1229,9 +1232,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ConfirmationPage_title;
-
-	/** */
-	public static String CreateBranchPage_InvalidNameMessage;
 
 	/** */
 	public static String CreateBranchPage_BranchAlreadyExistsMessage;
