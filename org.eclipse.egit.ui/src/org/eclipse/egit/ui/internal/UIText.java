8ed12664ed4d02aa589502bf51dd6f963439b691
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:02:14 +0000

    First commited as before_22990_rev9

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -4967,6 +4967,9 @@ public class UIText extends NLS {
 	public static String GitVariableResolver_VariableReferencesNonExistentResource;
 
 	/** */
+	public static String GitTemplateVariableResolver_GitConfigDescription;
+
+	/** */
 	public static String StagingView_UnstagedChanges;
 
 	/** */
@@ -5043,6 +5046,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_Refresh;
+
+	/** */
+	public static String StagingView_GetRepo;
 
 	/** */
 	public static String StagingView_ReplaceWith;
