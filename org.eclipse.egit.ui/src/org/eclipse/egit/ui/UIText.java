07a35dc86225de12e59c14192c904a66fdff0a42
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:01:00 +0000

    First commited as before_4867_rev6

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -592,6 +592,9 @@ public class UIText extends NLS {
 	public static String ProjectsPreferencePage_AutoShareProjects;
 
 	/** */
+	public static String ProjectsPreferencePage_RestoreBranchProjects;
+
+	/** */
 	public static String GitProjectPropertyPage_LabelBranch;
 
 	/** */
@@ -979,9 +982,6 @@ public class UIText extends NLS {
 	public static String RepositoryLocationPage_title;
 
 	/** */
-	public static String RepositoryLocationContentProvider_errorProvidingRepoServer;
-
-	/** */
 	public static String RepositorySearchDialog_browse;
 
 	/** */
@@ -1136,6 +1136,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String SourceBranchPage_cannotCreateTemp;
+
+	/** */
+	public static String SourceBranchPage_CompositeTransportErrorMessage;
 
 	/** */
 	public static String CloneDestinationPage_title;
@@ -2572,6 +2575,12 @@ public class UIText extends NLS {
 	public static String CommitDialogPreferencePage_formatting;
 
 	/** */
+	public static String CommitDialogPreferencePage_includeUntrackedFiles;
+
+	/** */
+	public static String CommitDialogPreferencePage_includeUntrackedFilesTooltip;
+
+	/** */
 	public static String CommitDialogPreferencePage_signedOffBy;
 
 	/** */
@@ -2621,6 +2630,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String DecoratorPreferencesPage_bindingBranchName;
+
+	/** */
+	public static String DecoratorPreferencesPage_bindingBranchStatus;
 
 	/** */
 	public static String DecoratorPreferencesPage_bindingDirtyFlag;
@@ -3002,9 +3014,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitImportWizard_errorParsingURI;
-
-	/** */
-	public static String GitImportWizard_noRepositoryInfo;
 
 	/** */
 	public static String GitImportWizard_WizardTitle;
