445ebfbd041e6865a924dc2557c33816f13c6dc8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:05:14 +0000

    First commited as before_25605_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -665,6 +665,9 @@ public class UIText extends NLS {
 	public static String GitPreferenceRoot_SecureStoreUseByDefault;
 
 	/** */
+	public static String GitPreferenceRoot_useLogicalModel;
+
+	/** */
 	public static String ProcessStepsRebaseCommand_CancelDialogMessage;
 
 	/** */
@@ -2718,6 +2721,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String FetchGerritChangePage_LocalBranchRadio;
+
+	/** */
+	public static String FetchGerritChangePage_LocalBranchCheckout;
 
 	/** */
 	public static String FetchGerritChangePage_MissingChangeMessage;
