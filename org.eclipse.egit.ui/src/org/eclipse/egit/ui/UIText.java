7a41af58cd8b5ab5f59bab6061ce722046849214
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:30:45 +0000

    First commited as before_2610_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -172,6 +172,9 @@ public class UIText extends NLS {
 	public static String GenerateHistoryJob_errorComputingHistory;
 
 	/** */
+	public static String GenerateHistoryJob_NoCommits;
+
+	/** */
 	public static String EGitCredentialsProvider_errorReadingCredentials;
 
 	/** */
@@ -2312,18 +2315,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitDialogPreferencePage_createChangeIdTooltip;
-
-	/** */
-	public static String CommitDialogPreferencePage_footers;
-
-	/** */
-	public static String CommitDialogPreferencePage_formatting;
-
-	/** */
-	public static String CommitDialogPreferencePage_signedOffBy;
-
-	/** */
-	public static String CommitDialogPreferencePage_signedOffByTooltip;
 
 	/** */
 	public static String Decorator_exceptionMessage;
