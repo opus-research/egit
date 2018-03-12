bb0989c082dc486fe1349696fe1390aab1fc9af2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:12:24 +0000

    First commited as before_302_rev3

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/ConnectProviderOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/ConnectProviderOperation.java
@@ -51,7 +51,7 @@ public class ConnectProviderOperation implements IWorkspaceRunnable {
 	 *            the project to connect to the Git team provider.
 	 */
 	public ConnectProviderOperation(final IProject proj) {
-		this(proj, proj.getLocation().append("/").append(Constants.DOT_GIT).toFile());
+		this(proj, new File(Constants.DOT_GIT));
 	}
 
 	/**
@@ -60,7 +60,7 @@ public class ConnectProviderOperation implements IWorkspaceRunnable {
 	 * @param proj
 	 *            the project to connect to the Git team provider.
 	 * @param pathToRepo
-	 *            absolute path to the repository
+	 *            relative path to the repository
 	 */
 	public ConnectProviderOperation(final IProject proj, File pathToRepo) {
 		this.projects.put(proj, pathToRepo);
