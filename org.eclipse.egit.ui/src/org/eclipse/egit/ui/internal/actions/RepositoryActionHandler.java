08093ac2bf6121fd80863386bf993482e8094bed
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:28:20 +0000

    First commited as before_48302_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RepositoryActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RepositoryActionHandler.java
@@ -8,7 +8,6 @@
  * Copyright (C) 2012, 2013 François Rey <eclipse.org_@_francois_._rey_._name>
  * Copyright (C) 2013 Laurent Goubet <laurent.goubet@obeo.fr>
  * Copyright (C) 2015, IBM Corporation (Dani Megert <daniel_megert@ch.ibm.com>)
- * Copyright (C) 2015, Stefan Dirix <sdirix@eclipsesource.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -466,10 +465,10 @@ abstract class RepositoryActionHandler extends AbstractHandler {
 			return path;
 	}
 
-	protected List<PreviousCommit> findPreviousCommits(IResource resource)
-			throws IOException {
+	protected List<PreviousCommit> findPreviousCommits() throws IOException {
 		List<PreviousCommit> result = new ArrayList<PreviousCommit>();
 		Repository repository = getRepository();
+		IResource resource = getSelectedResources()[0];
 		String path = RepositoryMapping.getMapping(resource.getProject())
 				.getRepoRelativePath(resource);
 		try (RevWalk rw = new RevWalk(repository)) {
