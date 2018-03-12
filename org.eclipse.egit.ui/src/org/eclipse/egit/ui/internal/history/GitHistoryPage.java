f169579627a235e70746ef9996e1184e18526e07
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:22:08 +0000

    First commited as before_1928_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
@@ -30,7 +31,6 @@ import org.eclipse.egit.ui.UIIcons;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.UIUtils;
-import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.repository.tree.FileNode;
 import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
@@ -88,7 +88,7 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.team.ui.history.HistoryPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartSite;
-import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.progress.IWorkbenchSiteProgressService;
 
 /** Graphical commit history viewer. */
@@ -111,7 +111,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 	// we need to keep track of these actions so that we can
 	// dispose them when the page is disposed (the history framework
 	// does not do this for us)
-	private final List<IWorkbenchAction> actionsToDispose = new ArrayList<IWorkbenchAction>();
+	private final List<BooleanPrefAction> actionsToDispose = new ArrayList<BooleanPrefAction>();
 
 	private final IPersistentPreferenceStore store = (IPersistentPreferenceStore) Activator
 			.getDefault().getPreferenceStore();
@@ -346,9 +346,6 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 		};
 		actionsToDispose.add(compareModeAction);
 
-		CompareUtils.ReuseCompareEditorAction reuseCompareEditorAction = new CompareUtils.ReuseCompareEditorAction();
-		actionsToDispose.add(reuseCompareEditorAction);
-
 		compareModeAction.setImageDescriptor(UIIcons.ELCL16_COMPARE_VIEW);
 		compareModeAction.setToolTipText(UIText.GitHistoryPage_compareMode);
 
@@ -389,7 +386,6 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 		viewMenuMgr.add(showAllResourceVersionsAction);
 		viewMenuMgr.add(new Separator());
 		viewMenuMgr.add(compareModeAction);
-		viewMenuMgr.add(reuseCompareEditorAction);
 		viewMenuMgr.add(showAllBranchesAction);
 
 		final IAction showCommentAction = createShowComment();
@@ -797,19 +793,19 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 		}
 
 		// dispose of the actions (the history framework doesn't do this for us)
-		for (IWorkbenchAction action : actionsToDispose)
+		for (BooleanPrefAction action : actionsToDispose)
 			action.dispose();
 		actionsToDispose.clear();
 		cancelRefreshJob();
 		if (popupMgr != null) {
 			for (final IContributionItem i : popupMgr.getItems()) {
-				if (i instanceof IWorkbenchAction)
-					((IWorkbenchAction) i).dispose();
+				if (i instanceof ActionFactory.IWorkbenchAction)
+					((ActionFactory.IWorkbenchAction) i).dispose();
 			}
 			for (final IContributionItem i : getSite().getActionBars()
 					.getMenuManager().getItems()) {
-				if (i instanceof IWorkbenchAction)
-					((IWorkbenchAction) i).dispose();
+				if (i instanceof ActionFactory.IWorkbenchAction)
+					((ActionFactory.IWorkbenchAction) i).dispose();
 			}
 		}
 		super.dispose();
@@ -935,12 +931,6 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 				return false;
 			}
 
-			try {
-				initAndStartRevWalk(true);
-			} catch (IllegalStateException e) {
-				Activator.handleError(e.getMessage(), e.getCause(), true);
-				return false;
-			}
 			return true;
 		} finally {
 			if (trace)
@@ -965,7 +955,14 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 				}
 
 				if (showAllFilter == ShowFilter.SHOWALLFOLDER) {
-					final String path = map.getRepoRelativePath(r.getParent());
+					final String path;
+					// if the resource's parent is the workspace root, we will
+					// get nonsense from map.getRepoRelativePath(), so we
+					// check here and use the project instead
+					if (r.getParent() instanceof IWorkspaceRoot)
+						path = map.getRepoRelativePath(r.getProject());
+					else
+						path = map.getRepoRelativePath(r.getParent());
 					if (path != null && path.length() > 0)
 						paths.add(path);
 				} else if (showAllFilter == ShowFilter.SHOWALLPROJECT) {
@@ -1270,7 +1267,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 	}
 
 	private abstract class BooleanPrefAction extends Action implements
-			IPropertyChangeListener, IWorkbenchAction {
+			IPropertyChangeListener, ActionFactory.IWorkbenchAction {
 		private final String prefName;
 
 		BooleanPrefAction(final String pn, final String text) {
@@ -1305,5 +1302,4 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 			store.removePropertyChangeListener(this);
 		}
 	}
-
 }
