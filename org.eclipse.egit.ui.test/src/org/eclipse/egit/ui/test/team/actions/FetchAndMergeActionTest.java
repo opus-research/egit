9d3163afd932cef7c84226add0ddf8740d63f184
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:58:41 +0000

    First commited as before_74954_rev8

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/FetchAndMergeActionTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/FetchAndMergeActionTest.java
@@ -29,6 +29,7 @@ import org.eclipse.egit.ui.test.JobJoiner;
 import org.eclipse.egit.ui.test.TestUtil;
 import org.eclipse.egit.ui.view.repositories.GitRepositoriesViewTestUtils;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
@@ -36,7 +37,6 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
@@ -82,11 +82,12 @@ public class FetchAndMergeActionTest extends LocalRepositoryTestCase {
 
 		String oldContent = getTestFileContent();
 		fetch();
-		final String title = NLS.bind(UIText.FetchResultDialog_title,
-				childRepositoryFile.getParentFile().getName() + " - origin");
-		bot.waitUntil(Conditions.shellIsActive(title));
 
-		SWTBotShell confirm = bot.shell(title);
+		String uri = lookupRepository(childRepositoryFile).getConfig()
+				.getString(ConfigConstants.CONFIG_REMOTE_SECTION, "origin",
+						ConfigConstants.CONFIG_KEY_URL);
+		SWTBotShell confirm = bot.shell(NLS.bind(
+				UIText.FetchResultDialog_title, uri));
 		SWTBotTree tree = confirm.bot().tree();
 		String branch = tree.getAllItems()[0].getText();
 		assertTrue("Wrong result",
@@ -98,8 +99,7 @@ public class FetchAndMergeActionTest extends LocalRepositoryTestCase {
 		assertEquals(oldContent, newContent);
 
 		fetch();
-		bot.waitUntil(Conditions.shellIsActive(title));
-		confirm = bot.shell(title);
+		confirm = bot.shell(NLS.bind(UIText.FetchResultDialog_title, uri));
 		int count = confirm.bot().tree().rowCount();
 
 		confirm.close();
@@ -165,14 +165,23 @@ public class FetchAndMergeActionTest extends LocalRepositoryTestCase {
 	}
 
 	private void fetch() throws Exception {
+		SWTBotShell fetchDialog = openFetchDialog();
+		fetchDialog.bot().button(IDialogConstants.NEXT_LABEL).click();
+		JobJoiner jobJoiner = JobJoiner.startListening(JobFamilies.FETCH, 20, TimeUnit.SECONDS);
+		fetchDialog.bot().button(IDialogConstants.FINISH_LABEL).click();
+		jobJoiner.join();
+	}
+
+	private SWTBotShell openFetchDialog() throws Exception {
 		SWTBotTree projectExplorerTree = TestUtil.getExplorerTree();
 		getProjectItem(projectExplorerTree, PROJ1).select();
-		String menuString = util
-				.getPluginLocalizedValue("FetchFromUpstreamAction.label");
-		JobJoiner jobJoiner = JobJoiner.startListening(JobFamilies.FETCH, 20, TimeUnit.SECONDS);
+		String menuString = util.getPluginLocalizedValue("FetchAction_label");
+		String submenuString = util
+				.getPluginLocalizedValue("RemoteSubMenu.label");
 		ContextMenuHelper.clickContextMenu(projectExplorerTree, "Team",
-				menuString);
-		jobJoiner.join();
+				submenuString, menuString);
+		SWTBotShell dialog = bot.shell(UIText.FetchWizard_windowTitleDefault);
+		return dialog;
 	}
 
 	private SWTBotShell openMergeDialog() throws Exception {
