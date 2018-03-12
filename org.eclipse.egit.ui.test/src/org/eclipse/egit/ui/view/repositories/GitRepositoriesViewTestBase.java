2b512d7be40c7a2fcc923a19615f37f30e712bbe
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:56:22 +0000

    First commited as before_4587_rev1

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
@@ -141,9 +141,9 @@ public abstract class GitRepositoriesViewTestBase extends
 		untracked.addAll(Arrays.asList(commitables));
 		// commit to stable
 		CommitOperation op = new CommitOperation(commitables,
-				untracked, "Test Author <test.author@test.com>",
-				"Test Committer <test.commiter@test.com>",
-				"Initial commit");
+				new ArrayList<IFile>(), untracked,
+				"Test Author <test.author@test.com>",
+				"Test Committer <test.commiter@test.com>", "Initial commit");
 		op.execute(null);
 
 		// now create a stable branch (from master)
