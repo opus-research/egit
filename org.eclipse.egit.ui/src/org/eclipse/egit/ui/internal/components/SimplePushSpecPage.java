c2f3d282ef5d20909e0f83b640fd74b4338d2079
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:01:40 +0000

    First commited as before_4899_rev14

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/SimplePushSpecPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/SimplePushSpecPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2012, Markus Duft <markus.duft@salomon.at>
+ * Copyright (C) 2012 Markus Duft <markus.duft@salomon.at> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -75,7 +75,10 @@ public class SimplePushSpecPage extends WizardPage {
 
 		Composite inputPanel = new Composite(main, SWT.NONE);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(inputPanel);
-		inputPanel.setLayout(new GridLayout(2, false));
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginWidth= 0;
+		layout.marginHeight= 0;
+		inputPanel.setLayout(layout);
 
 		final Label lblRemote = new Label(inputPanel, SWT.NONE);
 		lblRemote.setText(UIText.SimplePushSpecPage_TargetRefName);
@@ -109,6 +112,17 @@ public class SimplePushSpecPage extends WizardPage {
 		});
 
 		setControl(main);
+	}
+
+	/*
+	 * @see org.eclipse.jface.dialogs.DialogPage#setVisible(boolean)
+	 * @since 2.0
+	 */
+	@Override
+	public void setVisible(boolean visible) {
+		super.setVisible(visible);
+		if (visible)
+			remoteRefName.setFocus();
 	}
 
 	/**
@@ -160,8 +174,7 @@ public class SimplePushSpecPage extends WizardPage {
 
 	@Override
 	public boolean isPageComplete() {
-		return remoteRefName.getText() != null
-				&& remoteRefName.getText().length() > 0;
+		return remoteRefName.getText().length() > 0;
 	}
 
 	/**
