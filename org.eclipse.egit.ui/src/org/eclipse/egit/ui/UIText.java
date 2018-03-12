11616e26656d11f745066e56be5d46b92a934ce1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:58:32 +0000

    First commited as before_4698_rev5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -181,12 +181,6 @@ public class UIText extends NLS {
 	public static String GenerateHistoryJob_NoCommits;
 
 	/** */
-	public static String GenerateHistoryJob_taskFoundMultipleCommits;
-
-	/** */
-	public static String GenerateHistoryJob_taskFoundSingleCommit;
-
-	/** */
 	public static String GerritConfigurationPage_ConfigureFetchReviewNotes;
 
 	/** */
@@ -4247,9 +4241,6 @@ public class UIText extends NLS {
 	public static String StagingView_ColumnLayout;
 
 	/** */
-	public static String StagingView_IndexDiffReload;
-
-	/** */
 	public static String StagingView_Refresh;
 
 	/** */
@@ -4266,6 +4257,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_StageItemMenuLabel;
+
+	/** */
+	public static String StagingViewContentProvider_SubmoduleError;
 
 	/** */
 	public static String StashApplyCommand_applyFailed;
