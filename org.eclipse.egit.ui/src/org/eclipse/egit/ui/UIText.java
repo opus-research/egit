aa2ab7d0496ec0f3d90fdb9d8e3659fb9917901d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:01:30 +0000

    First commited as before_4899_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1156,6 +1156,9 @@ public class UIText extends NLS {
 	public static String CloneDestinationPage_browseButton;
 
 	/** */
+	public static String CloneDestinationPage_cloneSubmodulesButton;
+
+	/** */
 	public static String CloneDestinationPage_DefaultRepoFolderTooltip;
 
 	/** */
@@ -2542,6 +2545,9 @@ public class UIText extends NLS {
 	public static String CheckoutDialog_Delete;
 
 	/** */
+	public static String CommitDialogPreferencePage_commitMessageHistory;
+
+	/** */
 	public static String CommitDialogPreferencePage_title;
 
 	/** */
@@ -2555,6 +2561,12 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitDialogPreferencePage_formatting;
+
+	/** */
+	public static String CommitDialogPreferencePage_includeUntrackedFiles;
+
+	/** */
+	public static String CommitDialogPreferencePage_includeUntrackedFilesTooltip;
 
 	/** */
 	public static String CommitDialogPreferencePage_signedOffBy;
@@ -2582,12 +2594,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String DecoratorPreferencesPage_recomputeAncestorDecorationsTooltip;
-
-	/** */
-	public static String DecoratorPreferencesPage_computeRecursiveLimit;
-
-	/** */
-	public static String DecoratorPreferencesPage_computeRecursiveLimitTooltip;
 
 	/** */
 	public static String DecoratorPreferencesPage_description;
@@ -4176,21 +4182,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String FetchChangeFromGerritCommand_noRepositorySelectedMessage;
-
-	/** */
-	public static String GitScmUrlImportWizardPage_title;
-
-	/** */
-	public static String GitScmUrlImportWizardPage_description;
-
-	/** */
-	public static String GitScmUrlImportWizardPage_importMaster;
-
-	/** */
-	public static String GitScmUrlImportWizardPage_importVersion;
-
-	/** */
-	public static String GitScmUrlImportWizardPage_counter;
 
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
