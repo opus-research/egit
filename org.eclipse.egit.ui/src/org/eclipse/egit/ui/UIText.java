f3a3046d91646f9166e636c6934552242b51eb14
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:07:49 +0000

    First commited as before_5167_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2566,6 +2566,12 @@ public class UIText extends NLS {
 	public static String BranchOperationUI_DetachedHeadMessage;
 
 	/** */
+	public static String BranchResultDialog_buttonCommit;
+
+	/** */
+	public static String BranchResultDialog_buttonReset;
+
+	/** */
 	public static String BranchResultDialog_CheckoutConflictsMessage;
 
 	/** */
@@ -3752,7 +3758,7 @@ public class UIText extends NLS {
 	public static String RevertFailureDialog_Title;
 
 	/** */
-	public static String RevertHandler_AlreadyRevertedMessae;
+	public static String RevertHandler_AlreadyRevertedMessage;
 
 	/** */
 	public static String RevertHandler_JobName;
