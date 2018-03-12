d8a1f9d78603f1a8c7da2628bcf92b30d23df14a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:14:12 +0000

    First commited as before_5880_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GraphLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GraphLabelProvider.java
@@ -23,8 +23,6 @@ class GraphLabelProvider extends BaseLabelProvider implements
 		ITableLabelProvider {
 	private GitDateFormatter dateFormatter;
 
-	private boolean showEmail;
-
 	private RevCommit lastCommit;
 
 	private PersonIdent lastAuthor;
@@ -41,17 +39,14 @@ class GraphLabelProvider extends BaseLabelProvider implements
 		if (columnIndex == 0)
 			return c.getShortMessage();
 		if (columnIndex == 3)
-			return c.getId().name();
+			return c.getId().abbreviate(8).name() + "..."; //$NON-NLS-1$
 		if (columnIndex == 1 || columnIndex == 2) {
 			final PersonIdent author = authorOf(c);
 			if (author != null)
 				switch (columnIndex) {
 				case 1:
-					if (showEmail)
-						return author.getName()
-								+ " <" + author.getEmailAddress() + '>'; //$NON-NLS-1$
-					else
-						return author.getName();
+					return author.getName()
+							+ " <" + author.getEmailAddress() + ">"; //$NON-NLS-1$ //$NON-NLS-2$
 				case 2:
 					return getDateFormatter().formatDate(author);
 				}
@@ -61,11 +56,8 @@ class GraphLabelProvider extends BaseLabelProvider implements
 			if (committer != null)
 				switch (columnIndex) {
 				case 4:
-					if (showEmail)
-						return committer.getName()
-								+ " <" + committer.getEmailAddress() + '>'; //$NON-NLS-1$
-					else
-						return committer.getName();
+					return committer.getName()
+							+ " <" + committer.getEmailAddress() + ">"; //$NON-NLS-1$ //$NON-NLS-2$
 				case 5:
 					return getDateFormatter().formatDate(committer);
 				}
@@ -111,12 +103,5 @@ class GraphLabelProvider extends BaseLabelProvider implements
 			format = Format.RELATIVE;
 		else
 			format = Format.LOCALE;
-	}
-
-	/**
-	 * @param showEmail true to show e-mail addresses, false otherwise
-	 */
-	public void setShowEmailAddresses(boolean showEmail) {
-		this.showEmail = showEmail;
 	}
 }
