57d0ee151ab32acf986a9b3e89c267f250382d4b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:47:12 +0000

    First commited as before_16124_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
@@ -457,21 +457,21 @@ public class CommitFileDiffViewer extends TableViewer {
 			IWorkbenchPage page = window.getActivePage();
 			RevCommit commit = d.getChange().equals(ChangeType.DELETE) ? d
 					.getCommit().getParent(0) : d.getCommit();
-			String path = d.getPath();
-			IFileRevision rev = CompareUtils.getFileRevision(path, commit,
-					getRepository(),
+			IFileRevision rev = CompareUtils.getFileRevision(d.getNewPath(),
+					commit, getRepository(),
 					d.getChange().equals(ChangeType.DELETE) ? d.getBlobs()[0]
 							: d.getBlobs()[d.getBlobs().length - 1]);
 			if (rev != null) {
 				BlameOperation op = new BlameOperation(getRepository(),
-						rev.getStorage(new NullProgressMonitor()), path,
+						rev.getStorage(new NullProgressMonitor()),
+						d.getNewPath(),
 						commit, window.getShell(), page);
 				JobUtil.scheduleUserJob(op, UIText.ShowBlameHandler_JobName,
 						JobFamilies.BLAME);
 			} else {
 				String message = NLS.bind(
 						UIText.CommitFileDiffViewer_notContainedInCommit,
-						path, d.getCommit().getId().getName());
+						d.getNewPath(), d.getCommit().getId().getName());
 				Activator.showError(message, null);
 			}
 		} catch (IOException e) {
