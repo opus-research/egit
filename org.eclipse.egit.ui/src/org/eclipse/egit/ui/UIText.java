7589d53a56d8a902f08d6a5d613bb69d3168f920
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:18:41 +0000

    First commited as before_1412_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1300,12 +1300,6 @@ public class UIText extends NLS {
 	public static String ConfigureUriPage_ConfigurePush_pagetitle;
 
 	/** */
-	public static String ConfigureUriPage_DuplicateUriMessage;
-
-	/** */
-	public static String ConfigureUriPage_DuplicateUriTitle;
-
-	/** */
 	public static String ConfigureUriPage_FetchUri_label;
 
 	/** */
@@ -1316,9 +1310,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ConfigureUriPage_ParsingProblem_message;
-
-	/** */
-	public static String ConfigureUriPage_PushUriGroup;
 
 	/** */
 	public static String ConfigureUriPage_Remove_button;
@@ -2406,9 +2397,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String SelectSynchronizeResourceDialog_includeUncommitedChnages;
-
-	/** */
-	public static String SelectUriWiazrd_Title;
 
 	/** */
 	public static String SynchronizeWithAction_localRepoName;
