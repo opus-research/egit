c1204f1190a9d0c30162a9a1daa1f87495b5d5b4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:12:41 +0000

    First commited as before_361_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/BranchOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/BranchOperation.java
@@ -10,12 +10,16 @@
  *******************************************************************************/
 package org.eclipse.egit.core.op;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.egit.core.internal.util.ProjectUtil;
+import org.eclipse.team.core.TeamException;
 import org.eclipse.jgit.errors.CheckoutConflictException;
 import org.eclipse.jgit.lib.Commit;
 import org.eclipse.jgit.lib.Constants;
@@ -24,7 +28,6 @@ import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.Tree;
 import org.eclipse.jgit.lib.WorkDirCheckout;
-import org.eclipse.team.core.TeamException;
 
 /**
  * This class implements checkouts of a specific revision. A check
@@ -74,10 +77,27 @@ public class BranchOperation implements IWorkspaceRunnable {
 		updateHeadRef();
 		monitor.worked(1);
 
-		ProjectUtil.refreshProjects(repository, monitor);
+		refreshProjects();
 		monitor.worked(1);
 
 		monitor.done();
+	}
+
+	private void refreshProjects() {
+		final IProject[] projects = ResourcesPlugin.getWorkspace().getRoot()
+				.getProjects();
+		final File parentFile = repository.getWorkDir();
+		for (IProject p : projects) {
+			final File file = p.getLocation().toFile();
+			if (file.getAbsolutePath().startsWith(parentFile.getAbsolutePath())) {
+				try {
+					System.out.println("Refreshing " + p);
+					p.refreshLocal(IResource.DEPTH_INFINITE, null);
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	private void updateHeadRef() throws TeamException {
