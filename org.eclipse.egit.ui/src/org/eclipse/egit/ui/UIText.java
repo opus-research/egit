cbbc5053a0553c3570338e48c14ad54e39197b3d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:37:13 +0000

    First commited as before_3289_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -418,9 +418,6 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_ShowSubMenuLabel;
 
 	/** */
-	public static String GitPreferenceRoot_automaticallyEnableChangesetModel;
-
-	/** */
 	public static String GitPreferenceRoot_CloningRepoGroupHeader;
 
 	/** */
@@ -455,9 +452,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitPreferenceRoot_RepoChangeScannerGroupHeader;
-
-	/** */
-	public static String GitPreferenceRoot_SynchronizeView;
 
 	/** */
 	public static String GitProjectPropertyPage_LabelBranch;
@@ -2921,31 +2915,10 @@ public class UIText extends NLS {
 	public static String CommitActionHandler_repository;
 
 	/** */
-	public static String CommitEditor_TitleHeader;
-
-	/** */
 	public static String CommitEditorInput_Name;
 
 	/** */
 	public static String CommitEditorInput_ToolTip;
-
-	/** */
-	public static String CommitEditorPage_LabelAuthor;
-
-	/** */
-	public static String CommitEditorPage_LabelCommitter;
-
-	/** */
-	public static String CommitEditorPage_LabelParent;
-
-	/** */
-	public static String CommitEditorPage_SectionFiles;
-
-	/** */
-	public static String CommitEditorPage_SectionMessage;
-
-	/** */
-	public static String CommitEditorPage_Title;
 
 	/** */
 	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
@@ -2976,9 +2949,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitGraphTable_CompareWithEachOtherInTreeMenuLabel;
-
-	/** */
-	public static String CommitGraphTable_OpenCommitLabel;
 
 	/** */
 	public static String GitSynchronizeWizard_synchronize;
