48f9335c8b8f2861c0ef5269ba48b77e16819356
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:06:56 +0000

    First commited as before_28037_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -3960,19 +3960,10 @@ public class UIText extends NLS {
 	public static String CommitEditor_couldNotShowRepository;
 
 	/** */
-	public static String CommitEditor_couldNotFindStashCommit;
-
-	/** */
-	public static String CommitEditor_couldNotGetStashIndex;
-
-	/** */
 	public static String CommitEditor_showGitRepo;
 
 	/** */
-	public static String CommitEditor_TitleHeaderCommit;
-
-	/** */
-	public static String CommitEditor_TitleHeaderStashedCommit;
+	public static String CommitEditor_TitleHeader;
 
 	/** */
 	public static String CommitEditorInput_Name;
@@ -4024,12 +4015,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitEditorPage_TooltipSignedOffByCommitter;
-
-	/** */
-	public static String StashEditorPage_UnstagedChanges;
-
-	/** */
-	public static String StashEditorPage_StagedChanges;
 
 	/** */
 	public static String Header_contextMenu_copy;
@@ -5004,9 +4989,6 @@ public class UIText extends NLS {
 	public static String StashDropCommand_dropFailed;
 
 	/** */
-	public static String StashDropCommand_stashCommitNotFound;
-
-	/** */
 	public static String StashDropCommand_jobTitle;
 
 	/** */
@@ -5070,7 +5052,7 @@ public class UIText extends NLS {
 	public static String CustomPromptDialog_information_about;
 
 	/** */
-	public static String GitModelSynchonize_fetchGitDataJobName;
+	public static String GitModelSynchronize_fetchGitDataJobName;
 
 	/** */
 	public static String FetchChangeFromGerritCommand_noRepositorySelectedTitle;
