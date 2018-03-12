672eb25fe43e6a946d92c243e2e6654e6c2c4d2e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:12:13 +0000

    First commited as before_160_rev2

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitTestCase.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitTestCase.java
@@ -10,8 +10,13 @@ package org.eclipse.egit.core.test;
 
 import java.io.File;
 import java.io.IOException;
+import org.eclipse.jgit.lib.Constants;
 
 import junit.framework.TestCase;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jgit.junit.MockSystemReader;
+import org.eclipse.jgit.util.SystemReader;
 
 public abstract class GitTestCase extends TestCase {
 
@@ -21,9 +26,13 @@ public abstract class GitTestCase extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		((MockSystemReader) SystemReader.getInstance()).setProperty(
+				Constants.GIT_CEILING_DIRECTORIES_KEY, ResourcesPlugin
+						.getWorkspace().getRoot().getLocation().toFile()
+						.getAbsoluteFile().toString());
 		project = new TestProject(true);
 		gitDir = new File(project.getProject().getWorkspace().getRoot()
-				.getRawLocation().toFile(), ".git");
+				.getRawLocation().toFile(), Constants.DOT_GIT);
 		rmrf(gitDir);
 	}
 
