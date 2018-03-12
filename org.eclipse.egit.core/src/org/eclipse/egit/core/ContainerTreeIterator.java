cec1fb9167551de3a9df01e6727481f02732bf86
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:20:03 +0000

    First commited as before_7297_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/ContainerTreeIterator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/ContainerTreeIterator.java
@@ -169,13 +169,6 @@ public class ContainerTreeIterator extends WorkingTreeIterator {
 			return false;
 		if (Team.isIgnoredHint(resource))
 			return true;
-		return isParentEntryIgnoredByTeamProvider(resource.getParent());
-	}
-
-	private boolean isParentEntryIgnoredByTeamProvider(IResource resource) {
-		if (resource.getType() == IResource.ROOT
-				|| resource.getType() == IResource.PROJECT)
-			return false;
 		return isEntryIgnoredByTeamProvider(resource.getParent());
 	}
 
