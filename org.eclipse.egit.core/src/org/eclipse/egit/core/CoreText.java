bfe0ff4324f8284fd17b1543d81f9fbcc1de4d75
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:22:41 +0000

    First commited as before_8326_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -1,6 +1,8 @@
 /*******************************************************************************
  * Copyright (C) 2008, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
+ * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
+ * Copyright (C) 2012, Markus Duft <markus.duft@salomon.at>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -64,6 +66,9 @@ public class CoreText extends NLS {
 
 	/** */
 	public static String CommitOperation_couldNotFindRepositoryMapping;
+
+	/** */
+	public static String CommitOperation_errorParsingPersonIdent;
 
 	/** */
 	public static String ConfigureFetchAfterCloneTask_couldNotFetch;
@@ -258,6 +263,9 @@ public class CoreText extends NLS {
 	public static String CreatePatchOperation_patchFileCouldNotBeWritten;
 
 	/** */
+	public static String IndexDiffCacheEntry_errorCalculatingIndexDelta;
+
+	/** */
 	public static String IndexDiffCacheEntry_refreshingProjects;
 
 	/** */
@@ -307,6 +315,9 @@ public class CoreText extends NLS {
 
 	/** */
 	public static String AddToIndexOperation_failed;
+
+	/** */
+	public static String RemoveFromIndexOperation_removingFilesFromIndex;
 
 	/** */
 	public static String RemoveFromIndexOperation_failed;
--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/util/ResourceUtil.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/util/ResourceUtil.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -38,9 +39,23 @@ public class ResourceUtil {
 	public static IResource getResourceForLocation(IPath location) {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IFile file = root.getFileForLocation(location);
-		if (file != null)
+		if (file != null && file.exists())
 			return file;
-		return root.getContainerForLocation(location);
+		IContainer container = root.getContainerForLocation(location);
+		if (container != null && container.exists())
+			return container;
+		return null;
+	}
+
+	/**
+	 * Return the corresponding file if it exists.
+	 *
+	 * @param location
+	 * @return the file, or null
+	 */
+	public static IFile getFileForLocation(IPath location) {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		return root.getFileForLocation(location);
 	}
 
 	/**
