bf919e62adb11a4b7746c5281edf5b4b53ac5207
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:38:22 +0000

    First commited as before_3369_rev6

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3479,6 +3479,15 @@ public class UIText extends NLS {
 	public static String CommitFileDiffViewer_OpenWorkingTreeVersionInEditorMenuLabel;
 
 	/** */
+	public static String CommitResultLabelProvider_SectionAuthor;
+
+	/** */
+	public static String CommitResultLabelProvider_SectionMessage;
+
+	/** */
+	public static String CommitResultLabelProvider_SectionRepository;
+
+	/** */
 	public static String CommitSearchQuery_Label;
 
 	/** */
@@ -3507,9 +3516,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitSelectionDialog_WindowTitle;
-
-	/** */
-	public static String CommitUI_commitFailed;
 
 	/** */
 	public static String EgitUiEditorUtils_openFailed;
