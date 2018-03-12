4fe9d7ba061ac6f13261e9a2d7638b84d604d893
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:08:32 +0000

    First commited as before_5299_rev9

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -4241,6 +4241,9 @@ public class UIText extends NLS {
 	public static String StagingView_ColumnLayout;
 
 	/** */
+	public static String StagingView_IndexDiffReload;
+
+	/** */
 	public static String StagingView_Refresh;
 
 	/** */
@@ -4257,9 +4260,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_StageItemMenuLabel;
-
-	/** */
-	public static String StagingViewContentProvider_SubmoduleError;
 
 	/** */
 	public static String StashApplyCommand_applyFailed;
