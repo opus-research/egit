31db6e46a2ead7fe1983c4da8d77f3f7d0055be6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:13:17 +0000

    First commited as before_544_rev9

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -40,10 +40,16 @@ public class UIText extends NLS {
 	public static String Activator_scanningRepositories;
 
 	/** */
+	public static String AddToIndexAction_addingFiles;
+
+	/** */
 	public static String AddToIndexAction_addingFilesFailed;
 
 	/** */
 	public static String AddToIndexAction_indexesWithUnmergedEntries;
+
+	/** */
+	public static String AssumeUnchanged_assumeUnchanged;
 
 	/** */
 	public static String WizardProjectsImportPage_ImportProjectsTitle;
@@ -113,6 +119,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String SelectRemoteNamePage_SelectRemoteNameMessage;
+
+	/** */
+	public static String SetQuickdiffBaselineAction_setQuickdiffBaseline;
 
 	/** */
 	public static String SharingWizard_windowTitle;
@@ -398,6 +407,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RepositorySelectionPage_uriChoice;
+
+	/** */
+	public static String SoftResetToRevisionAction_softReset;
 
 	/** */
 	public static String SourceBranchPage_repoEmpty;
@@ -736,6 +748,12 @@ public class UIText extends NLS {
 	public static String ResetAction_resetFailed;
 
 	/** */
+	public static String ResetQuickdiffBaselineAction_resetQuickdiffBaseline;
+
+	/** */
+	public static String ResetQuickdiffBaselineHeadParentAction_0;
+
+	/** */
 	public static String ResourceHistory_toggleCommentWrap;
 
 	/** */
@@ -749,6 +767,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ResourceHistory_toggleTooltips;
+
+	/** */
+	public static String HardResetToRevisionAction_hardReset;
 
 	/** */
 	public static String HistoryPage_authorColumn;
@@ -1750,6 +1771,9 @@ public class UIText extends NLS {
 	public static String DiscardChangesAction_refreshErrorMessage;
 
 	/** */
+	public static String Disconnect_disconnect;
+
+	/** */
 	public static String GitCompareFileRevisionEditorInput_contentIdentifier;
 
 	/** */
@@ -1873,82 +1897,19 @@ public class UIText extends NLS {
 	public static String GitShareProjectsPage_RepositoryLabel;
 
 	/** */
+	public static String MixedResetToRevisionAction_mixedReset;
+
+	/** */
 	public static String UIIcons_errorDeterminingIconBase;
 
 	/** */
 	public static String UIIcons_errorLoadingPluginImage;
 
 	/** */
-	public static String TagAction_cannotCheckout;
+	public static String Untrack_untrack;
 
 	/** */
-	public static String TagAction_cannotGetBranchName;
-
-	/** */
-	public static String TagAction_repositoryState;
-
-	/** */
-	public static String TagAction_errorCreatingTag;
-
-	/** */
-	public static String TagAction_unableToCreateTag;
-
-	/** */
-	public static String TagAction_errorDuringTagging;
-
-	/** */
-	public static String TagAction_errorWhileGettingRevCommits;
-
-	/** */
-	public static String TagAction_unableToResolveHeadObjectId;
-
-	/** */
-	public static String TagAction_errorWhileMappingRevTag;
-
-	/** */
-	public static String TagAction_creating;
-
-	/** */
-	public static String TagAction_taggingFailed;
-
-	/** */
-	public static String CreateTagDialog_tagName;
-
-	/** */
-	public static String CreateTagDialog_tagMessage;
-
-	/** */
-	public static String CreateTagDialog_questionNewTagTitle;
-
-	/** */
-	public static String CreateTagDialog_overwriteTag;
-
-	/** */
-	public static String CreateTagDialog_overwriteTagToolTip;
-
-	/** */
-	public static String CreateTagDialog_existingTags;
-
-	/** */
-	public static String CreateTagDialog_advanced;
-
-	/** */
-	public static String CreateTagDialog_advancedToolTip;
-
-	/** */
-	public static String CreateTagDialog_advancedMessage;
-
-	/** */
-	public static String CreateTagDialog_tagNameToolTip;
-
-	/** */
-	public static String CreateTagDialog_clearButton;
-
-	/** */
-	public static String CreateTagDialog_clearButtonTooltip;
-
-	/** */
-	public static String CommitCombo_showSuggestedCommits;
+	public static String Update_update;
 
 	static {
 		initializeMessages("org.eclipse.egit.ui.uitext", UIText.class); //$NON-NLS-1$
