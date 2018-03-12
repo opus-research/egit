4b26c1a2d5c6a5801c926254c552bffc9f292692
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:34:33 +0000

    First commited as before_11225_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/RepositoriesViewPropertyTester.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/RepositoriesViewPropertyTester.java
@@ -136,16 +136,6 @@ public class RepositoriesViewPropertyTester extends PropertyTester {
 			switch (node.getRepository().getRepositoryState()) {
 			case REBASING_INTERACTIVE:
 				return true;
-			case REBASING_REBASING:
-				return true;
-			default:
-				return false;
-			}
-
-		if (property.equals("canContinueRebase")) //$NON-NLS-1$
-			switch (node.getRepository().getRepositoryState()) {
-			case REBASING_INTERACTIVE:
-				return true;
 			default:
 				return false;
 			}
