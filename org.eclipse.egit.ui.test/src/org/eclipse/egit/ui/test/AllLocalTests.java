7b30f2dbbd3d5a149a842279bf6fdb780e915be4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:34:57 +0000

    First commited as before_3000_rev1

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
@@ -15,8 +15,7 @@ import org.eclipse.egit.ui.test.history.HistoryViewTest;
 import org.eclipse.egit.ui.test.team.actions.AllTeamActionTests;
 import org.eclipse.egit.ui.test.trace.TraceConfigurationDialogTest;
 import org.eclipse.egit.ui.view.repositories.AllRepositoriesViewTests;
-import org.eclipse.egit.ui.view.synchronize.SynchronizeViewGitChangeSetModelTest;
-import org.eclipse.egit.ui.view.synchronize.SynchronizeViewWorkspaceModelTest;
+import org.eclipse.egit.ui.view.synchronize.SynchronizeViewTest;
 import org.eclipse.egit.ui.wizards.clone.GitCloneWizardHttpTest;
 import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
 import org.eclipse.egit.ui.wizards.share.SharingWizardTest;
@@ -35,8 +34,7 @@ import org.junit.runners.Suite.SuiteClasses;
 		PushTest.class,
 		SpellcheckableMessageAreaTest.class,
 		TraceConfigurationDialogTest.class,
-		SynchronizeViewWorkspaceModelTest.class,
-		SynchronizeViewGitChangeSetModelTest.class })
+		SynchronizeViewTest.class })
 public class AllLocalTests {
 	// empty class, don't need anything here
 }
