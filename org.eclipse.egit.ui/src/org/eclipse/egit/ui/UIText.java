20f7c0a6ce88728c70b5ac6b83d491693bc3f245
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:39:06 +0000

    First commited as before_3404_rev7

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3023,13 +3023,10 @@ public class UIText extends NLS {
 	public static String GitBranchSynchronizeWizardPage_description;
 
 	/** */
-	public static String GitBranchSynchronizeWizardPage_projects;
+	public static String GitBranchSynchronizeWizardPage_repositories;
 
 	/** */
-	public static String GitBranchSynchronizeWizardPage_destination;
-
-	/** */
-	public static String GitBranchSynchronizeWizardPage_includeUncommitedChanges;
+	public static String GitBranchSynchronizeWizardPage_branches;
 
 	/** */
 	public static String GitBranchSynchronizeWizardPage_selectAll;
@@ -3527,6 +3524,9 @@ public class UIText extends NLS {
 	public static String CommitSearchPage_CaseSensitive;
 
 	/** */
+	public static String CommitSearchPage_CheckAll;
+
+	/** */
 	public static String CommitSearchPage_CommitId;
 
 	/** */
@@ -3558,6 +3558,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitSearchPage_TreeId;
+
+	/** */
+	public static String CommitSearchPage_UncheckAll;
 
 	/** */
 	public static String CommitSearchQuery_Label;
