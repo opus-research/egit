6e7e7d5491393136a5339306751f19a1bfb81070
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:41:07 +0000

    First commited as before_3561_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -15,6 +15,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.TimeZone;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ParameterizedCommand;
@@ -22,9 +23,16 @@ import org.eclipse.core.commands.common.CommandException;
 import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.egit.core.EclipseGitProgressTransformer;
 import org.eclipse.egit.core.IteratorService;
 import org.eclipse.egit.core.op.CommitOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
@@ -34,7 +42,6 @@ import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.actions.ActionCommands;
-import org.eclipse.egit.ui.internal.actions.BooleanPrefAction;
 import org.eclipse.egit.ui.internal.commit.CommitEditor;
 import org.eclipse.egit.ui.internal.commit.CommitHelper;
 import org.eclipse.egit.ui.internal.commit.CommitUI;
@@ -50,8 +57,6 @@ import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.preference.IPersistentPreferenceStore;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.LocalSelectionTransfer;
 import org.eclipse.jface.viewers.IOpenListener;
 import org.eclipse.jface.viewers.ISelection;
@@ -103,6 +108,7 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.ISources;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.forms.IFormColors;
@@ -112,6 +118,7 @@ import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.progress.IWorkbenchSiteProgressService;
 
 /**
  * A GitX style staging view with embedded commit dialog.
@@ -343,13 +350,6 @@ public class StagingView extends ViewPart {
 			}
 		};
 
-		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
-		if (preferenceStore.contains(UIPreferences.STAGING_VIEW_SYNC_SELECTION))
-			reactOnSelection = preferenceStore.getBoolean(
-					UIPreferences.STAGING_VIEW_SYNC_SELECTION);
-		else
-			preferenceStore.setDefault(UIPreferences.STAGING_VIEW_SYNC_SELECTION, true);
-
 		updateSectionText();
 		updateToolbar();
 
@@ -382,31 +382,6 @@ public class StagingView extends ViewPart {
 	private void updateToolbar() {
 		IToolBarManager toolbar = getViewSite().getActionBars()
 				.getToolBarManager();
-
-		// refresh
-		Action refreshAction = new Action(UIText.StagingView_Refresh, IAction.AS_PUSH_BUTTON) {
-			public void run() {
-				reload(currentRepository);
-			}
-		};
-		refreshAction.setImageDescriptor(UIIcons.ELCL16_REFRESH);
-		toolbar.add(refreshAction);
-
-		// link with selection
-		Action linkSelectionAction = new BooleanPrefAction(
-				(IPersistentPreferenceStore) Activator.getDefault()
-						.getPreferenceStore(),
-				UIPreferences.STAGING_VIEW_SYNC_SELECTION,
-				UIText.StagingView_LinkSelection) {
-			@Override
-			public void apply(boolean value) {
-				reactOnSelection = value;
-			}
-		};
-		linkSelectionAction.setImageDescriptor(UIIcons.ELCL16_SYNCED);
-		toolbar.add(linkSelectionAction);
-
-		toolbar.add(new Separator());
 
 		amendPreviousCommitAction = new Action(
 				UIText.StagingView_Ammend_Previous_Commit, IAction.AS_CHECK_BOX) {
@@ -693,44 +668,86 @@ public class StagingView extends ViewPart {
 		return false;
 	}
 
-	// TODO move to a Job?
-	private IndexDiff reload(final Repository repository) {
-		final boolean repositoryChanged = repository != currentRepository;
+	private void reload(final Repository repository) {
+		final boolean repositoryChanged = currentRepository == repository;
+
+		final AtomicReference<IndexDiff> results = new AtomicReference<IndexDiff>();
+
+		final String jobTitle = MessageFormat.format(UIText.StagingView_IndexDiffReload,
+				StagingView.getRepositoryName(repository));
+
+		Job job = new Job(jobTitle) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				IndexDiff indexDiff = doReload(repository, monitor, jobTitle);
+				results.set(indexDiff);
+				return Status.OK_STATUS;
+			}
+		};
+
+		job.setUser(true);
+		job.setRule(ResourcesPlugin.getWorkspace().getRoot());
+
+		job.addJobChangeListener(new JobChangeAdapter() {
+			public void done(final IJobChangeEvent event) {
+				form.getDisplay().asyncExec(new Runnable() {
+					public void run() {
+						if (form.isDisposed())
+							return;
+
+						final IndexDiff indexDiff = results.get();
+						unstagedTableViewer.setInput(new Object[] { repository,
+								indexDiff });
+						stagedTableViewer
+								.setInput(new Object[] { repository, indexDiff });
+						commitAction.setEnabled(repository.getRepositoryState()
+								.canCommit());
+						form.setText(StagingView.getRepositoryName(repository));
+						if (repositoryChanged) {
+							updateCommitMessageComponent();
+							clearCommitMessageToggles();
+						}
+						updateSectionText();
+					}
+
+				});
+			}
+		});
+
+		schedule(job);
+	}
+
+	private void schedule(final Job j) {
+		IWorkbenchPartSite site = getSite();
+		if (site != null) {
+			final IWorkbenchSiteProgressService p;
+			p = (IWorkbenchSiteProgressService) site
+					.getAdapter(IWorkbenchSiteProgressService.class);
+			if (p != null) {
+				p.schedule(j, 0, true /* use half-busy cursor */);
+				return;
+			}
+		}
+		j.schedule();
+	}
+
+	private IndexDiff doReload(Repository repository, IProgressMonitor monitor, String jobTitle) {
 		currentRepository = repository;
 
+		EclipseGitProgressTransformer jgitMonitor = new EclipseGitProgressTransformer(
+				monitor);
 
 		final IndexDiff indexDiff;
 		try {
 			WorkingTreeIterator iterator = IteratorService.createInitialIterator(repository);
 			indexDiff = new IndexDiff(repository, Constants.HEAD, iterator);
-			indexDiff.diff();
+			indexDiff.diff(jgitMonitor, 0, 0, jobTitle);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
 		removeListeners();
 		attachListeners(repository);
-
-		form.getDisplay().asyncExec(new Runnable() {
-			public void run() {
-				if (form.isDisposed())
-					return;
-
-				unstagedTableViewer.setInput(new Object[] { repository,
-						indexDiff });
-				stagedTableViewer
-						.setInput(new Object[] { repository, indexDiff });
-				commitAction.setEnabled(repository.getRepositoryState()
-						.canCommit());
-				form.setText(StagingView.getRepositoryName(repository));
-				if (repositoryChanged) {
-					updateCommitMessageComponent();
-					clearCommitMessageToggles();
-					updateSectionText();
-				}
-			}
-
-		});
 
 		return indexDiff;
 	}
