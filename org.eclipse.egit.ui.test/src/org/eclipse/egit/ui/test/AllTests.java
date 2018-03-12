6de2a93a02032b7487ce3ff1ada4d558c89980b4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:15:11 +0000

    First commited as before_874_rev16

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  * Copyright (C) 2010, Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
  *
  * All rights reserved. This program and the accompanying materials
@@ -10,7 +10,6 @@ package org.eclipse.egit.ui.test;
 
 import org.eclipse.egit.ui.prefpages.configuration.GlobalConfigurationPageTest;
 import org.eclipse.egit.ui.view.repositories.AllRepositoriesViewTests;
-import org.eclipse.egit.ui.view.synchronize.SynchronizeViewTest;
 import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
 import org.eclipse.egit.ui.wizards.share.SharingWizardTest;
 import org.junit.runner.RunWith;
@@ -18,11 +17,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( { AllRepositoriesViewTests.class, 	//
-		GitCloneWizardTest.class, 					//
-		GlobalConfigurationPageTest.class,			//
-		SharingWizardTest.class,					//
-		SynchronizeViewTest.class })
+@SuiteClasses( { GitCloneWizardTest.class, GlobalConfigurationPageTest.class,
+		SharingWizardTest.class, AllRepositoriesViewTests.class })
 public class AllTests {
 	// empty class, don't need anything here
 }
