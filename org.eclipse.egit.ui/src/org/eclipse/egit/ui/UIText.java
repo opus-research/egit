683d06236dd7c7b27b7bf770dc041c15847c03fd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:15:35 +0000

    First commited as before_939_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2156,9 +2156,6 @@ public class UIText extends NLS {
 	public static String SelectSynchronizeResourceDialog_includeUncommitedChnages;
 
 	/** */
-	public static String SelectUriWiazrd_Title;
-
-	/** */
 	public static String SynchronizeWithAction_localRepoName;
 
 	static {
