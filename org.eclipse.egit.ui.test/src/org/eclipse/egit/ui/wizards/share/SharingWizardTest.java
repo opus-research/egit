5ed0703a9056cd88c3145910ba4f5f6d1f811704
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:26:34 +0000

    First commited as before_2288_rev3

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/share/SharingWizardTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/share/SharingWizardTest.java
@@ -15,22 +15,33 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.common.ExistingOrNewPage;
+import org.eclipse.egit.ui.common.ExistingOrNewPage.Row;
 import org.eclipse.egit.ui.common.SharingWizard;
 import org.eclipse.egit.ui.test.Eclipse;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.NoMessageException;
+import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
 import org.eclipse.jgit.junit.MockSystemReader;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.util.SystemReader;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -39,7 +50,10 @@ import org.junit.runner.RunWith;
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class SharingWizardTest {
 
-	private static final String projectName = "TestProject";
+	private static final String projectName0 = "TestProject";
+	private static final String projectName1 = "TestProject1";
+	private static final String projectName2 = "TestProject2";
+	private static final String projectName3 = "TestProject3";
 
 	private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
@@ -59,24 +73,39 @@ public class SharingWizardTest {
 		bot.perspectiveById("org.eclipse.jdt.ui.JavaPerspective").activate();
 		bot.viewByTitle("Package Explorer").show();
 
+		ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
+	}
+
+	private static String createProject(String projectName) {
 		bot.menu("File").menu("New").menu("Project...").click();
 		bot.tree().getTreeItem("General").expand().getNode("Project").select();
 		bot.button("Next >").click();
 
 		bot.textWithLabel("Project name:").setText(projectName);
 
+		String path = bot.textWithLabel("Location:").getText();
 		bot.button("Finish").click();
-		ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
+		return path;
 	}
 
-	@AfterClass
-	public static void afterClass() throws Exception {
-		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
-				projectName);
-		project.close(null);
-		project.delete(false, null);
+	@After
+	public void after() throws Exception {
+		erase(projectName0);
+		erase(projectName1);
+		erase(projectName2);
+		erase(projectName3);
 		ResourcesPlugin.getWorkspace().getRoot().refreshLocal(
 				IResource.DEPTH_INFINITE, null);
+		new Eclipse().reset();
+	}
+
+	private void erase(String projectName) throws CoreException {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				projectName);
+		if (project.exists()) {
+			project.close(null);
+			project.delete(false, null);
+		}
 	}
 
 	@Before
@@ -87,31 +116,32 @@ public class SharingWizardTest {
 	}
 
 	@Test
-	public void shareProjectWithNewlyCreatedRepo() throws Exception {
+	public void shareProjectAndCreateRepo() throws Exception {
+		createProject(projectName0);
 		ExistingOrNewPage existingOrNewPage = sharingWizard
-				.openWizard(projectName);
+				.openWizard(projectName0);
 
 		// initial state
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		String projectPath = workspace.getRoot().getProject(projectName)
+		String projectPath = workspace.getRoot().getProject(projectName0)
 				.getLocation().toOSString();
 
-		existingOrNewPage.assertContents(projectName, projectPath, "", "");
+		existingOrNewPage.assertContents(false, projectName0, projectPath, "", "");
 		existingOrNewPage.assertEnabling(false, false, false);
 
 		// select project
-		bot.tree().getTreeItem(projectName).select();
-		existingOrNewPage.assertContents(projectName, projectPath, "",
+		bot.tree().getTreeItem(projectName0).select();
+		existingOrNewPage.assertContents(false, projectName0, projectPath, "",
 				projectPath);
 		existingOrNewPage.assertEnabling(true, true, false);
 
 		// create repository
 		bot.button("Create Repository").click();
 
-		String repopath = workspace.getRoot().getProject(projectName)
+		String repopath = workspace.getRoot().getProject(projectName0)
 				.getLocation().append(Constants.DOT_GIT).toOSString();
 		existingOrNewPage
-				.assertContents(projectName, projectPath, repopath, "");
+				.assertContents(true, projectName0, projectPath, repopath, "");
 		existingOrNewPage.assertEnabling(false, false, true);
 
 		assertTrue((new File(repopath)).exists());
@@ -120,17 +150,67 @@ public class SharingWizardTest {
 		bot.button("Finish").click();
 		Thread.sleep(1000);
 		assertEquals("org.eclipse.egit.core.GitProvider",
-				workspace.getRoot().getProject(projectName)
+				workspace.getRoot().getProject(projectName0)
 						.getPersistentProperty(
 								new QualifiedName("org.eclipse.team.core",
 										"repository")));
 	}
 
-	// TODO: push this in the junit class runner. This can then be shared across
-	// all tests.
-	@After
-	public void resetWorkbench() {
-		new Eclipse().reset();
-	}
+	@Test
+	public void shareProjectWithAlreadyCreatedRepos() throws IOException,
+			InterruptedException, NoFilepatternException, NoHeadException,
+			NoMessageException, ConcurrentRefUpdateException,
+			JGitInternalException, WrongRepositoryStateException {
+		FileRepository repo1 = new FileRepository(new File(
+				createProject(projectName1), "../.git"));
+		repo1.create();
+		repo1.close();
+		FileRepository repo2 = new FileRepository(new File(
+				createProject(projectName2), ".git"));
+		repo2.create();
+		repo2.close();
+		FileRepository repo3 = new FileRepository(new File(
+				createProject(projectName3), ".git"));
+		repo3.create();
+		Git git = new Git(repo3);
+		git.add().addFilepattern(".").call();
+		git.commit().setAuthor("A U Thior", "au.thor@example.com").setMessage("Created Project 3").call();
+		repo3.close();
 
+		ExistingOrNewPage existingOrNewPage = sharingWizard.openWizard(
+				projectName1, projectName2, projectName3);
+
+		// initial state
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		String projectPath1 = workspace.getRoot().getProject(projectName1)
+				.getLocation().toOSString();
+		String projectPath2 = workspace.getRoot().getProject(projectName2)
+				.getLocation().toOSString();
+		String projectPath3 = workspace.getRoot().getProject(projectName3)
+				.getLocation().toOSString();
+		existingOrNewPage.assertContents(
+				new Row[] {
+						new Row(true, projectName1, projectPath1, ".."
+								+ File.separator + ".git"),
+						new Row(false, projectName2, projectPath2, "", new Row[] {
+								new Row(false, ".", "", ".git"),
+								new Row(false, "..", "", ".." + File.separator
+										+ ".git")}),
+						new Row(false, projectName3, projectPath3, "", new Row[] {
+								new Row(true, ".", "", ".git"),
+								new Row(false, "..", "", ".." + File.separator
+										+ ".git")
+						})}, "");
+
+		bot.tree().getAllItems()[1].getItems()[0].check();
+		existingOrNewPage.assertEnabling(false, false, true);
+		bot.button("Finish").click();
+		Thread.sleep(1000);
+		assertEquals(repo1.getDirectory().getCanonicalPath(), RepositoryMapping
+				.getMapping(workspace.getRoot().getProject(projectName1))
+				.getRepository().getDirectory().toString());
+		assertEquals(repo2.getDirectory().getCanonicalPath(), RepositoryMapping
+				.getMapping(workspace.getRoot().getProject(projectName2))
+				.getRepository().getDirectory().toString());
+	}
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
@@ -218,6 +218,9 @@ class CommitGraphTable {
 
 		Control c = getControl();
 		menuMgr.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+		// TODO extract this as a constant in the future
+		site.registerContextMenu("org.eclipse.egit.history.popup", menuMgr,table); //$NON-NLS-1$
+
 		c.setMenu(menuMgr.createContextMenu(c));
 		c.addMenuDetectListener(menuListener = new MenuListener(menuMgr,
 				getTableView(), site, copy));
@@ -436,11 +439,6 @@ class CommitGraphTable {
 								.add(getCommandContributionItem(
 										HistoryViewCommands.COMPARE_WITH_TREE,
 										UIText.GitHistoryPage_CompareWithCurrentHeadMenu));
-				else if (selectionSize == 2)
-					popupMgr
-							.add(getCommandContributionItem(
-									HistoryViewCommands.COMPARE_VERSIONS,
-									UIText.GitHistoryPage_CompareWithEachOtherMenuLabel));
 				if (selectionSize > 0) {
 					popupMgr.add(getCommandContributionItem(
 							HistoryViewCommands.OPEN,
@@ -500,6 +498,10 @@ class CommitGraphTable {
 				resetManager.add(getCommandContributionItem(
 						HistoryViewCommands.RESET,
 						UIText.GitHistoryPage_ResetHardMenuLabel, parameters));
+			} else if (selectionSize == 2) {
+				popupMgr.add(getCommandContributionItem(
+						HistoryViewCommands.COMPARE_VERSIONS,
+						UIText.GitHistoryPage_CompareWithEachOtherMenuLabel));
 			}
 			popupMgr.add(new Separator());
 
