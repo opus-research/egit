65c5e9dc304d4b26cac7bc6e814a76663c4822f9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:49:13 +0000

    First commited as before_65998_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/resources/ResourceState.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/resources/ResourceState.java
@@ -93,14 +93,6 @@ public class ResourceState implements IResourceState {
 		return assumeUnchanged;
 	}
 
-	@Override
-	public final boolean hasUnstagedChanges() {
-		if (isIgnored()) {
-			return false;
-		}
-		return !isTracked() || isDirty() || isMissing() || hasConflicts();
-	}
-
 	/**
 	 * Sets the staged property.
 	 *
