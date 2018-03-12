2caf4165f6f86fc907f596645a873805faac997f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:20:21 +0000

    First commited as before_1677_rev3

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/SynchronizeViewTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/SynchronizeViewTest.java
@@ -8,20 +8,23 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.view.synchronize;
 
-import static org.eclipse.egit.ui.test.ContextMenuHelper.clickContextMenu;
-import static org.eclipse.jgit.lib.Constants.HEAD;
-import static org.eclipse.jgit.lib.Constants.MASTER;
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellIsActive;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
-import org.eclipse.egit.ui.test.Eclipse;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.egit.ui.common.EGitTestCase;
+import org.eclipse.egit.ui.common.GitImportRepoWizard;
+import org.eclipse.egit.ui.common.RepoPropertiesPage;
+import org.eclipse.egit.ui.common.RepoRemoteBranchesPage;
+import org.eclipse.egit.ui.test.ContextMenuHelper;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.swt.finder.SWTBot;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotRadio;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.AfterClass;
@@ -30,31 +33,38 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class SynchronizeViewTest extends LocalRepositoryTestCase {
+public class SynchronizeViewTest extends EGitTestCase {
 
+	private static boolean changePerspective = false;
+
+	// this test fails when is run inside eclipse with Maven POM editor
 	@Test
 	public void shouldReturnNoChanges() throws Exception {
-		// given
-		resetRepository(PROJ1);
-		changeFilesInProject();
-		showDialog(PROJ1, "Team", "Synchronize...");
-
 		// when
-		bot.shell("Synchronize repository: " + REPO1 + File.separator + ".git")
+		changeFilesInProject();
+		showSynchronizationDialog("org.eclipse.egit.ui");
+
+		// given
+		bot.shell("Synchronize repository: egit" + File.separator + ".git")
 				.activate();
 
-		bot.comboBox(0)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(1).setSelection(HEAD);
+		bot.comboBox(0).setSelection("local .git");
+		bot.comboBox(1).setSelection("HEAD");
 
-		bot.comboBox(2)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(3).setSelection(MASTER);
+		bot.comboBox(2).setSelection("local .git");
+		bot.comboBox(3).setSelection("master");
 
 		// do not check 'Include local changes'
 
 		// fire action
 		bot.button("OK").click();
+
+		handleConfirmOpenPerspective();
+
+		// wait for dispose "Git Resource Synchronization"
+		bot.waitUntil(shellIsActive("Git Resource Synchronization"), 15000);
+		SWTBotShell gitResSyncShell = bot.shell("Git Resource Synchronization");
+		bot.waitUntil(shellCloses(gitResSyncShell), 300000);
 
 		// then
 		SWTBotTree syncViewTree = bot.viewByTitle("Synchronize").bot().tree();
@@ -64,22 +74,19 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 	// this test fails when is run inside eclipse with Maven POM editor
 	@Test
 	public void shouldReturnListOfChanges() throws Exception {
-		// given
-		resetRepository(PROJ1);
-		changeFilesInProject();
-		showDialog(PROJ1, "Team", "Synchronize...");
-
 		// when
-		bot.shell("Synchronize repository: " + REPO1 + File.separator + ".git")
+		changeFilesInProject();
+		showSynchronizationDialog("org.eclipse.egit.ui");
+
+		// given
+		bot.shell("Synchronize repository: egit" + File.separator + ".git")
 				.activate();
 
-		bot.comboBox(0)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(1).setSelection(HEAD);
+		bot.comboBox(0).setSelection("local .git");
+		bot.comboBox(1).setSelection("HEAD");
 
-		bot.comboBox(2)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(3).setSelection(MASTER);
+		bot.comboBox(2).setSelection("local .git");
+		bot.comboBox(3).setSelection("master");
 
 		// include local changes
 		bot.checkBox("Include local uncommited changes in comparison").click();
@@ -87,77 +94,88 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 		// fire action
 		bot.button("OK").click();
 
+		handleConfirmOpenPerspective();
+
+		// wait for dispose "Git Resource Synchronization"
+		bot.waitUntil(shellIsActive("Git Resource Synchronization"), 15000);
+		SWTBotShell shell = bot.shell("Git Resource Synchronization");
+		bot.waitUntil(shellCloses(shell), 300000);
+
 		// then
 		SWTBotTree syncViewTree = bot.viewByTitle("Synchronize").bot().tree();
-		assertEquals(1, syncViewTree.getAllItems().length);
+		assertEquals(2, syncViewTree.getAllItems().length);
 
 		SWTBotTreeItem[] syncItems = syncViewTree.getAllItems();
-		assertEquals(UIText.GitModelWorkingTree_workingTree,
-				syncItems[0].getText());
+		assertEquals("org.eclipse.egit.core", syncItems[0].getText());
+		assertEquals("org.eclipse.egit.ui", syncItems[1].getText());
+
+		syncItems[0].expand();
+		syncItems[1].expand();
+
+		assertEquals(1, syncItems[0].getNodes().size());
+		assertEquals("pom.xml", syncItems[0].getNodes().get(0));
+		assertEquals(1, syncItems[1].getNodes().size());
+		assertEquals("pom.xml", syncItems[1].getNodes().get(0));
 	}
 
 	@Test
 	public void shouldCompareBranchAgainstTag() throws Exception {
-		// given
-		resetRepository(PROJ1);
-		createTag(PROJ1, "v0.0");
-		makeChangesAndCommit(PROJ1);
-		showDialog(PROJ1, "Team", "Synchronize...");
-
 		// when
-		bot.shell("Synchronize repository: " + REPO1 + File.separator + ".git")
+		showSynchronizationDialog("org.eclipse.egit.ui");
+
+		// given
+		bot.shell("Synchronize repository: egit" + File.separator + ".git")
 				.activate();
 
-		bot.comboBox(0)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(1).setSelection("v0.0");
+		bot.comboBox(0).setSelection("origin");
+		bot.comboBox(1).setSelection("stable-0.7");
 
-		bot.comboBox(2)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(3).setSelection(HEAD);
+		bot.comboBox(2).setSelection("local .git");
+		bot.comboBox(3).setSelection("v0.8.1");
 
 		// fire action
 		bot.button("OK").click();
 
-		// wait for synchronization process finish
-		bot.sleep(1000);
+		handleConfirmOpenPerspective();
+
+		// wait for dispose "Git Resource Synchronization"
+		bot.waitUntil(shellIsActive("Git Resource Synchronization"), 15000);
+		SWTBotShell shell = bot.shell("Git Resource Synchronization");
+		bot.waitUntil(shellCloses(shell), 300000);
 
 		// then
 		SWTBotTree syncViewTree = bot.viewByTitle("Synchronize").bot().tree();
-		assertEquals(1, syncViewTree.getAllItems().length);
+		assertEquals(8, syncViewTree.getAllItems().length);
 	}
 
 	@Test
 	public void shouldCompareTagAgainstTag() throws Exception {
-		// given
-		resetRepository(PROJ1);
-		createTag(PROJ1, "v0.1");
-		makeChangesAndCommit(PROJ1);
-		createTag(PROJ1, "v0.2");
-		makeChangesAndCommit(PROJ1);
-		showDialog(PROJ1, "Team", "Synchronize...");
-
 		// when
-		bot.shell("Synchronize repository: " + REPO1 + File.separator + ".git")
+		showSynchronizationDialog("org.eclipse.egit.ui");
+
+		// given
+		bot.shell("Synchronize repository: egit" + File.separator + ".git")
 				.activate();
 
-		bot.comboBox(0)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(1).setSelection("v0.1");
+		bot.comboBox(0).setSelection("local .git");
+		bot.comboBox(1).setSelection("v0.7.0");
 
-		bot.comboBox(2)
-				.setSelection(UIText.SynchronizeWithAction_localRepoName);
-		bot.comboBox(3).setSelection("v0.2");
+		bot.comboBox(2).setSelection("local .git");
+		bot.comboBox(3).setSelection("v0.8.1");
 
 		// fire action
 		bot.button("OK").click();
 
-		// wait for synchronization process finish
-		bot.sleep(1000);
+		handleConfirmOpenPerspective();
+
+		// wait for dispose "Git Resource Synchronization"
+		bot.waitUntil(shellIsActive("Git Resource Synchronization"), 15000);
+		SWTBotShell shell = bot.shell("Git Resource Synchronization");
+		bot.waitUntil(shellCloses(shell), 300000);
 
 		// then
 		SWTBotTree syncViewTree = bot.viewByTitle("Synchronize").bot().tree();
-		assertEquals(1, syncViewTree.getAllItems().length);
+		assertEquals(8, syncViewTree.getAllItems().length);
 	}
 
 	// this test always fails with cause:
@@ -167,7 +185,7 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 	@Test
 	public void shouldLaunchSynchronizationFromGitRepositories()
 			throws Exception {
-		// given
+		// when
 		bot.menu("Window").menu("Show View").menu("Other...").click();
 		bot.shell("Show View").bot().tree().expandNode("Git").getNode(
 				"Git Repositories").doubleClick();
@@ -184,7 +202,14 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 		branchNode.select();
 		branchNode.contextMenu("Synchronize").click();
 
-		// when
+		handleConfirmOpenPerspective();
+
+		// wait for dispose "Git Resource Synchronization"
+		bot.waitUntil(shellIsActive("Git Resource Synchronization"), 15000);
+		SWTBotShell shell = bot.shell("Git Resource Synchronization");
+		bot.waitUntil(shellCloses(shell), 300000);
+
+		// given
 
 		// then
 		SWTBotTree syncViewTree = bot.viewByTitle("Synchronize").bot().tree();
@@ -199,25 +224,44 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 
 	@BeforeClass
 	public static void setupEnvironment() throws Exception {
-		// disable perspective synchronize selection
-		bot.menu("Window").click().menu("Preferences").click();
-		bot.shell("Preferences").activate();
-		bot.tree().getTreeItem("Team").expand().select();
-		SWTBotRadio syncPerspectiveCheck = bot.radio("Never");
-		if (!syncPerspectiveCheck.isSelected())
-			syncPerspectiveCheck.click();
+		// turn off auto building
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceDescription description = workspace.getDescription();
+		description.setAutoBuilding(false);
+		workspace.setDescription(description);
 
-		bot.button("OK").click();
+		// obtain copy of EGit project
+		GitImportRepoWizard importWizard = new GitImportRepoWizard();
 
-		File repositoryFile = createProjectAndCommitToRepository();
-		createChildRepository(repositoryFile);
-		Activator.getDefault().getRepositoryUtil()
-				.addConfiguredRepository(repositoryFile);
+		importWizard.openWizard();
+		String repoName = "egit";
+		String repoUrl = "git://egit.eclipse.org/egit.git";
+		if (!importWizard.containsRepo(repoName))
+			addRepository(importWizard, repoUrl);
+
+		importWizard.selectAndCloneRepository(repoName);
+		importWizard.waitForCreate();
+		waitForWorkspaceRefresh();
 	}
 
 	@AfterClass
 	public static void restoreEnvironmentSetup() throws Exception {
-		new Eclipse().reset();
+		// turn off auto building
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceDescription description = workspace.getDescription();
+		description.setAutoBuilding(true);
+		workspace.setDescription(description);
+	}
+
+	private static void addRepository(GitImportRepoWizard importWizard,
+			String repoUrl) {
+		RepoPropertiesPage propertiesPage = importWizard.openCloneWizard();
+
+		RepoRemoteBranchesPage remoteBranches = propertiesPage
+				.nextToRemoteBranches(repoUrl);
+		remoteBranches.selectBranches("master");
+
+		remoteBranches.nextToWorkingCopy().waitForCreate();
 	}
 
 	private void changeFilesInProject() throws Exception {
@@ -225,60 +269,37 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 		packageExplorerBot.activeShell();
 		SWTBotTree tree = packageExplorerBot.tree();
 
-		SWTBotTreeItem coreTreeItem = tree.getAllItems()[0];
-		SWTBotTreeItem rootNode = coreTreeItem.expand().getNode(0)
-				.expand().select();
-		rootNode.getNode(0).select().doubleClick();
+		SWTBotTreeItem coreTreeItem = tree.getAllItems()[3];
+		coreTreeItem.expand();
+		for (String node : coreTreeItem.getNodes()) {
+			if (node.contains("pom.xml")) {
+				coreTreeItem.getNode(node).doubleClick();
+				break;
+			}
+		}
 
-		SWTBotEditor corePomEditor = bot.editorByTitle(FILE1);
+		SWTBotEditor corePomEditor = bot.editorByTitle("pom.xml");
 		corePomEditor.toTextEditor()
 				.insertText("<!-- EGit jUnit test case -->");
 		corePomEditor.saveAndClose();
+		coreTreeItem.collapse();
 
-		rootNode.getNode(1).select().doubleClick();
-		SWTBotEditor uiPomEditor = bot.editorByTitle(FILE2);
+		SWTBotTreeItem uiTreeItem = tree.getAllItems()[6];
+		uiTreeItem.expand();
+		for (String node : uiTreeItem.getNodes()) {
+			if (node.contains("pom.xml")) {
+				uiTreeItem.getNode(node).doubleClick();
+				break;
+			}
+		}
+
+		SWTBotEditor uiPomEditor = bot.editorByTitle("pom.xml");
 		uiPomEditor.toTextEditor().insertText("<!-- EGit jUnit test case -->");
 		uiPomEditor.saveAndClose();
-		
-		coreTreeItem.collapse();
+		uiTreeItem.collapse();
 	}
 
-	private void resetRepository(String projectName) {
-		showDialog(projectName, "Team", "Reset...");
-
-		bot.shell(UIText.ResetCommand_WizardTitle).bot().activeShell();
-		bot.radio(UIText.ResetTargetSelectionDialog_ResetTypeHardButton)
-				.click();
-		bot.button(UIText.ResetTargetSelectionDialog_ResetButton).click();
-
-		bot.shell(UIText.ResetTargetSelectionDialog_ResetQuestion).bot()
-				.activeShell();
-		bot.button("Yes").click();
-
-	}
-
-	private void createTag(String projectName, String tagName) {
-		showDialog(projectName, "Team", "Tag...");
-
-		bot.shell("Create new tag").bot().activeShell();
-		bot.text(0).setFocus();
-		bot.text(0).setText(tagName);
-		bot.text(1).setFocus();
-		bot.text(1).setText(tagName);
-		bot.button("OK").click();
-	}
-
-	private void makeChangesAndCommit(String projectName) throws Exception {
-		changeFilesInProject();
-
-		showDialog(projectName, "Team", UIText.CommitAction_commit);
-
-		bot.shell(UIText.CommitDialog_CommitChanges).bot().activeShell();
-		bot.styledText(0).setText("test commit");
-		bot.button(UIText.CommitDialog_Commit).click();
-	}
-
-	private void showDialog(String projectName, String... cmd) {
+	private void showSynchronizationDialog(String projectName) {
 		SWTBot packageExplorerBot = bot.viewByTitle("Package Explorer").bot();
 		packageExplorerBot.activeShell();
 		SWTBotTree tree = packageExplorerBot.tree();
@@ -295,7 +316,16 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 			}
 		}
 
-		clickContextMenu(tree, cmd);
+		ContextMenuHelper.clickContextMenu(tree, "Team", "Synchronize...");
+	}
+
+	private void handleConfirmOpenPerspective() {
+		if (!changePerspective) {
+			changePerspective = true;
+			bot.waitUntil(shellIsActive("Confirm Open Perspective"), 15000);
+			bot.checkBox("Remember my decision").click();
+			bot.button("No").click();
+		}
 	}
 
 }
