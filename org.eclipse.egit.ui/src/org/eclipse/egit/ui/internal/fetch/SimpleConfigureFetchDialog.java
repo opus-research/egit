eeb6657c0c0048614915f410e9dbeeffe24da20d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:57:32 +0000

    First commited as before_73638_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/SimpleConfigureFetchDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/SimpleConfigureFetchDialog.java
@@ -1,13 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2011, 2016 Mathias Kinzler <mathias.kinzler@sap.com> and others
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 493935
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.fetch;
 
@@ -23,7 +20,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.UIText;
-import org.eclipse.egit.ui.internal.gerrit.GerritDialogSettings;
 import org.eclipse.egit.ui.internal.push.RefSpecDialog;
 import org.eclipse.egit.ui.internal.push.RefSpecWizard;
 import org.eclipse.egit.ui.internal.repository.SelectUriWizard;
@@ -554,11 +550,6 @@ public class SimpleConfigureFetchDialog extends TitleAreaDialog {
 				repository.getConfig().save();
 			} catch (IOException e) {
 				Activator.handleError(e.getMessage(), e, true);
-			}
-			Repository repo = repository;
-			RemoteConfig rc = config;
-			if (repo != null && rc != null) {
-				GerritDialogSettings.updateRemoteConfig(repo, rc);
 			}
 			if (buttonId == OK)
 				try {
