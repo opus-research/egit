bef1b15e0d976eb9b0c61f482928a9e02014dc8d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:07:09 +0000

    First commited as before_5131_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -79,6 +79,9 @@ public class UIText extends NLS {
 	public static String AddConfigEntryDialog_EnterValueMessage;
 
 	/** */
+	public static String AddConfigEntryDialog_EntryExistsMessage;
+
+	/** */
 	public static String AddConfigEntryDialog_KeyComponentsMessage;
 
 	/** */
@@ -1588,7 +1591,7 @@ public class UIText extends NLS {
 	public static String HistoryPage_authorColumn;
 
 	/** */
-	public static String HistoryPage_authorDateColumn;
+	public static String HistoryPage_dateColumn;
 
 	/** */
 	public static String HistoryPage_refreshJob;
@@ -1891,13 +1894,22 @@ public class UIText extends NLS {
 	public static String ConfigurationChecker_homeNotSet;
 
 	/** */
+	public static String ConfigurationEditorComponent_AddButton;
+
+	/** */
 	public static String ConfigurationEditorComponent_BrowseForPrefix;
 
 	/** */
 	public static String ConfigurationEditorComponent_CannotChangeGitPrefixError;
 
 	/** */
+	public static String ConfigurationEditorComponent_ChangeButton;
+
+	/** */
 	public static String ConfigurationEditorComponent_ConfigLocationLabel;
+
+	/** */
+	public static String ConfigurationEditorComponent_DeleteButton;
 
 	/** */
 	public static String ConfigurationEditorComponent_EmptyStringNotAllowed;
@@ -1912,7 +1924,7 @@ public class UIText extends NLS {
 	public static String ConfigurationEditorComponent_KeyColumnHeader;
 
 	/** */
-	public static String ConfigurationEditorComponent_AddButton;
+	public static String ConfigurationEditorComponent_NewValueButton;
 
 	/** */
 	public static String ConfigurationEditorComponent_NoConfigLocationKnown;
@@ -1933,10 +1945,10 @@ public class UIText extends NLS {
 	public static String ConfigurationEditorComponent_ReadOnlyLocationFormat;
 
 	/** */
-	public static String ConfigurationEditorComponent_RemoveButton;
+	public static String ConfigurationEditorComponent_RemoveAllButton;
 
 	/** */
-	public static String ConfigurationEditorComponent_RemoveTooltip;
+	public static String ConfigurationEditorComponent_RemoveAllTooltip;
 
 	/** */
 	public static String ConfigurationEditorComponent_RemoveSectionMessage;
@@ -1955,6 +1967,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ConfigurationEditorComponent_ValueColumnHeader;
+
+	/** */
+	public static String ConfigurationEditorComponent_ValueLabel;
 
 	/** */
 	public static String ConfigurationEditorComponent_WrongNumberOfTokensMessage;
@@ -2549,12 +2564,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String BranchOperationUI_DetachedHeadMessage;
-
-	/** */
-	public static String BranchResultDialog_buttonCommit;
-
-	/** */
-	public static String BranchResultDialog_buttonReset;
 
 	/** */
 	public static String BranchResultDialog_CheckoutConflictsMessage;
@@ -3488,9 +3497,6 @@ public class UIText extends NLS {
 	public static String CommitGraphTable_Committer;
 
 	/** */
-	public static String CommitGraphTable_committerDataColumn;
-
-	/** */
 	public static String CommitGraphTable_CompareWithEachOtherInTreeMenuLabel;
 
 	/** */
@@ -3743,7 +3749,7 @@ public class UIText extends NLS {
 	public static String RevertFailureDialog_Title;
 
 	/** */
-	public static String RevertHandler_AlreadyRevertedMessage;
+	public static String RevertHandler_AlreadyRevertedMessae;
 
 	/** */
 	public static String RevertHandler_JobName;
