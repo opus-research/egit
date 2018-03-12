9e64960f02513d0bd9029a0bd4fc963721062f3c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:39:35 +0000

    First commited as before_3444_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
@@ -58,16 +58,12 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.part.IPageSite;
 
-/**
- * Viewer to display {@link FileDiff} objects in a table.
- */
-public class CommitFileDiffViewer extends TableViewer {
+class CommitFileDiffViewer extends TableViewer {
 	private static final String LINESEP = System.getProperty("line.separator"); //$NON-NLS-1$
 
 	private Repository db;
@@ -90,7 +86,7 @@ public class CommitFileDiffViewer extends TableViewer {
 
 	private IAction compare;
 
-	private final IWorkbenchSite site;
+	private final IPageSite site;
 
 	/**
 	 * Shows a list of file changed by a commit.
@@ -100,7 +96,7 @@ public class CommitFileDiffViewer extends TableViewer {
 	 * @param parent
 	 * @param site
 	 */
-	public CommitFileDiffViewer(final Composite parent, final IWorkbenchSite site) {
+	CommitFileDiffViewer(final Composite parent, final IPageSite site) {
 		// since out parent is a SashForm, we can't add the alternate
 		// text to be displayed in case of no input directly to that
 		// parent; we create our own parent instead and set the
@@ -227,27 +223,24 @@ public class CommitFileDiffViewer extends TableViewer {
 		mgr.add(selectAll = createStandardAction(ActionFactory.SELECT_ALL));
 		mgr.add(copy = createStandardAction(ActionFactory.COPY));
 
-		if (site instanceof IPageSite) {
-			final IPageSite pageSite = (IPageSite) site;
-			getControl().addFocusListener(new FocusListener() {
-				public void focusLost(FocusEvent e) {
-					pageSite.getActionBars().setGlobalActionHandler(
-							ActionFactory.SELECT_ALL.getId(), null);
-					pageSite.getActionBars().setGlobalActionHandler(
-							ActionFactory.COPY.getId(), null);
-					pageSite.getActionBars().updateActionBars();
-				}
+		getControl().addFocusListener(new FocusListener() {
+			public void focusLost(FocusEvent e) {
+				site.getActionBars().setGlobalActionHandler(
+						ActionFactory.SELECT_ALL.getId(), null);
+				site.getActionBars().setGlobalActionHandler(
+						ActionFactory.COPY.getId(), null);
+				site.getActionBars().updateActionBars();
+			}
 
-				public void focusGained(FocusEvent e) {
-					updateActionEnablement(getSelection());
-					pageSite.getActionBars().setGlobalActionHandler(
-							ActionFactory.SELECT_ALL.getId(), selectAll);
-					pageSite.getActionBars().setGlobalActionHandler(
-							ActionFactory.COPY.getId(), copy);
-					pageSite.getActionBars().updateActionBars();
-				}
-			});
-		}
+			public void focusGained(FocusEvent e) {
+				updateActionEnablement(getSelection());
+				site.getActionBars().setGlobalActionHandler(
+						ActionFactory.SELECT_ALL.getId(), selectAll);
+				site.getActionBars().setGlobalActionHandler(
+						ActionFactory.COPY.getId(), copy);
+				site.getActionBars().updateActionBars();
+			}
+		});
 	}
 
 	private void updateActionEnablement(ISelection selection) {
@@ -390,13 +383,7 @@ public class CommitFileDiffViewer extends TableViewer {
 		return db;
 	}
 
-	/**
-	 * Set repository and tree walk
-	 *
-	 * @param repository
-	 * @param walk
-	 */
-	public void setTreeWalk(Repository repository, TreeWalk walk) {
+	void setTreeWalk(Repository repository, TreeWalk walk) {
 		db = repository;
 		walker = walk;
 	}
