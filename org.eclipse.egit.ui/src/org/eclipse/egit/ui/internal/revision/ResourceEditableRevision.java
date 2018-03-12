668287bdd942eb50e5a148336a45400a09fa561e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:19:37 +0000

    First commited as before_38591_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/revision/ResourceEditableRevision.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/revision/ResourceEditableRevision.java
@@ -53,7 +53,7 @@ public class ResourceEditableRevision extends EditableRevision {
 	@Override
 	public void setContent(final byte[] newContent) {
 		try {
-			runnableContext.run(true, false, new IRunnableWithProgress() {
+			runnableContext.run(false, false, new IRunnableWithProgress() {
 				public void run(IProgressMonitor myMonitor)
 						throws InvocationTargetException, InterruptedException {
 					try {
