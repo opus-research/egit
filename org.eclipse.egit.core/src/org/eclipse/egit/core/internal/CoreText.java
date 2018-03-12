0d0f38a7edd4ce4f035ca3831d596c00bf473b92
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:59:27 +0000

    First commited as before_22670_rev6

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
@@ -214,9 +214,6 @@ public class CoreText extends NLS {
 	public static String MoveDeleteHook_unmergedFileInFolderError;
 
 	/** */
-	public static String MoveDeleteHook_cannotAutoStageDeletion;
-
-	/** */
 	public static String Error_CanonicalFile;
 
 	/** */
