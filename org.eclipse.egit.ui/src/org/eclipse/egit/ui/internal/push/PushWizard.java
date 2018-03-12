1c5e93ce799714df0827871a60adba91b703d7b9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:15:15 +0000

    First commited as before_889_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushWizard.java
@@ -125,7 +125,7 @@ public class PushWizard extends Wizard {
 
 		job.setUser(true);
 		job.schedule();
-		repoPage.saveUriInPrefs();
+		RepositorySelectionPage.saveUriInPrefs(repoPage.getSelection().getURI().toString());
 
 		return true;
 	}
