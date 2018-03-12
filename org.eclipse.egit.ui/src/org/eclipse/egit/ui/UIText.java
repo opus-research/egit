e48c3a2a190510ba987c6453f2b85de071242d78
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:24:51 +0000

    First commited as before_8706_rev5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1563,9 +1563,6 @@ public class UIText extends NLS {
 	/** */
 	public static String RefSpecPage_titlePush;
 
-	/***/
-	public static String RefSpecPage_titlePusxh;
-
 	/** */
 	public static String RefSpecPage_annotatedTagsGroup;
 
