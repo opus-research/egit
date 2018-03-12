29075467aca28e514612918d3521e76329a1a8b1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:47:38 +0000

    First commited as before_64507_rev1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
@@ -312,7 +312,6 @@ public class Activator extends Plugin implements DebugOptionsListener {
 	@Override
 	public void stop(final BundleContext context) throws Exception {
 		GitProjectData.detachFromWorkspace();
-		repositoryCache.dispose();
 		repositoryCache = null;
 		indexDiffCache.dispose();
 		indexDiffCache = null;
