2ab602010dce1c2331bd9a73e52a07280342b1a4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:35:19 +0000

    First commited as before_11381_rev3

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/submodule/SubmoduleSyncTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/submodule/SubmoduleSyncTest.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Copyright (c) 2012, 2013 GitHub Inc and others.
+ *  Copyright (c) 2012 GitHub Inc and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  *  Contributors:
  *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
- *    Laurent Goubet <laurent.goubet@obeo.fr - 404121
  *****************************************************************************/
 package org.eclipse.egit.ui.submodule;
 
@@ -71,7 +70,6 @@ public class SubmoduleSyncTest extends GitRepositoriesViewTestBase {
 		command.setURI(uri);
 		Repository subRepo = command.call();
 		assertNotNull(subRepo);
-		subRepo.close();
 
 		String newUri = "git://server/repo.git";
 		File modulesFile = new File(repo.getWorkTree(),
