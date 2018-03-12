c34d412ea162ed283c15a30ffec0fd369bade85d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:17:00 +0000

    First commited as before_1194_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/sharing/ExistingOrNewPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/sharing/ExistingOrNewPage.java
@@ -40,6 +40,7 @@ import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -146,7 +147,7 @@ class ExistingOrNewPage extends WizardPage {
 		button = new Button(g, SWT.PUSH);
 		button.setLayoutData(GridDataFactory.fillDefaults().create());
 		button.setText(UIText.ExistingOrNewPage_CreateButton);
-		button.addSelectionListener(new SelectionAdapter() {
+		button.addSelectionListener(new SelectionListener() {
 			public void widgetSelected(SelectionEvent e) {
 				File gitDir = new File(repositoryToCreate.getText(),Constants.DOT_GIT);
 				try {
@@ -187,6 +188,8 @@ class ExistingOrNewPage extends WizardPage {
 				updateCreateOptions();
 				getContainer().updateButtons();
 			}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
 		repositoryToCreate = new Text(g, SWT.SINGLE | SWT.BORDER);
 		repositoryToCreate.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).span(1,1).create());
@@ -206,9 +209,12 @@ class ExistingOrNewPage extends WizardPage {
 		dotGitSegment.setText(File.separatorChar + Constants.DOT_GIT);
 		dotGitSegment.setLayoutData(GridDataFactory.fillDefaults().align(SWT.LEFT, SWT.CENTER).create());
 
-		tree.addSelectionListener(new SelectionAdapter() {
+		tree.addSelectionListener(new SelectionListener() {
 			public void widgetSelected(SelectionEvent e) {
 				updateCreateOptions();
+			}
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// Empty
 			}
 		});
 		updateCreateOptions();
