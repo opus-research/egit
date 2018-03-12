2de6905b5cd18f83731adcf23e86da381e78c1c1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:28:51 +0000

    First commited as before_2480_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
@@ -11,7 +11,6 @@ package org.eclipse.egit.ui.internal.synchronize.model;
 import static org.eclipse.compare.structuremergeviewer.Differencer.LEFT;
 import static org.eclipse.compare.structuremergeviewer.Differencer.RIGHT;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -118,29 +117,6 @@ public class GitModelRepository extends GitModelObject {
 	@Override
 	public boolean isContainer() {
 		return true;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == this)
-			return true;
-
-		if (obj instanceof GitModelRepository) {
-			File objWorkTree = ((GitModelRepository) obj).repo.getWorkTree();
-			return objWorkTree.equals(repo.getWorkTree());
-		}
-
-		return false;
-	}
-
-	@Override
-	public int hashCode() {
-		return repo.getWorkTree().hashCode();
-	}
-
-	@Override
-	public String toString() {
-		return "ModelRepository[" + repo.getWorkTree() + "]"; //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	private void getChildrenImpl() {
