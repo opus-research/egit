f30ee06a327247a9ff91642e55d63a479fcfc7ea
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:26:37 +0000

    First commited as before_2290_rev3

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitTestResourceVariantTree.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitTestResourceVariantTree.java
@@ -1,13 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Dariusz Luksza <dariusz@luksza.org>
  *******************************************************************************/
 package org.eclipse.egit.core.synchronize;
 
@@ -15,18 +12,24 @@ import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.team.core.TeamException;
-import org.eclipse.team.core.variants.SessionResourceVariantByteStore;
+import org.eclipse.team.core.variants.ResourceVariantByteStore;
 
-class GitBaseResourceVariantTree extends GitResourceVariantTree {
+/**
+ * Implementation of abstract {@link GitResourceVariantTree} class. It is only
+ * used in {@link GitResourceVariantTreeTest} for testing public methods that
+ * are implemented in base class.
+ */
+class GitTestResourceVariantTree extends GitResourceVariantTree {
 
-	GitBaseResourceVariantTree(GitSynchronizeDataSet gsds) {
-		super(new SessionResourceVariantByteStore(), gsds);
+	GitTestResourceVariantTree(GitSynchronizeDataSet data,
+			ResourceVariantByteStore store) {
+		super(store, data);
 	}
 
 	@Override
 	protected RevCommit getRevCommit(GitSynchronizeData gsd)
 			throws TeamException {
-		return gsd.getCommonAncestorRev();
+		return null;
 	}
 
 }
