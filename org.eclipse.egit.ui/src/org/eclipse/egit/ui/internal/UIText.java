c54f65030f823dd371dbb71c45ab66978e8cdab8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:13:30 +0000

    First commited as before_32364_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -765,6 +765,9 @@ public class UIText extends NLS {
 	public static String RebaseInteractiveStepActionToolBarProvider_UndoText;
 
 	/** */
+	public static String RebaseInteractiveView_HeadingStep;
+
+	/** */
 	public static String RebaseInteractiveView_HeadingAction;
 
 	/** */
@@ -2175,6 +2178,9 @@ public class UIText extends NLS {
 	public static String CommitDialog_SelectAll;
 
 	/** */
+	public static String CommitDialog_SelectForCommit;
+
+	/** */
 	public static String CommitDialog_ShowUntrackedFiles;
 
 	/** */
@@ -2233,6 +2239,15 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitDialog_MessageNoFilesSelected;
+
+	/** */
+	public static String CommitDialog_OpenStagingViewError;
+
+	/** */
+	public static String CommitDialog_OpenStagingViewLink;
+
+	/** */
+	public static String CommitDialog_OpenStagingViewToolTip;
 
 	/** */
 	public static String CommitDialog_Path;
