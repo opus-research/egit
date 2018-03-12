9e7ed18e17c1f7dc839343361b7e31cbefb92164
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:32:36 +0000

    First commited as before_50913_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CompareUtils.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CompareUtils.java
@@ -547,13 +547,7 @@ public class CompareUtils {
 	 */
 	public static void compareHeadWithWorkspace(Repository repository,
 			IFile file) {
-		RepositoryMapping mapping = RepositoryMapping.getMapping(file);
-		if (mapping == null) {
-			Activator.error(NLS.bind(UIText.GitHistoryPage_errorLookingUpPath,
-					file.getLocation(), repository), null);
-			return;
-		}
-		String path = mapping.getRepoRelativePath(
+		String path = RepositoryMapping.getMapping(file).getRepoRelativePath(
 				file);
 		ITypedElement base = getHeadTypedElement(repository, path);
 		if (base == null)
@@ -601,11 +595,6 @@ public class CompareUtils {
 				}
 				final RepositoryMapping mapping = RepositoryMapping
 						.getMapping(file);
-				if (mapping == null) {
-					return Activator.createErrorStatus(
-							NLS.bind(UIText.GitHistoryPage_errorLookingUpPath,
-									file.getLocation(), repository));
-				}
 				final String gitPath = mapping.getRepoRelativePath(file);
 				final ITypedElement base = SaveableCompareEditorInput
 						.createFileElement(file);
@@ -801,11 +790,6 @@ public class CompareUtils {
 				final IFile file = (IFile) resources[0];
 				final RepositoryMapping mapping = RepositoryMapping
 						.getMapping(file);
-				if (mapping == null) {
-					Activator.error(NLS.bind(UIText.GitHistoryPage_errorLookingUpPath,
-							file.getLocation(), repository), null);
-					return;
-				}
 				final String gitPath = mapping.getRepoRelativePath(file);
 
 				compareBetween(repository, gitPath, leftRev, rightRev, page);
@@ -1103,11 +1087,6 @@ public class CompareUtils {
 	public static ITypedElement getIndexTypedElement(final IFile baseFile)
 			throws IOException {
 		final RepositoryMapping mapping = RepositoryMapping.getMapping(baseFile);
-		if (mapping == null) {
-			Activator.error(NLS.bind(UIText.GitHistoryPage_errorLookingUpPath,
-					baseFile.getLocation(), null), null);
-			return null;
-		}
 		final Repository repository = mapping.getRepository();
 		final String gitPath = mapping.getRepoRelativePath(baseFile);
 		final String encoding = CompareCoreUtils.getResourceEncoding(baseFile);
