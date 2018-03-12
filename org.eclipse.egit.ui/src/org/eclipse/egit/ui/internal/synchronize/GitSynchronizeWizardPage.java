aa1c552583c259dbf03020a9f153d3d4551d2ba4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:38:59 +0000

    First commited as before_3403_rev8

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitSynchronizeWizardPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitSynchronizeWizardPage.java
@@ -17,7 +17,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -45,7 +44,6 @@ import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
@@ -199,20 +197,7 @@ class GitSynchronizeWizardPage extends WizardPage {
 
 			@Override
 			protected CellEditor getCellEditor(Object element) {
-				Repository repo = (Repository) element;
-				List<String> refs = new LinkedList<String>(repo.getAllRefs()
-						.keySet());
-
-				List<Ref> additionalRefs;
-				try {
-					additionalRefs = repo.getRefDatabase().getAdditionalRefs();
-				} catch (IOException e) {
-					additionalRefs = null;
-				}
-				if (additionalRefs != null)
-					for (Ref ref : additionalRefs)
-						refs.add(ref.getName());
-
+				Set<String> refs = ((Repository) element).getAllRefs().keySet();
 				branchesEditor.setItems(refs.toArray(new String[refs.size()]));
 
 				return branchesEditor;
