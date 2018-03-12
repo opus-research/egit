9f7ea7f9b71e0cb921b823c9dbc34351dbab0f9c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:17:39 +0000

    First commited as before_6512_rev4

--- org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryMapping.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryMapping.java
@@ -179,19 +179,8 @@ public class RepositoryMapping {
 
 	public String toString() {
 		return "RepositoryMapping[" //$NON-NLS-1$
-				+ nullEmptyOrString(containerPathString)
-				+ " -> '" //$NON-NLS-1$
-				+ nullEmptyOrString(gitDirPathString)
-				+ "', absolute path: '" + getGitDirAbsolutePath() + "' ]"; //$NON-NLS-1$ //$NON-NLS-2$
-	}
-
-	private String nullEmptyOrString(String s) {
-		if (s == null)
-			return "<null>"; //$NON-NLS-1$
-		else if (s.length() == 0)
-			return "<empty>"; //$NON-NLS-1$
-		else
-			return s;
+				+ containerPathString + " -> " //$NON-NLS-1$
+				+ gitDirPathString + "]"; //$NON-NLS-1$
 	}
 
 	/**
