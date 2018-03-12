efb900eca0e9a0a71f875631deaa8be120f48b8b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:53:46 +0000

    First commited as before_17916_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1029,9 +1029,6 @@ public class UIText extends NLS {
 	public static String CheckoutConflictDialog_conflictMessage;
 
 	/** */
-	public static String CheckoutDialog_Message;
-
-	/** */
 	public static String CheckoutDialog_OkCheckout;
 
 	/** */
@@ -1039,9 +1036,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CheckoutDialog_Title;
-
-	/** */
-	public static String CheckoutDialog_WindowTitle;
 
 	/** */
 	public static String CheckoutHandler_SelectBranchMessage;
@@ -1378,6 +1372,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CloneDestinationPage_errorInitialBranchRequired;
+
+	/** */
+	public static String CloneDestinationPage_errorInvalidRemoteName;
 
 	/** */
 	public static String CloneDestinationPage_errorNotEmptyDir;
@@ -1729,6 +1726,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ResetTargetSelectionDialog_CommitterLabel;
+
+	/** */
+	public static String ResetTargetSelectionDialog_DetachedHeadState;
 
 	/** */
 	public static String ResetTargetSelectionDialog_ExpressionLabel;
@@ -2568,6 +2568,9 @@ public class UIText extends NLS {
 	public static String FetchGerritChangePage_AfterFetchGroup;
 
 	/** */
+	public static String FetchGerritChangePage_BranchEditButton;
+
+	/** */
 	public static String FetchGerritChangePage_BranchNameText;
 
 	/** */
@@ -2868,28 +2871,40 @@ public class UIText extends NLS {
 	public static String BranchResultDialog_DetachedHeadWarningDontShowAgain;
 
 	/** */
-	public static String CheckoutDialog_ErrorCouldNotCreateNewRef;
+	public static String BranchSelectionAndEditDialog_Message;
 
 	/** */
-	public static String CheckoutDialog_ErrorCouldNotDeleteRef;
+	public static String BranchSelectionAndEditDialog_Title;
 
 	/** */
-	public static String CheckoutDialog_ErrorCouldNotRenameRef;
+	public static String BranchSelectionAndEditDialog_WindowTitle;
 
 	/** */
-	public static String CheckoutDialog_QuestionNewBranchTitle;
+	public static String BranchSelectionAndEditDialog_OkClose;
 
 	/** */
-	public static String CheckoutDialog_QuestionNewBranchNameMessage;
+	public static String BranchSelectionAndEditDialog_ErrorCouldNotCreateNewRef;
 
 	/** */
-	public static String CheckoutDialog_NewBranch;
+	public static String BranchSelectionAndEditDialog_ErrorCouldNotDeleteRef;
 
 	/** */
-	public static String CheckoutDialog_Rename;
+	public static String BranchSelectionAndEditDialog_ErrorCouldNotRenameRef;
 
 	/** */
-	public static String CheckoutDialog_Delete;
+	public static String BranchSelectionAndEditDialog_QuestionNewBranchTitle;
+
+	/** */
+	public static String BranchSelectionAndEditDialog_QuestionNewBranchNameMessage;
+
+	/** */
+	public static String BranchSelectionAndEditDialog_NewBranch;
+
+	/** */
+	public static String BranchSelectionAndEditDialog_Rename;
+
+	/** */
+	public static String BranchSelectionAndEditDialog_Delete;
 
 	/** */
 	public static String CommitDialogPreferencePage_commitMessageHistory;
@@ -3105,6 +3120,15 @@ public class UIText extends NLS {
 	public static String RepositoriesView_Branches_Nodetext;
 
 	/** */
+	public static String RepositoriesView_CheckoutConfirmationMessage;
+
+	/** */
+	public static String RepositoriesView_CheckoutConfirmationTitle;
+
+	/** */
+	public static String RepositoriesView_CheckoutConfirmationToggleMessage;
+
+	/** */
 	public static String RepositoriesView_ClipboardContentNoGitRepoMessage;
 
 	/** */
@@ -3199,6 +3223,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String DialogsPreferencePage_ShowInitialConfigCheckbox;
+
+	/** */
+	public static String DialogsPreferencePage_ShowCheckoutConfirmation;
 
 	/** */
 	public static String DialogsPreferencePage_ShowCloneFailedDialog;
@@ -4867,6 +4894,15 @@ public class UIText extends NLS {
 	public static String CloneFailureDialog_checkList;
 
 	/** */
+	public static String CloneFailureDialog_checkList_git;
+
+	/** */
+	public static String CloneFailureDialog_checkList_ssh;
+
+	/** */
+	public static String CloneFailureDialog_checkList_https;
+
+	/** */
 	public static String GarbageCollectCommand_jobTitle;
 
 	/** */
@@ -4892,6 +4928,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitScmUrlImportWizardPage_counter;
+
+	/** */
+	public static String BranchEditDialog_Title;
 
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
