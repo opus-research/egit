849edbf3614f96a117f3dce99fee148027b00f83
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:54:59 +0000

    First commited as before_18639_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -128,12 +128,8 @@ public class UIText extends NLS {
 	/** */
 	public static String BlameInformationControl_DiffHeaderLabel;
 
-	public static String BlameInformationControl_OpenCommitLink;
-
 	/** */
 	public static String BlameInformationControl_ShowAnnotationsLink;
-
-	public static String BlameInformationControl_ShowInHistoryLink;
 
 	/** */
 	public static String AssumeUnchanged_assumeUnchanged;
