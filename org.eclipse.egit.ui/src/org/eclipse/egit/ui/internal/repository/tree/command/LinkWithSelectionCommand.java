1a514393ce05a0046ef900b381494fb3da59516a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:45:01 +0000

    First commited as before_3798_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/LinkWithSelectionCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/LinkWithSelectionCommand.java
@@ -14,9 +14,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolItem;
-import org.eclipse.swt.widgets.Widget;
 
 /**
  * Implements "Link With Selection" toggle
@@ -26,17 +24,8 @@ public class LinkWithSelectionCommand extends
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		// is there a better way?
 		Event evt = (Event) event.getTrigger();
-		Widget w = evt.widget;
-		final boolean selected;
-		if (w instanceof ToolItem) {
-			ToolItem item = (ToolItem) w;
-			selected = item.getSelection();
-		} else if (w instanceof MenuItem) {
-			MenuItem item = (MenuItem) w;
-			selected = item.getSelection();
-		} else
-			throw new IllegalStateException(
-					"unexpected type: " + w.getClass().toString()); //$NON-NLS-1$
+		ToolItem item = (ToolItem) evt.widget;
+		boolean selected = item.getSelection();
 		getView(event).setReactOnSelection(selected);
 		return null;
 	}
