9b33a44caebd68c8ebc5967c2d8b07918afffa2f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:45:04 +0000

    First commited as before_61700_rev7

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/factories/GitAdapterFactory.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/factories/GitAdapterFactory.java
@@ -146,14 +146,12 @@ public class GitAdapterFactory implements IAdapterFactory {
 		try {
 			IPath repoRelativePath = gitPath.makeRelativeTo(
 					new Path(repository.getWorkTree().getAbsolutePath()));
-			IProject[] project = ProjectUtil.getProjectsContaining(repository,
+			IProject[] projects = ProjectUtil.getProjectsContaining(repository,
 					Collections.singleton(repoRelativePath.toString()));
-			if (project != null && project.length > 0) {
+			if (projects.length > 0) {
 				IPath projectRelativePath = gitPath
-						.makeRelativeTo(project[0].getLocation());
-				IPath rootRelativePath = project[0].getFullPath()
-						.append(projectRelativePath);
-				return root.getFile(rootRelativePath);
+						.makeRelativeTo(projects[0].getLocation());
+				return projects[0].getFile(projectRelativePath);
 			}
 		} catch (CoreException e) {
 			// Ignore and fall through
