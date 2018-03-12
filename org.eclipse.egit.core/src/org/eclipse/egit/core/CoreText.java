66bff1dbcacd63b9ee3f17bad1c52892a7250e3b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:37:18 +0000

    First commited as before_11988_rev1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -4,7 +4,6 @@
  * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
  * Copyright (C) 2012, Markus Duft <markus.duft@salomon.at>
  * Copyright (C) 2013, Matthias Sohn <matthias.sohn@sap.com>
- * Copyright (C) 2013, Daniel Megert <daniel_megert@ch.ibm.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -150,11 +149,6 @@ public class CoreText extends NLS {
 	/** */
 	public static String GitFileHistory_gitNotAttached;
 
-	/**
-	 * @since 3.0
-	 */
-	public static String GitFileHistory_invalidCommit;
-
 	/** */
 	public static String GitFileHistory_invalidHeadRevision;
 
@@ -290,11 +284,6 @@ public class CoreText extends NLS {
 	/** */
 	public static String IndexDiffCacheEntry_reindexing;
 
-	/**
-	 * @since 3.0
-	 */
-	public static String IndexDiffCacheEntry_reindexingIncrementally;
-
 	/** */
 	public static String IndexFileRevision_errorLookingUpPath;
 
@@ -375,11 +364,6 @@ public class CoreText extends NLS {
 
 	/** */
 	public static String GitResourceVariantTree_fetchingVariant;
-
-	/**
-	 * @since 3.0
-	 */
-	public static String GitResourceVariantTreeSubscriber_CouldNotFindSourceVariant;
 
 	/** */
 	public static String GitBranchResourceVariantTreeSubscriber_gitRepository;
