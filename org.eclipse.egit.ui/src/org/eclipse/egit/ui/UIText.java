cc4d796d0fc0714e809970681a82c7aba67db2e1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:28:43 +0000

    First commited as before_2472_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1612,15 +1612,6 @@ public class UIText extends NLS {
 	public static String GlobalConfigurationPreferencePage_userSettingTabTitle;
 
 	/** */
-	public static String GlobalConfigurationPreferencePage_repositorySettingTabTitle;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_repositorySettingRepositoryLabel;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_repositorySettingNoRepositories;
-
-	/** */
 	public static String ConfigureRemoteWizard_WizardTitle_Change;
 
 	/** */
