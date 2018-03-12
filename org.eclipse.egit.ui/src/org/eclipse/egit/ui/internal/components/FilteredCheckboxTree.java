82b4785168ab619a37f4c7266fe097137b8d608b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:25:38 +0000

    First commited as before_44389_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/FilteredCheckboxTree.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/FilteredCheckboxTree.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 Red Hat, Inc.
+ * Copyright (c) 2010 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *    Chris Aniszczyk <caniszczyk@gmail.com> - initial implementation
- *    Lars Vogel <Lars.Vogel@vogella.com> - Bug 462866
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.components;
 
@@ -72,7 +71,6 @@ public class FilteredCheckboxTree extends FilteredTree {
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.dialogs.FilteredTree#doCreateTreeViewer(org.eclipse.swt.widgets.Composite, int)
 	 */
-	@Override
 	protected TreeViewer doCreateTreeViewer(Composite actParent, int style) {
 		int treeStyle = style | SWT.CHECK | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER;
 		Tree tree = null;
@@ -91,15 +89,12 @@ public class FilteredCheckboxTree extends FilteredTree {
 	 * to synchronous mode before a filter is done.
 	 * @see org.eclipse.ui.dialogs.FilteredTree#doCreateRefreshJob()
 	 */
-	@Override
 	protected WorkbenchJob doCreateRefreshJob() {
 		WorkbenchJob filterJob = super.doCreateRefreshJob();
 		filterJob.addJobChangeListener(new JobChangeAdapter() {
-			@Override
 			public void done(IJobChangeEvent event) {
 				if (event.getResult().isOK()) {
 					getDisplay().asyncExec(new Runnable() {
-						@Override
 						public void run() {
 							if (checkboxViewer.getTree().isDisposed())
 								return;
@@ -115,7 +110,6 @@ public class FilteredCheckboxTree extends FilteredTree {
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.dialogs.FilteredTree#doCreateFilterText(org.eclipse.swt.widgets.Composite)
 	 */
-	@Override
 	protected Text doCreateFilterText(Composite actParent) {
 		// Overridden so the text gets create using the toolkit if we have one
 		Text parentText = super.doCreateFilterText(actParent);
@@ -125,16 +119,6 @@ public class FilteredCheckboxTree extends FilteredTree {
 			return fToolkit.createText(actParent, null, style);
 		}
 		return parentText;
-	}
-
-	@Override
-	protected String getFilterString() {
-		String filterString = super.getFilterString();
-		if (filterString.indexOf("*") != 0 && filterString.indexOf("?") != 0 //$NON-NLS-1$ //$NON-NLS-2$
-				&& filterString.indexOf(".") != 0) {//$NON-NLS-1$
-			filterString = "*" + filterString; //$NON-NLS-1$
-		}
-		return filterString;
 	}
 
 	/**
