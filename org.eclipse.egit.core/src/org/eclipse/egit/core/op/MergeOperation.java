3d519fb2a2f7f192c974a695d54602f297e34531
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:31:05 +0000

    First commited as before_9873_rev1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/MergeOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/MergeOperation.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2010 SAP AG.
- * Copyright (C) 2012, Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
+ * Copyright (C) 2012, 2013 Tomasz Zarna <tzarna@gmail.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,7 +14,6 @@
 package org.eclipse.egit.core.op;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRunnable;
@@ -31,11 +30,13 @@ import org.eclipse.egit.core.CoreText;
 import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.api.MergeCommand.FastForwardMode;
 import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -107,11 +108,14 @@ public class MergeOperation implements IEGitOperation {
 				mymonitor.worked(1);
 				MergeCommand merge;
 				try {
+					FastForwardMode ffmode = getFastForwardMode();
 					Ref ref = repository.getRef(refName);
 					if (ref != null)
-						merge = git.merge().include(ref);
+						merge = git.merge().include(ref).setFastForward(ffmode);
 					else
-						merge = git.merge().include(ObjectId.fromString(refName));
+						merge = git.merge()
+								.include(ObjectId.fromString(refName))
+								.setFastForward(ffmode);
 				} catch (IOException e) {
 					throw new TeamException(CoreText.MergeOperation_InternalError, e);
 				}
@@ -129,17 +133,8 @@ public class MergeOperation implements IEGitOperation {
 				} catch (ConcurrentRefUpdateException e) {
 					throw new TeamException(CoreText.MergeOperation_MergeFailedRefUpdate, e);
 				} catch (CheckoutConflictException e) {
-					StringBuilder builder = new StringBuilder();
-					for (String f : e.getConflictingPaths()) {
-						builder.append("\n"); //$NON-NLS-1$
-						builder.append(f);
-					}
-					throw new TeamException(
-								new Status(
-									IStatus.INFO,
-									Activator.getPluginId(),
-									MessageFormat.format(CoreText.MergeOperation_CheckoutConflict,
-									builder.toString())));
+					mergeResult = new MergeResult(e.getConflictingPaths());
+					return;
 				} catch (GitAPIException e) {
 					throw new TeamException(e.getLocalizedMessage(), e.getCause());
 				} finally {
@@ -153,6 +148,17 @@ public class MergeOperation implements IEGitOperation {
 		ResourcesPlugin.getWorkspace().run(action, monitor);
 	}
 
+	private FastForwardMode getFastForwardMode() throws IOException {
+		FastForwardMode ffmode = FastForwardMode.valueOf(repository.getConfig()
+				.getEnum(ConfigConstants.CONFIG_KEY_MERGE, null,
+						ConfigConstants.CONFIG_KEY_FF,
+						FastForwardMode.Merge.TRUE));
+		ffmode = repository.getConfig().getEnum(
+				ConfigConstants.CONFIG_BRANCH_SECTION, repository.getBranch(),
+				ConfigConstants.CONFIG_KEY_MERGEOPTIONS, ffmode);
+		return ffmode;
+	}
+
 	/**
 	 * @return the merge result, or <code>null</code> if this has not been
 	 *         executed or if an exception occurred
@@ -164,6 +170,4 @@ public class MergeOperation implements IEGitOperation {
 	public ISchedulingRule getSchedulingRule() {
 		return ResourcesPlugin.getWorkspace().getRoot();
 	}
-
-
 }
