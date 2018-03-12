7080c99475e91574453a5159124a9ec34bee371d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:26:48 +0000

    First commited as before_2303_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/mapping/GitTreeTraversal.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/mapping/GitTreeTraversal.java
@@ -103,11 +103,13 @@ class GitTreeTraversal extends ResourceTraversal {
 
 				IResource resource = null;
 				if (objectType == Constants.OBJ_BLOB)
-					resource = ROOT.getFile(childPath.makeRelativeTo(ROOT.getLocation()));
+					resource = ROOT.getFileForLocation(childPath);
 				else if (objectType == Constants.OBJ_TREE) {
 					resource = ROOT.getProject(name);
 					if (resource == null)
 						resource = ROOT.getFolder(childPath.makeRelativeTo(ROOT.getLocation()));
+
+					resource = ROOT.getContainerForLocation(childPath);
 				}
 
 				if (resource != null)
