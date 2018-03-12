2e95ff952d6a8da32c11aa8b69edf4e85b50a63e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:14:44 +0000

    First commited as before_787_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CommitAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CommitAction.java
@@ -144,6 +144,8 @@ public class CommitAction extends RepositoryAction {
 		commitDialog.setPreselectedFiles(getSelectedFiles());
 		commitDialog.setAuthor(author);
 		commitDialog.setCommitter(committer);
+		if(notTracked.size() == files.size())
+			commitDialog.setShowUntracked(true);
 
 		if (previousCommit != null) {
 			commitDialog.setPreviousCommitMessage(previousCommit.getMessage());
