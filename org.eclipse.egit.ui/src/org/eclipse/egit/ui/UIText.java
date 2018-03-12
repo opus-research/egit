7f85491b0dca3c7dbe8bd0ec3adef5c400ce5bf0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:21:01 +0000

    First commited as before_7638_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -4459,9 +4459,6 @@ public class UIText extends NLS {
 	public static String StagingView_CommitMessage;
 
 	/** */
-	public static String StagingView_CommitAndPush;
-
-	/** */
 	public static String StagingView_Committer;
 
 	/** */
