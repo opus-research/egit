1b0540bc8c42da760f69d0114e5f5fed9731cbca
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:37:20 +0000

    First commited as before_3293_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -418,9 +418,6 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_ShowSubMenuLabel;
 
 	/** */
-	public static String GitPreferenceRoot_automaticallyEnableChangesetModel;
-
-	/** */
 	public static String GitPreferenceRoot_CloningRepoGroupHeader;
 
 	/** */
@@ -455,9 +452,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitPreferenceRoot_RepoChangeScannerGroupHeader;
-
-	/** */
-	public static String GitPreferenceRoot_SynchronizeView;
 
 	/** */
 	public static String GitProjectPropertyPage_LabelBranch;
@@ -2921,9 +2915,6 @@ public class UIText extends NLS {
 	public static String CommitActionHandler_repository;
 
 	/** */
-	public static String CommitEditor_TitleHeader;
-
-	/** */
 	public static String CommitEditorInput_Name;
 
 	/** */
@@ -2976,9 +2967,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitGraphTable_CompareWithEachOtherInTreeMenuLabel;
-
-	/** */
-	public static String CommitGraphTable_OpenCommitLabel;
 
 	/** */
 	public static String GitSynchronizeWizard_synchronize;
