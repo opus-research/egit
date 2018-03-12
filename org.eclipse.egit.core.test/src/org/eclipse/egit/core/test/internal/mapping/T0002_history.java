b9d31f23033ee2c7d18e03a2e940d32f48f0fc4c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:12:25 +0000

    First commited as before_309_rev3

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/internal/mapping/T0002_history.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/internal/mapping/T0002_history.java
@@ -9,10 +9,6 @@
  *******************************************************************************/
 package org.eclipse.egit.core.test.internal.mapping;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,6 +19,10 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.egit.core.GitProvider;
 import org.eclipse.egit.core.op.ConnectProviderOperation;
 import org.eclipse.egit.core.test.GitTestCase;
+import org.eclipse.team.core.RepositoryProvider;
+import org.eclipse.team.core.history.IFileHistory;
+import org.eclipse.team.core.history.IFileHistoryProvider;
+import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.jgit.lib.Commit;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.FileTreeEntry;
@@ -32,12 +32,6 @@ import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.Tree;
-import org.eclipse.team.core.RepositoryProvider;
-import org.eclipse.team.core.history.IFileHistory;
-import org.eclipse.team.core.history.IFileHistoryProvider;
-import org.eclipse.team.core.history.IFileRevision;
-import org.junit.Before;
-import org.junit.Test;
 
 public class T0002_history extends GitTestCase {
 
@@ -56,8 +50,8 @@ public class T0002_history extends GitTestCase {
 	private Tree tree;
 	private ObjectWriter objectWriter;
 
-	@Before
-	public void setUp() throws Exception {
+	protected void setUp() throws Exception {
+		super.setUp();
 		project.createSourceFolder();
 		gitDir = new File(project.getProject().getWorkspace().getRoot()
 				.getRawLocation().toFile(), Constants.DOT_GIT);
@@ -125,7 +119,6 @@ public class T0002_history extends GitTestCase {
 		return f;
 	}
 
-	@Test
 	public void testSingleRevision_1() {
 		GitProvider provider = (GitProvider)RepositoryProvider.getProvider(project.project);
 		assertNotNull(provider);
@@ -134,9 +127,9 @@ public class T0002_history extends GitTestCase {
 		IFileRevision fileRevision = fileHistory.getFileRevision("60f0d7917fe2aed5c92b5bc35dfb55b9b8ada359");
 		assertEquals("60f0d7917fe2aed5c92b5bc35dfb55b9b8ada359", fileRevision.getContentIdentifier());
 		assertEquals("J. Author",fileRevision.getAuthor());
+
 	}
 
-	@Test
 	public void testSingleRevision_2() {
 		GitProvider provider = (GitProvider)RepositoryProvider.getProvider(project.project);
 		assertNotNull(provider);
@@ -146,7 +139,6 @@ public class T0002_history extends GitTestCase {
 		assertNull(fileRevision); // not matched by getFileHistoryFor
 	}
 
-	@Test
 	public void testSingleRevision_3() {
 		GitProvider provider = (GitProvider)RepositoryProvider.getProvider(project.project);
 		assertNotNull(provider);
@@ -155,9 +147,9 @@ public class T0002_history extends GitTestCase {
 		IFileRevision fileRevision = fileHistory.getFileRevision("fd5a571c8a3a4a152e4c413d09d3ecb7d41e1e5f");
 		assertEquals("fd5a571c8a3a4a152e4c413d09d3ecb7d41e1e5f", fileRevision.getContentIdentifier());
 		assertEquals("J. Author",fileRevision.getAuthor());
+
 	}
 
-	@Test
 	public void testShallowHistory() {
 		GitProvider provider = (GitProvider)RepositoryProvider.getProvider(project.project);
 		assertNotNull(provider);
@@ -167,9 +159,9 @@ public class T0002_history extends GitTestCase {
 		assertEquals(1, fileRevisions.length);
 		assertEquals("60f0d7917fe2aed5c92b5bc35dfb55b9b8ada359", fileRevisions[0].getContentIdentifier());
 		assertEquals("J. Author",fileRevisions[0].getAuthor());
+
 	}
 
-	@Test
 	public void testDeepHistory_A() {
 		GitProvider provider = (GitProvider)RepositoryProvider.getProvider(project.project);
 		assertNotNull(provider);
@@ -179,9 +171,9 @@ public class T0002_history extends GitTestCase {
 		assertEquals(1, fileRevisions.length);
 		assertEquals("60f0d7917fe2aed5c92b5bc35dfb55b9b8ada359", fileRevisions[0].getContentIdentifier());
 		assertEquals("J. Author",fileRevisions[0].getAuthor());
+
 	}
 
-	@Test
 	public void testDeepHistory_B() {
 		GitProvider provider = (GitProvider)RepositoryProvider.getProvider(project.project);
 		assertNotNull(provider);
@@ -193,5 +185,6 @@ public class T0002_history extends GitTestCase {
 		assertEquals("J. Author",fileRevisions[0].getAuthor());
 		assertEquals("60f0d7917fe2aed5c92b5bc35dfb55b9b8ada359", fileRevisions[1].getContentIdentifier());
 		assertEquals("J. Author",fileRevisions[0].getAuthor());
+
 	}
 }
