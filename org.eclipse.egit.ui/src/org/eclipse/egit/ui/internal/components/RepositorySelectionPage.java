b108975573bce9e8b79b850e9e0d2be72202cb6c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:24:04 +0000

    First commited as before_2138_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -551,7 +551,9 @@ public class RepositorySelectionPage extends WizardPage {
 		userText.setLayoutData(createFieldGridData());
 		userText.addModifyListener(new ModifyListener() {
 			public void modifyText(final ModifyEvent e) {
-				setURI(uri.setUser(nullString(userText.getText())));
+				Protocol protocol = getProtocol();
+				if (protocol != Protocol.HTTP && protocol != Protocol.HTTPS)
+					setURI(uri.setUser(nullString(userText.getText())));
 				user = userText.getText();
 			}
 		});
@@ -867,13 +869,19 @@ public class RepositorySelectionPage extends WizardPage {
 	}
 
 	private void updateAuthGroup() {
-		int idx = scheme.getSelectionIndex();
-		if (idx >= 0) {
-			Protocol p = Protocol.values()[idx];
+		Protocol p = getProtocol();
+		if (p != null) {
 			hostText.setEnabled(p.hasHost());
 			portText.setEnabled(p.hasPort());
 			setEnabledRecursively(authGroup, p.canAuthenticate());
 		}
+	}
+
+	private Protocol getProtocol() {
+		int idx = scheme.getSelectionIndex();
+		if (idx >= 0)
+			return Protocol.values()[idx];
+		return null;
 	}
 
 	@Override
