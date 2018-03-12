dc330e6068d680e94af87adb4faa134f69bb4ccd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:21:45 +0000

    First commited as before_40853_rev5

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
@@ -40,6 +40,7 @@ import org.eclipse.egit.core.EclipseGitProgressTransformer;
 import org.eclipse.egit.core.IteratorService;
 import org.eclipse.egit.core.JobFamilies;
 import org.eclipse.egit.core.internal.CoreText;
+import org.eclipse.egit.core.internal.job.RuleUtil;
 import org.eclipse.egit.core.internal.trace.GitTraceLocation;
 import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.jgit.dircache.DirCache;
@@ -144,14 +145,13 @@ public class IndexDiffCacheEntry {
 	}
 
 	/**
-	 * This method creates (but do not starts) a {@link Job} that refreshes all
-	 * open projects related to the repository and afterwards triggers the
-	 * (asynchronous) recalculation of the {@link IndexDiff}. This ensures that
-	 * the {@link IndexDiff} calculation is not working on out-dated resources.
+	 * This method starts a Job that refreshes all open projects related to the
+	 * repository and afterwards triggers the (asynchronous) recalculation of
+	 * the IndexDiff. This ensures that the IndexDiff calculation is not working
+	 * on out-dated resources.
 	 *
-	 * @return new job ready to be scheduled, never null
 	 */
-	public Job createRefreshResourcesAndIndexDiffJob() {
+	public void refreshResourcesAndIndexDiff() {
 		String repositoryName = Activator.getDefault().getRepositoryUtil()
 				.getRepositoryName(repository);
 		String jobName = MessageFormat
@@ -173,8 +173,8 @@ public class IndexDiffCacheEntry {
 			}
 
 		};
-		job.setRule(ResourcesPlugin.getWorkspace().getRoot());
-		return job;
+		job.setRule(RuleUtil.getRule(repository));
+		job.schedule();
 	}
 
 	/**
