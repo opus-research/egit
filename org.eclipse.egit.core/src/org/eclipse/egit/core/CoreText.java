0a62e85791536460273576d77f9e8fceb378342e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:56:52 +0000

    First commited as before_4618_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -347,9 +347,6 @@ public class CoreText extends NLS {
 	/** */
 	public static String GitRemoteFolder_fetchingMembers;
 
-	/** */
-	public static String GitURI_InvalidURI;
-
 	static {
 		initializeMessages(BUNDLE_NAME,	CoreText.class);
 	}
