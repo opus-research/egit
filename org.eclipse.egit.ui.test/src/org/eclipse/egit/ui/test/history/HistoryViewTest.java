0a7ce92f3e4128f9726875035f689f7aaded6855
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:18:53 +0000

    First commited as before_1438_rev1

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/history/HistoryViewTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/history/HistoryViewTest.java
@@ -29,6 +29,7 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotPerspective;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
@@ -291,7 +292,9 @@ public class HistoryViewTest extends LocalRepositoryTestCase {
 
 		ContextMenuHelper.clickContextMenu(table,
 				UIText.GitHistoryPage_CreateTagMenuLabel);
-		SWTBotShell dialog = bot.shell(UIText.CreateTagDialog_NewTag);
+		SWTBotShell dialog = bot.shell(NLS.bind(
+				UIText.CreateTagDialog_CreateTagOnCommitTitle, commit[0]
+						.getId().name()));
 		dialog.bot().textWithLabel(UIText.CreateTagDialog_tagName).setText(
 				"NewTag");
 		dialog.bot().textWithLabel(UIText.CreateTagDialog_tagMessage).setText(
