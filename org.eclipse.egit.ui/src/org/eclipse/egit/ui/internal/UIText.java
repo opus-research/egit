a9032ff10e2857f59344e756dee3a5fd7e07ece2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:15:39 +0000

    First commited as before_34283_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -5008,9 +5008,6 @@ public class UIText extends NLS {
 	public static String StagingViewContentProvider_SubmoduleError;
 
 	/** */
-	public static String StagingViewContentProvider_WalkSubmodules;
-
-	/** */
 	public static String StashApplyCommand_applyFailed;
 
 	/** */
