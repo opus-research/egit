be100108eab39303a45ee350bb4835652a462e26
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:09:40 +0000

    First commited as before_30185_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1983,6 +1983,9 @@ public class UIText extends NLS {
 	public static String PushAction_wrongURITitle;
 
 	/** */
+	public static String PushBranchPage_CommitLabel;
+
+	/** */
 	public static String PushBranchPage_RemoteBranchNameLabel;
 
 	/** */
