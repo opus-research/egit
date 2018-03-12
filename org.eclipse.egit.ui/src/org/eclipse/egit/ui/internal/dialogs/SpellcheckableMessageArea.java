184bb3ea025188f4a0b66055ee7369b2ba057ffc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:32:28 +0000

    First commited as before_50780_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/SpellcheckableMessageArea.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/SpellcheckableMessageArea.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, 2015 Benjamin Muskalla <bmuskalla@eclipsesource.com> and others.
+ * Copyright (C) 2010, 2013 Benjamin Muskalla <bmuskalla@eclipsesource.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,7 +9,6 @@
  * Contributors:
  *    Benjamin Muskalla (EclipseSource) - initial implementation
  *    Tomasz Zarna (IBM) - show whitespace action, bug 371353
- *    Wayne Beaton (Eclipse Foundation) - Bug 433721
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.dialogs;
 
@@ -1058,7 +1057,7 @@ public class SpellcheckableMessageArea extends Composite {
 	 *
 	 */
 	@Override
-	public boolean forceFocus() {
+	public boolean setFocus() {
 		return getTextWidget().setFocus();
 	}
 
