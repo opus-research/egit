22e201780443839ad43b38e0f2d9a2994e406639
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:39:28 +0000

    First commited as before_3421_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3646,9 +3646,6 @@ public class UIText extends NLS {
 	/** */
 	public static String StagingView_Commit;
 
-	/** */
-	public static String SynchronizeWithMenu_custom;
-
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
 	}
