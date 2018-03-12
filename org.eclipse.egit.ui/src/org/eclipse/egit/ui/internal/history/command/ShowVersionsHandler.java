20cbe27edec21c2ea792d4273dd1bbdc58c145b9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:32:38 +0000

    First commited as before_50949_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/ShowVersionsHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/ShowVersionsHandler.java
@@ -110,36 +110,6 @@ public class ShowVersionsHandler extends AbstractHistoryCommandHandler {
 						ids.add(commit.getId());
 					}
 				}
-<<<<<<< Upstream, based on origin/master
-=======
-				if (rev != null) {
-					if (compareMode) {
-						ITypedElement right = CompareUtils
-								.getFileRevisionTypedElement(commitPath,
-										commit, map.getRepository());
-						final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
-								SaveableCompareEditorInput
-										.createFileElement(resource), right,
-								null);
-						try {
-							CompareUtils.openInCompare(workBenchPage,
-									map.getRepository(), in);
-						} catch (Exception e) {
-							errorOccurred = true;
-						}
-					} else
-						try {
-							EgitUiEditorUtils.openEditor(getPart(event)
-									.getSite().getPage(), rev,
-									new NullProgressMonitor());
-						} catch (CoreException e) {
-							Activator.logError(
-									UIText.GitHistoryPage_openFailed, e);
-							errorOccurred = true;
-						}
-				} else
-					ids.add(commit.getId());
->>>>>>> 310f634 https://bugs.eclipse.org/bugs/show_bug.cgi?id=356832
 			}
 		}
 		if (input instanceof File) {
@@ -173,7 +143,7 @@ public class ShowVersionsHandler extends AbstractHistoryCommandHandler {
 											commit, repo);
 							final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
 									left, right, null);
-							CompareUtils.openInCompare(workBenchPage, repo, in);
+							CompareUtils.openInCompare(workBenchPage, in);
 						} catch (IOException e) {
 							errorOccurred = true;
 						}
