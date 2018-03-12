177db8b42df5565c290e704f1dac72d83a1d418c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:56:43 +0000

    First commited as before_19536_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitImportWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitImportWizard.java
@@ -83,21 +83,9 @@ public class GitImportWizard extends AbstractGitCloneWizard implements IImportWi
 		}
 	};
 
-	private GitProjectsImportPage projectsImportPage = new GitProjectsImportPage() {
-		public void setVisible(boolean visible) {
-			if (visible)
-				setProjectsList(importWithDirectoriesPage.getPath());
-			super.setVisible(visible);
-		}
-	};
+	private GitProjectsImportPage projectsImportPage = new GitProjectsImportPage() ;
 
-	private GitCreateGeneralProjectPage createGeneralProjectPage = new GitCreateGeneralProjectPage() {
-		public void setVisible(boolean visible) {
-			if (visible)
-				setPath(importWithDirectoriesPage.getPath());
-			super.setVisible(visible);
-		}
-	};
+	private GitCreateGeneralProjectPage createGeneralProjectPage = new GitCreateGeneralProjectPage();
 
 	private Repository existingRepo;
 
@@ -160,11 +148,16 @@ public class GitImportWizard extends AbstractGitCloneWizard implements IImportWi
 		} else if (page == importWithDirectoriesPage)
 			switch (importWithDirectoriesPage.getWizardSelection()) {
 			case GitSelectWizardPage.EXISTING_PROJECTS_WIZARD:
+				projectsImportPage.setProjectsList(importWithDirectoriesPage
+						.getPath());
 				return projectsImportPage;
 			case GitSelectWizardPage.NEW_WIZARD:
 				return null;
 			case GitSelectWizardPage.GENERAL_WIZARD:
+				createGeneralProjectPage.setPath(importWithDirectoriesPage
+						.getPath());
 				return createGeneralProjectPage;
+
 			}
 		else if (page == createGeneralProjectPage
 				|| page == projectsImportPage)
