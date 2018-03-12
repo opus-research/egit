3bd296aa595590f4ae9a0f43fdc23ae8c7fdec74
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:07:10 +0000

    First commited as before_28051_rev11

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushBranchActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushBranchActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 Robin Stocker <robin@nibor.org> and others.
+ * Copyright (c) 2013 Robin Stocker <robin@nibor.org> and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,7 +14,6 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.internal.push.PushBranchWizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 
@@ -25,19 +24,11 @@ public class PushBranchActionHandler extends RepositoryActionHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Repository repository = getRepository(true, event);
 
-		try {
-			PushBranchWizard wizard = null;
-			Ref ref = getBranchRef(repository);
-			if (ref != null) {
-				wizard = new PushBranchWizard(repository, ref);
-			} else {
-				ObjectId id = repository.resolve(repository.getFullBranch());
-				wizard = new PushBranchWizard(repository, id);
-			}
+		Ref branchRef = getBranchRef(repository);
+		if (branchRef != null) {
+			PushBranchWizard wizard = new PushBranchWizard(repository, branchRef);
 			WizardDialog dlg = new WizardDialog(getShell(event), wizard);
 			dlg.open();
-		} catch (IOException ex) {
-			// do nothing, return null;
 		}
 
 		return null;
@@ -46,7 +37,9 @@ public class PushBranchActionHandler extends RepositoryActionHandler {
 	@Override
 	public boolean isEnabled() {
 		Repository repository = getRepository();
-		return repository != null;
+		if (repository == null)
+			return false;
+		return getBranchRef(repository) != null;
 	}
 
 	private Ref getBranchRef(Repository repository) {
