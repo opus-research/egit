7e384711621514d2d6d5152522791b97a0cb3352
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:51:01 +0000

    First commited as before_16944_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -3847,9 +3847,6 @@ public class UIText extends NLS {
 	public static String NewRemoteDialog_FetchRadio;
 
 	/** */
-	public static String NewRemoteDialog_InvalidRemoteName;
-
-	/** */
 	public static String NewRemoteDialog_NameLabel;
 
 	/** */
