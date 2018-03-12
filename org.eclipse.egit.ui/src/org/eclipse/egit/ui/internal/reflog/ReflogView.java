b2235ff6ef724a8df47f92910584d1d401729b0e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:52:47 +0000

    First commited as before_4300_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/ReflogView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/ReflogView.java
@@ -12,8 +12,6 @@
 package org.eclipse.egit.ui.internal.reflog;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
@@ -50,7 +48,6 @@ import org.eclipse.jgit.events.RefsChangedEvent;
 import org.eclipse.jgit.events.RefsChangedListener;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
 import org.eclipse.jgit.revwalk.RevWalk;
@@ -102,8 +99,6 @@ public class ReflogView extends ViewPart implements RefsChangedListener {
 	private ISelectionListener selectionChangedListener;
 
 	private ListenerHandle addRefsChangedListener;
-
-	private final DateFormat absoluteFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //$NON-NLS-1$
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -197,32 +192,6 @@ public class ReflogView extends ViewPart implements RefsChangedListener {
 			}
 
 		});
-
-		TreeViewerColumn dateColumn = createColumn(layout,
-				UIText.ReflogView_DateColumnLabel, 15, SWT.LEFT);
-		dateColumn.setLabelProvider(new ColumnLabelProvider() {
-
-			@Override
-			public String getText(Object element) {
-				final ReflogEntry entry = (ReflogEntry) element;
-				final PersonIdent who = entry.getWho();
-				// TODO add option enabling to use RelativeDateFormatter
-				return absoluteFormatter.format(who.getWhen());
-			}
-
-			@Override
-			public String getToolTipText(Object element) {
-				final ReflogEntry entry = (ReflogEntry) element;
-				return entry.getNewId().name();
-			}
-
-			@Override
-			public Image getImage(Object element) {
-				return null;
-			}
-
-		});
-
 		TreeViewerColumn messageColumn = createColumn(layout,
 				UIText.ReflogView_MessageColumnHeader, 50, SWT.LEFT);
 		messageColumn.setLabelProvider(new ColumnLabelProvider() {
@@ -264,31 +233,6 @@ public class ReflogView extends ViewPart implements RefsChangedListener {
 				super.dispose();
 			}
 		});
-
-		TreeViewerColumn whoColumn = createColumn(layout,
-				UIText.ReflogView_CommitterColumnLabel, 15, SWT.LEFT);
-		whoColumn.setLabelProvider(new ColumnLabelProvider() {
-
-			@Override
-			public String getText(Object element) {
-				final ReflogEntry entry = (ReflogEntry) element;
-				final PersonIdent who = entry.getWho();
-				return who.getName() + " <" + who.getEmailAddress() + ">";  //$NON-NLS-1$//$NON-NLS-2$
-			}
-
-			@Override
-			public String getToolTipText(Object element) {
-				final ReflogEntry entry = (ReflogEntry) element;
-				return entry.getNewId().name();
-			}
-
-			@Override
-			public Image getImage(Object element) {
-				return null;
-			}
-
-		});
-
 		refLogTableTreeViewer.addOpenListener(new IOpenListener() {
 
 			public void open(OpenEvent event) {
