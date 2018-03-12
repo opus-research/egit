3e07a94cce0ba81e41e137249dd08060a039ce95
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:58:36 +0000

    First commited as before_21785_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -430,9 +430,6 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_showAllBranches;
 
 	/** */
-	public static String GitHistoryPage_squashMenuItem;
-
-	/** */
 	public static String GitHistoryPage_CheckoutMenuLabel;
 
 	/** */
@@ -497,9 +494,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitHistoryPage_notContainedInCommits;
-
-	/** */
-	public static String GitHistoryPage_ModifyMenuLabel;
 
 	/** */
 	public static String GitHistoryPage_MultiResourcesType;
@@ -752,18 +746,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RebaseInteractiveView_HeadingStatus;
-
-	/** */
-	public static String RebaseInteractiveView_HeadingAuthor;
-
-	/** */
-	public static String RebaseInteractiveView_HeadingAuthorDate;
-
-	/** */
-	public static String RebaseInteractiveView_HeadingCommitter;
-
-	/** */
-	public static String RebaseInteractiveView_HeadingCommitDate;
 
 	/** */
 	public static String RebaseInteractiveView_NoSelection;
@@ -2767,9 +2749,6 @@ public class UIText extends NLS {
 	public static String FetchResultTable_statusRejected;
 
 	/** */
-	public static String FetchResultTable_statusPruned;
-
-	/** */
 	public static String FetchResultTable_statusUpToDate;
 
 	/** */
@@ -4551,24 +4530,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String SkipRebaseCommand_JobName;
-
-	/** */
-	public static String SquashHandler_CommitsNotOnCurrentBranch;
-
-	/** */
-	public static String SquashHandler_Error_Title;
-
-	/** */
-	public static String SquashHandler_ErrorCheckingIfCommitsAreOnCurrentBranch;
-
-	/** */
-	public static String SquashHandler_InternalError;
-
-	/** */
-	public static String SquashHandler_JobName;
-
-	/** */
-	public static String SquashHandler_MessageFromCommitX;
 
 	/** */
 	public static String SwitchToMenu_NewBranchMenuLabel;
