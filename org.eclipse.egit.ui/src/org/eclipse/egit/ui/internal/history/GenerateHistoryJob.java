d1501701ddfa8c7857d2e4d52abafd59fa267e63
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:14:22 +0000

    First commited as before_5899_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GenerateHistoryJob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GenerateHistoryJob.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
- * Copyright (C) 2012, Matthias Sohn <matthias.sohn@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -25,7 +24,6 @@ import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.trace.GitTraceLocation;
 import org.eclipse.jgit.revwalk.RevFlag;
 import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -38,15 +36,11 @@ class GenerateHistoryJob extends Job {
 
 	private final GitHistoryPage page;
 
-	private final SWTCommitList loadedCommits;
-
-	private int itemToLoad = 1;
-
-	private RevCommit commitToLoad;
-
-	private RevCommit commitToShow;
+	private final SWTCommitList allCommits;
 
 	private int lastUpdateCnt;
+
+	private long lastUpdateAt;
 
 	private boolean trace;
 
@@ -58,9 +52,13 @@ class GenerateHistoryJob extends Job {
 						ghp.getInputInternal().getRepository())));
 		page = ghp;
 		this.walk = walk;
-		loadedCommits = new SWTCommitList(control);
-		loadedCommits.source(walk);
+		allCommits = new SWTCommitList(control);
+		allCommits.source(walk);
 		trace = GitTraceLocation.HISTORYVIEW.isActive();
+	}
+
+	public RevWalk getWalk() {
+		return walk;
 	}
 
 	@Override
@@ -73,47 +71,40 @@ class GenerateHistoryJob extends Job {
 						GitTraceLocation.HISTORYVIEW.getLocation());
 			try {
 				for (;;) {
-					int oldsz = loadedCommits.size();
+					final int oldsz = allCommits.size();
 					if (trace)
 						GitTraceLocation.getTrace().trace(
 								GitTraceLocation.HISTORYVIEW.getLocation(),
 								"Filling commit list"); //$NON-NLS-1$
 					// ensure that filling (here) and reading (CommitGraphTable)
 					// the commit list is thread safe
-					synchronized (loadedCommits) {
-						if (commitToLoad != null) {
-							loadedCommits.fillTo(commitToLoad, maxCommits);
-							commitToShow = commitToLoad;
-							commitToLoad = null;
-						} else
-							loadedCommits.fillTo(oldsz + BATCH_SIZE - 1);
+					synchronized (allCommits) {
+						allCommits.fillTo(oldsz + BATCH_SIZE - 1);
 					}
 					if (monitor.isCanceled())
 						return Status.CANCEL_STATUS;
-					final boolean loadIncrementally = !Activator.getDefault().getPreferenceStore()
-							.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_FINDTOOLBAR);
-					if (loadedCommits.size() > itemToLoad + (BATCH_SIZE / 2) + 1 && loadIncrementally)
-						break;
-					if (maxCommits > 0 && loadedCommits.size() > maxCommits)
+					if (maxCommits > 0 && allCommits.size() > maxCommits)
 						incomplete = true;
-					if (incomplete || oldsz == loadedCommits.size())
+					if (incomplete || oldsz == allCommits.size())
 						break;
 
-					if (loadedCommits.size() != 1)
+					if (allCommits.size() != 1)
 						monitor.setTaskName(MessageFormat
 								.format(UIText.GenerateHistoryJob_taskFoundMultipleCommits,
-										Integer.valueOf(loadedCommits.size())));
+										Integer.valueOf(allCommits.size())));
 					else
 						monitor.setTaskName(UIText.GenerateHistoryJob_taskFoundSingleCommit);
+
+					final long now = System.currentTimeMillis();
+					if (now - lastUpdateAt < 2000 && lastUpdateCnt > 0)
+						continue;
+					updateUI(incomplete);
+					lastUpdateAt = now;
 				}
 			} catch (IOException e) {
 				status = new Status(IStatus.ERROR, Activator.getPluginId(),
 						UIText.GenerateHistoryJob_errorComputingHistory, e);
 			}
-			if (trace)
-				GitTraceLocation.getTrace().trace(
-						GitTraceLocation.HISTORYVIEW.getLocation(),
-						"Loaded " + loadedCommits.size() + " commits"); //$NON-NLS-1$ //$NON-NLS-2$
 			updateUI(incomplete);
 		} finally {
 			monitor.done();
@@ -124,20 +115,19 @@ class GenerateHistoryJob extends Job {
 		return status;
 	}
 
-	private void updateUI(boolean incomplete) {
+	void updateUI(boolean incomplete) {
 		if (trace)
 			GitTraceLocation.getTrace().traceEntry(
 					GitTraceLocation.HISTORYVIEW.getLocation());
 		try {
-			if (!incomplete && loadedCommits.size() == lastUpdateCnt)
+			if (!incomplete && allCommits.size() == lastUpdateCnt)
 				return;
 
-			final SWTCommit[] asArray = new SWTCommit[loadedCommits.size()];
-			loadedCommits.toArray(asArray);
+			final SWTCommit[] asArray = new SWTCommit[allCommits.size()];
+			allCommits.toArray(asArray);
 			RevFlag highlightFlag = walk.newFlag("highlight"); //$NON-NLS-1$
-			page.showCommitList(this, loadedCommits, asArray, commitToShow, incomplete, highlightFlag);
-			commitToShow = null;
-			lastUpdateCnt = loadedCommits.size();
+			page.showCommitList(this, allCommits, asArray, incomplete, highlightFlag);
+			lastUpdateCnt = allCommits.size();
 		} finally {
 			if (trace)
 				GitTraceLocation.getTrace().traceExit(
@@ -163,7 +153,7 @@ class GenerateHistoryJob extends Job {
 		Display.getDefault().asyncExec(new Runnable() {
 
 			public void run() {
-				loadedCommits.dispose();
+				allCommits.dispose();
 			}
 		});
 	}
@@ -175,15 +165,4 @@ class GenerateHistoryJob extends Job {
 		return super.belongsTo(family);
 	}
 
-	void setLoadHint(final int index) {
-		itemToLoad = index;
-	}
-
-	void setLoadHint(final RevCommit c) {
-		commitToLoad = c;
-	}
-
-	int loadMoreItemsThreshold() {
-		return loadedCommits.size() - (BATCH_SIZE / 2);
-	}
 }
