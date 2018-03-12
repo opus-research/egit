fd181a6c033608b2a1565950d8fb90004f85165e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:16:07 +0000

    First commited as before_1057_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -11,7 +11,6 @@
 package org.eclipse.egit.ui;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 /**
@@ -42,11 +41,11 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.DECORATOR_RECURSIVE_LIMIT,
 				Integer.MAX_VALUE);
 		store.setDefault(UIPreferences.DECORATOR_FILETEXT_DECORATION,
-				GitLightweightDecorator.DecorationHelper.FILE_FORMAT_DEFAULT);
+				UIText.DecoratorPreferencesPage_fileFormatDefault);
 		store.setDefault(UIPreferences.DECORATOR_FOLDERTEXT_DECORATION,
-				GitLightweightDecorator.DecorationHelper.FOLDER_FORMAT_DEFAULT);
+				UIText.DecoratorPreferencesPage_folderFormatDefault);
 		store.setDefault(UIPreferences.DECORATOR_PROJECTTEXT_DECORATION,
-				GitLightweightDecorator.DecorationHelper.PROJECT_FORMAT_DEFAULT);
+				UIText.DecoratorPreferencesPage_projectFormatDefault);
 		store.setDefault(UIPreferences.DECORATOR_SHOW_TRACKED_ICON, true);
 		store.setDefault(UIPreferences.DECORATOR_SHOW_UNTRACKED_ICON, true);
 		store.setDefault(UIPreferences.DECORATOR_SHOW_STAGED_ICON, true);
