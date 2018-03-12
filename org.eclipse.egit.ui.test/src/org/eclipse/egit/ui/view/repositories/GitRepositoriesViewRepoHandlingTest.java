2e71ce4e5b23e723f969b66c4618523270376a9f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:04:42 +0000

    First commited as before_25288_rev1

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewRepoHandlingTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewRepoHandlingTest.java
@@ -317,7 +317,10 @@ public class GitRepositoriesViewRepoHandlingTest extends
 		SWTBotShell shell = bot.shell(UIText.NewRepositoryWizard_WizardTitle);
 		IPath newPath = new Path(getTestDirectory().getPath());
 		shell.bot().textWithLabel(UIText.CreateRepositoryPage_DirectoryLabel)
-				.setText(newPath.append("NewRepository").toOSString());
+				.setText(newPath.toOSString());
+		shell.bot()
+				.textWithLabel(UIText.CreateRepositoryPage_RepositoryNameLabel)
+				.setText("NewRepository");
 		shell.bot().button(IDialogConstants.FINISH_LABEL).click();
 		refreshAndWait();
 		File repoFile = new File(newPath.append("NewRepository").toFile(),
@@ -331,14 +334,16 @@ public class GitRepositoriesViewRepoHandlingTest extends
 						myUtil.getPluginLocalizedValue("RepoViewCreateRepository.tooltip"))
 				.click();
 		shell = bot.shell(UIText.NewRepositoryWizard_WizardTitle);
-		newPath = new Path(getTestDirectory().getPath()).append("bare").append(
-				"NewBareRepository");
+		newPath = new Path(getTestDirectory().getPath()).append("bare");
+		shell.bot()
+				.textWithLabel(UIText.CreateRepositoryPage_RepositoryNameLabel)
+				.setText("NewBareRepository");
 		shell.bot().textWithLabel(UIText.CreateRepositoryPage_DirectoryLabel)
 				.setText(newPath.toOSString());
 		shell.bot().checkBox(UIText.CreateRepositoryPage_BareCheckbox).select();
 		shell.bot().button(IDialogConstants.FINISH_LABEL).click();
 		refreshAndWait();
-		repoFile = newPath.toFile();
+		repoFile = newPath.append("NewBareRepository").toFile();
 		myRepoViewUtil.getRootItem(getOrOpenView().bot().tree(), repoFile);
 		assertTrue(myRepoViewUtil.lookupRepository(repoFile).isBare());
 	}
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseInteractiveCurrentHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseInteractiveCurrentHandler.java
@@ -39,7 +39,7 @@ public class RebaseInteractiveCurrentHandler extends AbstractRebaseHistoryComman
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-		PlotCommit commit = (PlotCommit) getSelection(event).getFirstElement();
+		PlotCommit commit = (PlotCommit) getSelection(getPage()).getFirstElement();
 		final Repository repository = getRepository(event);
 		if (repository == null)
 			return null;
