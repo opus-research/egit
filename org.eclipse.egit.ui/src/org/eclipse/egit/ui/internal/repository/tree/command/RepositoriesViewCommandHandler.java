6e7e0ef56715dca688ca09f8231ac40907d0d978
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:19:00 +0000

    First commited as before_6919_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RepositoriesViewCommandHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RepositoriesViewCommandHandler.java
@@ -210,18 +210,22 @@ abstract class RepositoriesViewCommandHandler<T> extends AbstractHandler {
 	protected Collection<IPath> getSelectedFileAndFolderPaths(ExecutionEvent event) throws ExecutionException {
 		Collection<IPath> paths = new ArrayList<IPath>();
 		for (Object selectedNode : getSelectedNodes(event)) {
-			RepositoryTreeNode node = (RepositoryTreeNode) selectedNode;
-			Repository repository = node.getRepository();
-			IPath path;
-			if (node.getType().equals(RepositoryTreeNodeType.FOLDER))
-				path = new Path(((FolderNode) node).getObject()
-						.getAbsolutePath());
-			else if (node.getType().equals(RepositoryTreeNodeType.FILE))
-				path = new Path(((FileNode) node).getObject().getAbsolutePath());
-			else
-				path = new Path(repository.getWorkTree().getAbsolutePath());
+			IPath path = getPath((RepositoryTreeNode) selectedNode);
 			paths.add(path);
 		}
 		return paths;
+	}
+
+	protected IPath getPath(RepositoryTreeNode node) {
+		Repository repository = node.getRepository();
+		IPath path;
+		if (node.getType().equals(RepositoryTreeNodeType.FOLDER))
+			path = new Path(((FolderNode) node).getObject()
+					.getAbsolutePath());
+		else if (node.getType().equals(RepositoryTreeNodeType.FILE))
+			path = new Path(((FileNode) node).getObject().getAbsolutePath());
+		else
+			path = new Path(repository.getWorkTree().getAbsolutePath());
+		return path;
 	}
 }
