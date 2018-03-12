7b2f925d316bc170fe185349a3f1ec687cb16142
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:12:13 +0000

    First commited as before_154_rev1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryFinder.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryFinder.java
@@ -13,7 +13,6 @@ package org.eclipse.egit.core.project;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -27,8 +26,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.egit.core.CoreText;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.util.SystemReader;
 
 /**
  * Searches for existing Git repositories associated with a project's files.
@@ -56,8 +53,6 @@ public class RepositoryFinder {
 	private final Collection<RepositoryMapping> results = new ArrayList<RepositoryMapping>();
 	private final Set<File> gitdirs = new HashSet<File>();
 
-	private Set<String> ceilingDirectories = new HashSet<String>();
-
 	/**
 	 * Create a new finder to locate Git repositories for a project.
 	 *
@@ -67,12 +62,6 @@ public class RepositoryFinder {
 	 */
 	public RepositoryFinder(final IProject p) {
 		proj = p;
-		String ceilingDirectoriesVar = SystemReader.getInstance().getenv(
-				Constants.GIT_CEILING_DIRECTORIES_KEY);
-		if (ceilingDirectoriesVar != null) {
-			ceilingDirectories.addAll(Arrays.asList(ceilingDirectoriesVar
-					.split(File.pathSeparator)));
-		}
 	}
 
 	/**
@@ -103,7 +92,6 @@ public class RepositoryFinder {
 		try {
 			if (loc != null) {
 				final File fsLoc = loc.toFile();
-				assert fsLoc.isAbsolute();
 				final File ownCfg = configFor(fsLoc);
 				final IResource[] children;
 
@@ -113,13 +101,10 @@ public class RepositoryFinder {
 				if (c.isLinked() || c instanceof IProject) {
 					File p = fsLoc.getParentFile();
 					while (p != null) {
-						System.out.println("Looking at candidate dir: " + p);
 						final File pCfg = configFor(p);
 						if (pCfg.isFile()) {
 							register(c, pCfg.getParentFile());
 						}
-						if (ceilingDirectories.contains(p.getPath()))
-							break;
 						p = p.getParentFile();
 					}
 				}
--- org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryMapping.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryMapping.java
@@ -83,13 +83,8 @@ public class RepositoryMapping {
 		containerPath = container.getProjectRelativePath().toPortableString();
 
 		if (cLoc.isPrefixOf(gLoc)) {
-			int matchingSegments = gLoc.matchingFirstSegments(cLoc);
-			IPath remainder = gLoc.removeFirstSegments(matchingSegments);
-			String device = remainder.getDevice();
-			if (device == null)
-				gitdirPath = remainder.toPortableString();
-			else
-				gitdirPath = remainder.toPortableString().substring(device.length());
+			gitdirPath = gLoc.removeFirstSegments(
+					gLoc.matchingFirstSegments(cLoc)).toPortableString();
 		} else if (gLocParent.isPrefixOf(cLoc)) {
 			cnt = cLoc.segmentCount() - cLoc.matchingFirstSegments(gLocParent);
 			p = "";
