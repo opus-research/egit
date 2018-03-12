59d1eac271027e6316235bcc8bdcc4109c0010e0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:41:48 +0000

    First commited as before_3615_rev8

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1135,6 +1135,18 @@ public class UIText extends NLS {
 	public static String RefContentProposal_unknownObject;
 
 	/** */
+	public static String ReflogView_ErrorOnOpenCommit;
+
+	/** */
+	public static String ReflogView_FromColumnHeader;
+
+	/** */
+	public static String ReflogView_MessageColumnHeader;
+
+	/** */
+	public static String ReflogView_ToColumnHeader;
+
+	/** */
 	public static String RefSpecDialog_AutoSuggestCheckbox;
 
 	/** */
@@ -2818,9 +2830,6 @@ public class UIText extends NLS {
 	public static String GitImportWizard_WizardTitle;
 
 	/** */
-	public static String GitScopeOperation_GitScopeManager;
-
-	/** */
 	public static String GitSelectRepositoryPage_AddButton;
 
 	/** */
@@ -2939,6 +2948,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String MultiPullResultDialog_OkStatus;
+
+	/** */
+	public static String MultiPullResultDialog_FailedStatus;
 
 	/** */
 	public static String MultiPullResultDialog_OverallStatusColumnHeader;
@@ -3092,12 +3104,6 @@ public class UIText extends NLS {
 	public static String CommitActionHandler_calculatingChanges;
 
 	/** */
-	public static String CommitActionHandler_errorBuildingScope;
-
-	/** */
-	public static String CommitActionHandler_lookingForChanges;
-
-	/** */
 	public static String CommitActionHandler_repository;
 
 	/** */
@@ -3201,6 +3207,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitBranchSynchronizeWizardPage_includeUncommitedChanges;
+
+	/** */
+	public static String GitBranchSynchronizeWizardPage_fetchChangesFromRemote;
 
 	/** */
 	public static String GitBranchSynchronizeWizardPage_selectAll;
@@ -3818,6 +3827,9 @@ public class UIText extends NLS {
 	public static String GitActionContributor_Pull;
 
 	/** */
+	public static String GitLabelProvider_UnableToRetrieveLabel;
+
+	/** */
 	public static String GitVariableResolver_InternalError;
 
 	/** */
@@ -3831,6 +3843,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_UnstagedChanges;
+
+	/** */
+	public static String StagingView_ShowFileNamesFirst;
 
 	/** */
 	public static String StagingView_StagedChanges;
@@ -3873,6 +3888,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_OpenNewCommits;
+
+	/** */
+	public static String StagingView_ColumnLayout;
 
 	/** */
 	public static String StagingView_IndexDiffReload;
