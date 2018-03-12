8f35653f743ee3fce5a1a76db588ac0869ecebbf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:36:20 +0000

    First commited as before_11895_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitActionContributor.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitActionContributor.java
@@ -63,9 +63,6 @@ class GitActionContributor extends SynchronizePageActionGroup {
 	public void fillContextMenu(IMenuManager menu) {
 		IStructuredSelection selection = (IStructuredSelection) getContext()
 				.getSelection();
-		if (selection.isEmpty())
-			return;
-
 		Object element = selection.getFirstElement();
 		IResource resource = ResourceUtil.getResource(element);
 		if (resource != null) {
