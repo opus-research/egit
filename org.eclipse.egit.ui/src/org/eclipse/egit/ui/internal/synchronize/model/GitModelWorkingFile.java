73e84613b7c85323dc9b854627f2bb9a0c3b6b26
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:22:10 +0000

    First commited as before_1930_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelWorkingFile.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelWorkingFile.java
@@ -10,32 +10,19 @@ package org.eclipse.egit.ui.internal.synchronize.model;
 
 import java.io.IOException;
 
-import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.ITypedElement;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.internal.LocalResourceTypedElement;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.osgi.util.NLS;
+import org.eclipse.egit.ui.internal.LocalResourceTypedElement;
 
 class GitModelWorkingFile extends GitModelBlob {
 
 	public GitModelWorkingFile(GitModelObjectContainer parent,
 			RevCommit commit, ObjectId repoId, String name) throws IOException {
 		super(parent, commit, repoId, repoId, null, name);
-	}
-
-	@Override
-	public void prepareInput(CompareConfiguration configuration,
-			IProgressMonitor monitor) throws CoreException {
-		super.prepareInput(configuration, monitor);
-		configuration.setLeftLabel(NLS.bind(
-				UIText.GitModelWorkingFile_localVersion, getLeft().getName()));
 	}
 
 	@Override
