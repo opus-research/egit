c3ca459e80397e7af50dd85137ad9558dff43c12
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:18:14 +0000

    First commited as before_6717_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -675,6 +675,9 @@ public class UIText extends NLS {
 	public static String RebaseCurrentRefCommand_RebasingCurrentJobName;
 
 	/** */
+	public static String RebaseCurrentRefCommand_ErrorGettingCurrentBranchMessage;
+
+	/** */
 	public static String RebaseResultDialog_Aborted;
 
 	/** */
@@ -1006,6 +1009,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CompareTreeView_WorkspaceVersionText;
+
+	/** */
+	public static String CompareTreeView_ShowIn_label;
 
 	/** */
 	public static String CompareUtils_errorGettingEncoding;
@@ -1962,6 +1968,12 @@ public class UIText extends NLS {
 	public static String CommitDialog_Title;
 
 	/** */
+	public static String CommitDialog_PushSectionTitle;
+
+	/** */
+	public static String CommitDialog_PushUpstream;
+
+	/** */
 	public static String ConfigurationChecker_gitPrefixWarningMessage;
 
 	/** */
@@ -2146,6 +2158,12 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitMessageViewer_parent;
+
+	/** */
+	public static String CompareWithHeadActionHandler_NoHeadTitle;
+
+	/** */
+	public static String CompareWithHeadActionHandler_NoHeadMessage;
 
 	/** */
 	public static String CompareWithIndexAction_errorOnAddToIndex;
@@ -2892,6 +2910,9 @@ public class UIText extends NLS {
 	public static String DecoratorPreferencesPage_labelDecorationsLink;
 
 	/** */
+	public static String DecoratorPreferencesPage_colorsAndFontsLink;
+
+	/** */
 	public static String DecoratorPreferencesPage_iconsShowTracked;
 
 	/** */
@@ -2940,6 +2961,9 @@ public class UIText extends NLS {
 	public static String DeleteRepositoryConfirmDialog_DeleteWorkingDirectoryCheckbox;
 
 	/** */
+	public static String DeleteRepositoryConfirmDialog_DeleteProjectsCheckbox;
+
+	/** */
 	public static String DeleteTagCommand_messageConfirmMultipleTag;
 
 	/** */
@@ -2952,10 +2976,13 @@ public class UIText extends NLS {
 	public static String DeleteTagCommand_titleConfirm;
 
 	/** */
-	public static String DeleteResourcesAction_confirmActionTitle;
+	public static String DeleteResourcesOperationUI_confirmActionTitle;
 
 	/** */
-	public static String DeleteResourcesAction_confirmActionMessage;
+	public static String DeleteResourcesOperationUI_confirmActionMessage;
+
+	/** */
+	public static String DeleteResourcesOperationUI_deleteFailed;
 
 	/** */
 	public static String IgnoreActionHandler_addToGitignore;
@@ -3391,6 +3418,15 @@ public class UIText extends NLS {
 
 	/** */
 	public static String MergeTargetSelectionDialog_TitleMergeWithBranch;
+
+	/** */
+	public static String MergeTargetSelectionDialog_MergeTypeGroup;
+
+	/** */
+	public static String MergeTargetSelectionDialog_MergeTypeCommitButton;
+
+	/** */
+	public static String MergeTargetSelectionDialog_MergeTypeSquashButton;
 
 	/** */
 	public static String MixedResetToRevisionAction_mixedReset;
@@ -3919,6 +3955,27 @@ public class UIText extends NLS {
 	public static String RenameBranchOnCommitHandler_SelectBranchDialogTitle;
 
 	/** */
+	public static String RepositoryStatistics_Description;
+
+	/** */
+	public static String RepositoryStatistics_LooseObjects;
+
+	/** */
+	public static String RepositoryStatistics_NrOfObjects;
+
+	/** */
+	public static String RepositoryStatistics_NrOfPackfiles;
+
+	/** */
+	public static String RepositoryStatistics_NrOfRefs;
+
+	/** */
+	public static String RepositoryStatistics_PackedObjects;
+
+	/** */
+	public static String RepositoryStatistics_SpaceNeededOnFilesystem;
+
+	/** */
 	public static String RevertFailureDialog_Message;
 
 	/** */
@@ -4363,6 +4420,12 @@ public class UIText extends NLS {
 	public static String CommitUI_commitFailed;
 
 	/** */
+	public static String CommitUI_pushFailedTitle;
+
+	/** */
+	public static String CommitUI_pushFailedMessage;
+
+	/** */
 	public static String EgitUiEditorUtils_openFailed;
 
 	/** */
@@ -4417,10 +4480,10 @@ public class UIText extends NLS {
 	public static String StagingView_Commit;
 
 	/** */
-	public static String StagingView_checkoutFailed;
+	public static String StagingView_CommitToolTip;
 
 	/** */
-	public static String StagingView_deleteFailed;
+	public static String StagingView_checkoutFailed;
 
 	/** */
 	public static String StagingView_commitFailed;
@@ -4601,6 +4664,15 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CloneFailureDialog_checkList;
+
+	/** */
+	public static String GarbageCollectCommand_jobTitle;
+
+	/** */
+	public static String GarbageCollectCommand_failed;
+
+	/** */
+	public static String GitModelSynchronizeParticipant_initialScopeName;
 
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
@@ -44,7 +44,6 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.TreeFilter;
 import org.eclipse.ui.model.WorkbenchAdapter;
 
 /**
@@ -70,14 +69,13 @@ public class FileDiff extends WorkbenchAdapter {
 	 *
 	 * @param walk
 	 * @param commit
-	 * @param markTreeFilters optional filters for marking entries, see {@link #isMarked(int)}
 	 * @return non-null but possibly empty array of file diffs
 	 * @throws MissingObjectException
 	 * @throws IncorrectObjectTypeException
 	 * @throws CorruptObjectException
 	 * @throws IOException
 	 */
-	public static FileDiff[] compute(final TreeWalk walk, final RevCommit commit, final TreeFilter... markTreeFilters)
+	public static FileDiff[] compute(final TreeWalk walk, final RevCommit commit)
 			throws MissingObjectException, IncorrectObjectTypeException,
 			CorruptObjectException, IOException {
 		final ArrayList<FileDiff> r = new ArrayList<FileDiff>();
@@ -91,7 +89,7 @@ public class FileDiff extends WorkbenchAdapter {
 		}
 
 		if (walk.getTreeCount() <= 2) {
-			List<DiffEntry> entries = DiffEntry.scan(walk, false, markTreeFilters);
+			List<DiffEntry> entries = DiffEntry.scan(walk);
 			for (DiffEntry entry : entries) {
 				final FileDiff d = new FileDiff(commit, entry);
 				r.add(d);
@@ -123,7 +121,6 @@ public class FileDiff extends WorkbenchAdapter {
 					d.blobs[i] = walk.getObjectId(i);
 					d.modes[i] = walk.getFileMode(i);
 				}
-
 				r.add(d);
 			}
 
@@ -294,17 +291,6 @@ public class FileDiff extends WorkbenchAdapter {
 		if (diffEntry.getOldMode() != null)
 			modes.add(diffEntry.getOldMode());
 		return modes.toArray(new FileMode[]{});
-	}
-
-	/**
-	 * Whether the mark tree filter with the specified index matched during scan
-	 * or not, see {@link #compute(TreeWalk, RevCommit, TreeFilter...)}.
-	 *
-	 * @param index the tree filter index to check
-	 * @return true if it was marked, false otherwise
-	 */
-	public boolean isMarked(int index) {
-		return diffEntry != null && diffEntry.isMarked(index);
 	}
 
 	/**
