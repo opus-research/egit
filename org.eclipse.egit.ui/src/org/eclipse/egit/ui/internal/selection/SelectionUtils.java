c5a49ce3e22e7fc09d2b1362e6b4dcf4bd1ab334
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:49:08 +0000

    First commited as before_65965_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/SelectionUtils.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/SelectionUtils.java
@@ -1,16 +1,20 @@
 /*******************************************************************************
- * Copyright (C) 2014, 2015 Robin Stocker <robin@nibor.org> and others.
+ * Copyright (C) 2014, 2016 Robin Stocker <robin@nibor.org> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 486857
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.selection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,7 +26,7 @@ import org.eclipse.core.resources.mapping.ResourceTraversal;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.egit.core.AdapterUtils;
-import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.core.internal.util.ResourceUtil;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.CommonUtils;
 import org.eclipse.egit.ui.internal.UIText;
@@ -44,7 +48,6 @@ import org.eclipse.ui.ISources;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.IHandlerService;
-import org.eclipse.ui.ide.ResourceUtil;
 
 /**
  * Utilities for working with selections.
@@ -200,6 +203,42 @@ public class SelectionUtils {
 	}
 
 	/**
+	 * Determines a set of either {@link IResource}s or {@link IPath}s from a
+	 * selection. For selection contents that adapt to {@link IResource} or
+	 * {@link ResourceMapping}, the containing {@link IResource}s are included
+	 * in the result set; otherwise for selection contents that adapt to
+	 * {@link IPath} these paths are included.
+	 *
+	 * @param selection
+	 *            to process
+	 * @return the set of {@link IResource} and {@link IPath} objects from the
+	 *         selection; not containing {@code null} values
+	 */
+	@NonNull
+	private static Set<Object> getSelectionContents(
+			@NonNull IStructuredSelection selection) {
+		Set<Object> result = new HashSet<>();
+		for (Object o : selection.toList()) {
+			IResource resource = AdapterUtils.adapt(o, IResource.class);
+			if (resource != null) {
+				result.add(resource);
+				continue;
+			}
+			ResourceMapping mapping = AdapterUtils.adapt(o,
+					ResourceMapping.class);
+			if (mapping != null) {
+				result.addAll(extractResourcesFromMapping(mapping));
+			} else {
+				IPath location = AdapterUtils.adapt(o, IPath.class);
+				if (location != null) {
+					result.add(location);
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
 	 * Figure out which repository to use. All selected resources must map to
 	 * the same Git repository.
 	 *
@@ -214,24 +253,20 @@ public class SelectionUtils {
 	@Nullable
 	private static Repository getRepository(boolean warn,
 			@NonNull IStructuredSelection selection, Shell shell) {
-
-		IPath[] locations = getSelectedLocations(selection);
+		Set<Object> elements = getSelectionContents(selection);
 		if (GitTraceLocation.SELECTION.isActive())
 			GitTraceLocation.getTrace().trace(
 					GitTraceLocation.SELECTION.getLocation(), "selection=" //$NON-NLS-1$
-							+ selection + ", locations=" //$NON-NLS-1$
-							+ Arrays.toString(locations));
+							+ selection + ", elements=" + elements.toString()); //$NON-NLS-1$
+
 		boolean hadNull = false;
 		Repository result = null;
-		for (IPath location : locations) {
-			RepositoryMapping mapping = RepositoryMapping.getMapping(location);
-			Repository repo;
-			if (mapping != null) {
-				repo = mapping.getRepository();
-			} else {
-				// location is outside workspace
-				repo = org.eclipse.egit.core.Activator.getDefault()
-						.getRepositoryCache().getRepository(location);
+		for (Object location : elements) {
+			Repository repo = null;
+			if (location instanceof IResource) {
+				repo = ResourceUtil.getRepository((IResource) location);
+			} else if (location instanceof IPath) {
+				repo = ResourceUtil.getRepository((IPath) location);
 			}
 			if (repo == null) {
 				hadNull = true;
@@ -291,7 +326,8 @@ public class SelectionUtils {
 			// Note that there is both a getResource(IEditorInput) as well as a
 			// getResource(Object), which don't do the same thing. We explicitly
 			// want the first here.
-			IResource resource = ResourceUtil.getResource(editorInput);
+			IResource resource = org.eclipse.ui.ide.ResourceUtil
+					.getResource(editorInput);
 			if (resource != null)
 				return new StructuredSelection(resource);
 			if (editorInput instanceof FileRevisionEditorInput) {
