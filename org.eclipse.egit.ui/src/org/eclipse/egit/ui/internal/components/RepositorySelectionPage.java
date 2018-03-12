06bb349668af83cb2508649a15a930d9fae7da59
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:15:29 +0000

    First commited as before_907_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -171,13 +171,8 @@ public class RepositorySelectionPage extends BaseWizardPage {
 			try {
 				if(text != null) {
 					text = text.trim();
-					int index = text.indexOf(' ');
-					if(index > 0)
-						text = text.substring(0,index);
-					if(Transport.canHandleProtocol(new URIish(text), FS.DETECTED)) {
-						if(!text.startsWith("http") || text.contains(".git")) //$NON-NLS-1$ //$NON-NLS-2$
-							preset = text;
-					}
+					if(Transport.canHandleProtocol(new URIish(text), FS.DETECTED))
+						preset = text;
 				}
 			} catch (URISyntaxException e) {
 				preset = null;
