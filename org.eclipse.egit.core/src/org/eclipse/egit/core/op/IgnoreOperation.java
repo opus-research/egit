ad43ed1d11c1282110756e1d322d815f95d0fdea
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:33:21 +0000

    First commited as before_51012_rev8

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
@@ -84,18 +84,16 @@ public class IgnoreOperation implements IEGitOperation {
 		monitor.beginTask(CoreText.IgnoreOperation_taskName, paths.size());
 		try {
 			for (IPath path : paths) {
-				if (monitor.isCanceled()) {
+				if (monitor.isCanceled())
 					break;
-				}
 				// TODO This is pretty inefficient; multiple ignores in
 				// the same directory cause multiple writes.
 
 				// NB This does the same thing in
 				// DecoratableResourceAdapter, but neither currently
 				// consult .gitignore
-				if (RepositoryUtil.canBeAutoIgnored(path)) {
+				if (!RepositoryUtil.isIgnored(path))
 					addIgnore(monitor, path);
-				}
 				monitor.worked(1);
 			}
 			monitor.done();
