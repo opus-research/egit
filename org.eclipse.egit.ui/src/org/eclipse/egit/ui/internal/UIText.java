079f22a1e52ff8f1c20f7b7c7b443696cbd6f359
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:16:28 +0000

    First commited as before_34903_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -11,6 +11,7 @@
  * Copyright (C) 2013, Ben Hammen <hammenb@gmail.com>
  * Copyright (C) 2014, Marc Khouzam <marc.khouzam@ericsson.com>
  * Copyright (C) 2014, Red Hat Inc.
+ * Copyright (C) 2014, Axel Richard <axel.richard@obeo.fr>
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -909,10 +910,16 @@ public class UIText extends NLS {
 	public static String RebaseResultDialog_StatusNothingToCommit;
 
 	/** */
+	public static String RebaseResultDialog_StatusInteractivePrepared;
+
+	/** */
 	public static String RebaseResultDialog_StatusOK;
 
 	/** */
 	public static String RebaseResultDialog_StatusStopped;
+
+	/** */
+	public static String RebaseResultDialog_StatusEdit;
 
 	/** */
 	public static String RebaseResultDialog_StatusUpToDate;
@@ -2574,6 +2581,9 @@ public class UIText extends NLS {
 	public static String PushResultDialog_label;
 
 	/** */
+	public static String PushResultDialog_label_failed;
+
+	/** */
 	public static String PushResultDialog_title;
 
 	/** */
@@ -3346,6 +3356,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RepositoriesView_WorkingDir_treenode;
+
+	/** */
+	public static String RepositoriesViewActionProvider_OpenWithMenu;
 
 	/** */
 	public static String RepositoriesViewContentProvider_ExceptionNodeText;
@@ -4210,10 +4223,16 @@ public class UIText extends NLS {
 	public static String ImportProjectsSelectionInRepositoryRequired;
 
 	/** */
+	public static String ImportChangedProjectsCommand_ImportingChangedProjects;
+
+	/** */
 	public static String InteractiveRebaseView_abortItem_text;
 
 	/** */
 	public static String InteractiveRebaseView_continueItem_text;
+
+	/** */
+	public static String InteractiveRebaseView_LinkSelection;
 
 	/** */
 	public static String InteractiveRebaseView_refreshItem_text;
@@ -5006,6 +5025,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingViewContentProvider_SubmoduleError;
+
+	/** */
+	public static String StagingViewLabelProvider_SymlinkError;
 
 	/** */
 	public static String StashApplyCommand_applyFailed;
