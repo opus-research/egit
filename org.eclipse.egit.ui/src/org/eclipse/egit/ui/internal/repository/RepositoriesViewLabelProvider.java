1ea3fd827e635c95e419cece6bae6df98e8853da
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:13:33 +0000

    First commited as before_579_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewLabelProvider.java
@@ -12,8 +12,6 @@ package org.eclipse.egit.ui.internal.repository;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -29,7 +27,6 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 
@@ -38,11 +35,6 @@ import org.eclipse.swt.widgets.TreeColumn;
  */
 public class RepositoriesViewLabelProvider extends BaseLabelProvider implements
 		ITableLabelProvider {
-
-	/**
-	 * A map of regular images to their decorated counterpart.
-	 */
-	private Map<Image, Image> decoratedImages = new HashMap<Image, Image>();
 
 	// private DefaultInformationControl infoControl;
 
@@ -189,16 +181,6 @@ public class RepositoriesViewLabelProvider extends BaseLabelProvider implements
 		}
 	}
 
-	@Override
-	public void dispose() {
-		// dispose of our decorated images
-		for (Image image : decoratedImages.values()) {
-			image.dispose();
-		}
-		decoratedImages.clear();
-		super.dispose();
-	}
-
 	private Image decorateImage(final Image image, Object element) {
 
 		RepositoryTreeNode node = (RepositoryTreeNode) element;
@@ -211,7 +193,23 @@ public class RepositoriesViewLabelProvider extends BaseLabelProvider implements
 			try {
 				String branch = node.getBranch();
 				if (refName.equals(branch)) {
-					return getDecoratedImage(image);
+					CompositeImageDescriptor cd = new CompositeImageDescriptor() {
+
+						@Override
+						protected Point getSize() {
+							return new Point(image.getBounds().width, image
+									.getBounds().width);
+						}
+
+						@Override
+						protected void drawCompositeImage(int width, int height) {
+							drawImage(image.getImageData(), 0, 0);
+							drawImage(UIIcons.OVR_CHECKEDOUT.getImageData(), 0,
+									0);
+
+						}
+					};
+					return cd.createImage();
 				}
 			} catch (IOException e1) {
 				// simply ignore here
@@ -225,7 +223,23 @@ public class RepositoriesViewLabelProvider extends BaseLabelProvider implements
 			for (IProject proj : ResourcesPlugin.getWorkspace().getRoot()
 					.getProjects()) {
 				if (proj.getLocation().equals(new Path(file.getAbsolutePath()))) {
-					return getDecoratedImage(image);
+					CompositeImageDescriptor cd = new CompositeImageDescriptor() {
+
+						@Override
+						protected Point getSize() {
+							return new Point(image.getBounds().width, image
+									.getBounds().width);
+						}
+
+						@Override
+						protected void drawCompositeImage(int width, int height) {
+							drawImage(image.getImageData(), 0, 0);
+							drawImage(UIIcons.OVR_CHECKEDOUT.getImageData(), 0,
+									0);
+
+						}
+					};
+					return cd.createImage();
 				}
 			}
 			return image;
@@ -233,34 +247,6 @@ public class RepositoriesViewLabelProvider extends BaseLabelProvider implements
 		default:
 			return image;
 		}
-	}
-
-	private Image getDecoratedImage(final Image image) {
-		// check if we have a decorated image yet or not
-		Image decoratedImage = decoratedImages.get(image);
-		if (decoratedImage == null) {
-			// create one
-			CompositeImageDescriptor cd = new CompositeImageDescriptor() {
-
-				@Override
-				protected Point getSize() {
-					Rectangle bounds = image.getBounds();
-					return new Point(bounds.width, bounds.height);
-				}
-
-				@Override
-				protected void drawCompositeImage(int width, int height) {
-					drawImage(image.getImageData(), 0, 0);
-					drawImage(UIIcons.OVR_CHECKEDOUT.getImageData(), 0,
-							0);
-
-				}
-			};
-			decoratedImage = cd.createImage();
-			// store it
-			decoratedImages.put(image, decoratedImage);
-		}
-		return decoratedImage;
 	}
 
 }
