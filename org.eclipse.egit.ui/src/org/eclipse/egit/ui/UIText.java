723afc2a2a9e7461f2ac63ef698fe1b4b1a99401
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:20:49 +0000

    First commited as before_1725_rev6

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1045,24 +1045,6 @@ public class UIText extends NLS {
 	public static String HistoryPreferencePage_title;
 
 	/** */
-	public static String PullCurrentBranchActionHandler_PullCanceledMessage;
-
-	/** */
-	public static String PullCurrentBranchActionHandler_PullCanceledTitle;
-
-	/** */
-	public static String PullCurrentBranchActionHandler_PullJobname;
-
-	/** */
-	public static String PullResultDialog_DialogTitle;
-
-	/** */
-	public static String PullResultDialog_FetchResultGroupHeader;
-
-	/** */
-	public static String PullResultDialog_MergeResultGroupHeader;
-
-	/** */
 	public static String PushAction_wrongURIDescription;
 
 	/** */
