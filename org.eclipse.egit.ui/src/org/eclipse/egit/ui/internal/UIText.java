0f13aa89ccc8ca7b4c449b7723929e7dd502bf6e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:03:08 +0000

    First commited as before_23856_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -571,9 +571,6 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_rewordMenuItem;
 
 	/** */
-	public static String GitHistoryPage_editMenuItem;
-
-	/** */
 	public static String GitHistoryPage_SetAsBaselineMenuLabel;
 
 	/** */
@@ -5058,21 +5055,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RewordHandler_JobName;
-
-	/** */
-	public static String EditHandler_JobName;
-
-	/** */
-	public static String EditHandler_CommitNotOnCurrentBranch;
-
-	/** */
-	public static String EditHandler_Error_Title;
-
-	/** */
-	public static String EditHandler_ErrorCheckingIfCommitIsOnCurrentBranch;
-
-	/** */
-	public static String EditHandler_InternalError;
 
 	/** */
 	public static String SynchronizeCommand_jobName;
