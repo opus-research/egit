5468afa994d75e85fefd49f21ce73418a16704b5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:59:45 +0000

    First commited as before_4824_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
@@ -305,8 +305,24 @@ public class FileDiff extends WorkbenchAdapter {
 		commit = c;
 	}
 
+	/**
+	 * Is this diff a submodule?
+	 *
+	 * @return true if submodule, false otherwise
+	 */
+	protected boolean isSubmodule() {
+		if (diffEntry == null)
+			return false;
+		return diffEntry.getOldMode() == FileMode.GITLINK
+				|| diffEntry.getNewMode() == FileMode.GITLINK;
+	}
+
 	public ImageDescriptor getImageDescriptor(Object object) {
-		final ImageDescriptor base = UIUtils.getEditorImage(getPath());
+		final ImageDescriptor base;
+		if (!isSubmodule())
+			base = UIUtils.getEditorImage(getPath());
+		else
+			base = UIIcons.REPOSITORY;
 		switch (getChange()) {
 		case ADD:
 			return new DecorationOverlayDescriptor(base,
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -4,6 +4,7 @@
  * Copyright (c) 2010, Stefan Lay <stefan.lay@sap.com>
  * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
  * Copyright (C) 2010-2011, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2012, Daniel megert <daniel_megert@ch.ibm.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -595,12 +596,18 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 	// react on changes to the relative date preference
 	private final IPropertyChangeListener listener = new IPropertyChangeListener() {
 		public void propertyChange(PropertyChangeEvent event) {
-			if (UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE.equals(event
-					.getProperty()))
-				if (graph.setRelativeDate(((Boolean) event.getNewValue())
-						.booleanValue()))
+			final String prop = event.getProperty();
+			if (UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE.equals(prop)) {
+				Object oldValue = event.getOldValue();
+				if (oldValue == null || !oldValue.equals(event.getNewValue())) {
+					graph.setRelativeDate(isShowingRelativeDates());
 					graph.getTableView().refresh();
+				}
 			}
+			if (UIPreferences.HISTORY_MAX_BRANCH_LENGTH.equals(prop)
+					|| UIPreferences.HISTORY_MAX_TAG_LENGTH.equals(prop))
+				graph.getTableView().refresh();
+		}
 	};
 
 	/**
@@ -709,8 +716,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 				graphDetailSplit);
 		graph = new CommitGraphTable(graphDetailSplit, getSite(), popupMgr);
 
-		graph.setRelativeDate(Activator.getDefault().getPreferenceStore()
-				.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE));
+		graph.setRelativeDate(isShowingRelativeDates());
 		Activator.getDefault().getPreferenceStore()
 				.addPropertyChangeListener(listener);
 
@@ -1864,5 +1870,9 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 			}
 			job = null;
 		}
+	}
+
+	private boolean isShowingRelativeDates() {
+		return Activator.getDefault().getPreferenceStore().getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE);
 	}
 }
