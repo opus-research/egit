c3eed093552b4ffa50483d4a9fbf601c32be067f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:43:06 +0000

    First commited as before_3674_rev6

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
@@ -156,17 +156,13 @@ public class GitModelRepository extends GitModelObject {
 			RevCommit srcCommit = rw.parseCommit(srcRev);
 
 			if (includeLocal) {
-				GitModelCache gitCache = new GitModelCache(this, srcCommit);
-				int gitCacheLen = gitCache.getChildren().length;
+				GitModelCache gitModelCache = new GitModelCache(this, srcCommit);
+				if (gitModelCache.getChildren().length > 0)
+					result.add(gitModelCache);
 
-				GitModelWorkingTree gitWorkingTree = getLocaWorkingTreeChanges();
-				int gitWorkingTreeLen = gitWorkingTree != null ? gitWorkingTree
-						.getChildren().length : 0;
-
-				if (gitCacheLen > 0 || gitWorkingTreeLen > 0) {
-					result.add(gitCache);
-					result.add(gitWorkingTree);
-				}
+				GitModelWorkingTree gitModelWorkingTree = getLocaWorkingTreeChanges();
+				if (gitModelWorkingTree != null)
+					result.add(gitModelWorkingTree);
 			}
 
 			if (srcRev.equals(dstRev))
@@ -204,7 +200,10 @@ public class GitModelRepository extends GitModelObject {
 
 	private GitModelWorkingTree getLocaWorkingTreeChanges() {
 		try {
-			return new GitModelWorkingTree(this);
+			GitModelWorkingTree gitModelWorkingTree = new GitModelWorkingTree(this);
+
+			if (gitModelWorkingTree.getChildren().length > 0)
+				return gitModelWorkingTree;
 		} catch (IOException e) {
 			Activator.logError(e.getMessage(), e);
 		}
