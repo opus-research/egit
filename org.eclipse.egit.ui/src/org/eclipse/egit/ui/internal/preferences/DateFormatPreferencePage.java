3ca20d5302c1441aa0b286a293c713c9d2a8a101
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:42:14 +0000

    First commited as before_58041_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/DateFormatPreferencePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/DateFormatPreferencePage.java
@@ -176,35 +176,22 @@ public class DateFormatPreferencePage extends FieldEditorPreferencePage
 					if (format == null) {
 						lastCustomValue = dateFormat.getStringValue();
 					}
-					updateFields((String) event.getNewValue());
+					format = fromString((String) event.getNewValue());
+					FormatInfo info = DATA.get(format);
+					formatExplanation.setText(info.explanation);
+					if (format == null) {
+						dateFormat.getTextControl(getFieldEditorParent())
+								.setEnabled(true);
+						dateFormat.setStringValue(lastCustomValue);
+					} else {
+						dateFormat.getTextControl(getFieldEditorParent())
+								.setEnabled(false);
+						dateFormat.setStringValue(info.format);
+						updatePreview(format);
+					}
 				}
 			}
 		});
-	}
-
-	private void updateFields(String newSelection) {
-		GitDateFormatter.Format format = fromString(newSelection);
-		FormatInfo info = DATA.get(format);
-		formatExplanation.setText(info.explanation);
-		if (format == null) {
-			dateFormat.getTextControl(getFieldEditorParent()).setEnabled(true);
-			dateFormat.setStringValue(lastCustomValue);
-		} else {
-			dateFormat.getTextControl(getFieldEditorParent()).setEnabled(false);
-			dateFormat.setStringValue(info.format);
-			updatePreview(format);
-		}
-	}
-
-	@Override
-	protected void performDefaults() {
-		super.performDefaults();
-		// We don't get property changed events when the default values are
-		// restored...
-		lastCustomValue = getPreferenceStore()
-				.getDefaultString(UIPreferences.DATE_FORMAT);
-		updateFields(getPreferenceStore()
-				.getDefaultString(UIPreferences.DATE_FORMAT_CHOICE));
 	}
 
 	private GitDateFormatter.Format fromString(String value) {
