1530b8231544b73c64b0c5c93a4bb42bdfb1d3fa
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:00:45 +0000

    First commited as before_4859_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2803,9 +2803,6 @@ public class UIText extends NLS {
 	public static String RepositoriesViewLabelProvider_RemoteTrackingNodetext;
 
 	/** */
-	public static String RepositoriesViewLabelProvider_SubmodulesNodeText;
-
-	/** */
 	public static String RepositoriesViewLabelProvider_SymbolicRefNodeText;
 
 	/** */
@@ -2921,12 +2918,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitCreateGeneralProjectPage_ProjectNameLabel;
-
-	/** */
-	public static String GitCreatePatchAction_cannotCreatePatch;
-
-	/** */
-	public static String GitCreatePatchAction_workingTreeClean;
 
 	/** */
 	public static String GitCreatePatchWizard_Browse;
