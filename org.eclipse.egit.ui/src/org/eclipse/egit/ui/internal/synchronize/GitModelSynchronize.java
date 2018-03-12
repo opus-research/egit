3462740660d9ab44ef127b04a44c8d571ae40b35
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:23:57 +0000

    First commited as before_8559_rev6

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitModelSynchronize.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitModelSynchronize.java
@@ -42,7 +42,7 @@ import org.eclipse.ui.PlatformUI;
  */
 public class GitModelSynchronize {
 
-	private static final String GIT_PROVIDER_ID = "org.eclipse.egit.core.GitProvider"; //$NON-NLS-1$
+	private static final String providerId = "org.eclipse.egit.core.GitProvider"; //$NON-NLS-1$
 
 	/**
 	 * Launches Git Model synchronization action
@@ -63,20 +63,7 @@ public class GitModelSynchronize {
 	 */
 	public static final void launch(final GitSynchronizeDataSet gsdSet,
 			IResource[] resources) {
-		ResourceMapping[] mappings = getGitResourceMappings(resources);
-
-		launch(gsdSet, mappings);
-	}
-
-	/**
-	 * Launches Git Model synchronization action using the specified resource
-	 * mapping
-	 *
-	 * @param gsdSet
-	 * @param mappings
-	 */
-	public static final void launch(final GitSynchronizeDataSet gsdSet,
-			ResourceMapping[] mappings) {
+		ResourceMapping[] mappings = getSelectedResourceMappings(resources);
 
 		IWorkbenchWindow window = PlatformUI.getWorkbench()
 				.getActiveWorkbenchWindow();
@@ -91,7 +78,7 @@ public class GitModelSynchronize {
 	 * @return the resource mappings that contain resources associated with the
 	 *         given provider
 	 */
-	private static ResourceMapping[] getGitResourceMappings(
+	private static ResourceMapping[] getSelectedResourceMappings(
 			IResource[] elements) {
 		List<ResourceMapping> providerMappings = new ArrayList<ResourceMapping>();
 
@@ -100,7 +87,7 @@ public class GitModelSynchronize {
 			if (adapted != null && adapted instanceof ResourceMapping) {
 				ResourceMapping mapping = (ResourceMapping) adapted;
 
-				if (isMappedToGitProvider(mapping))
+				if (isMappedToProvider(mapping))
 					providerMappings.add(mapping);
 			}
 		}
@@ -132,13 +119,13 @@ public class GitModelSynchronize {
 	 * @return <code>true</code> if resource is mapped to Git provider,
 	 *         <code>false</code> otherwise
 	 */
-	private static boolean isMappedToGitProvider(ResourceMapping element) {
+	private static boolean isMappedToProvider(ResourceMapping element) {
 		IProject[] projects = element.getProjects();
 		for (IProject project: projects) {
 			RepositoryProvider provider = RepositoryProvider
 					.getProvider(project);
 
-			if (provider != null && provider.getID().equals(GIT_PROVIDER_ID))
+			if (provider != null && provider.getID().equals(providerId))
 				return true;
 		}
 		return false;
