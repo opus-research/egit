5ada2bf683646d258e49fb4a8d0093fbb5162b49
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:32:40 +0000

    First commited as before_2797_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/CheckoutCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/CheckoutCommand.java
@@ -32,7 +32,7 @@ public class CheckoutCommand extends
 		Repository repo = node.getRepository();
 		String refName = ref.getLeaf().getName();
 		final BranchOperationUI op;
-		if (refName.startsWith(Constants.R_HEADS))
+		if (refName.startsWith(Constants.R_REFS))
 			op = new BranchOperationUI(repo, ref.getName());
 		else
 			op = new BranchOperationUI(repo, ref.getLeaf().getObjectId());
