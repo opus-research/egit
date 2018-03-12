c5f711f7cbff6d997692c96f4f9b38ddf05d4f92
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:42:03 +0000

    First commited as before_57084_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/DeleteBranchOnCommitHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/DeleteBranchOnCommitHandler.java
@@ -52,9 +52,7 @@ public class DeleteBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 					.size();
 			branchesOfCommit = getBranchesOfCommit(selection, repository, true);
 		} catch (IOException e) {
-			throw new ExecutionException(
-					UIText.AbstractHistoryCommitHandler_cantGetBranches,
-					e);
+			throw new ExecutionException("Could not obtain current Branch", e); //$NON-NLS-1$
 		}
 		// this should have been checked by isEnabled()
 		if (branchesOfCommit.isEmpty())
@@ -182,9 +180,7 @@ public class DeleteBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 			branchesOfCommit = getBranchesOfCommit(getSelection(page),
 					repository, true);
 		} catch (IOException e) {
-			Activator.logError(
-					UIText.AbstractHistoryCommitHandler_cantGetBranches,
-					e);
+			Activator.logError("Could not calculate Enablement", e); //$NON-NLS-1$
 			return false;
 		}
 		return !branchesOfCommit.isEmpty();
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/ReflogView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/ReflogView.java
@@ -10,21 +10,19 @@
  *   EclipseSource - Filtered Viewer
  *   Robin Stocker <robin@nibor.org> - Show In support
  *   Tobias Baumann <tobbaumann@gmail.com> - Bug 475836
- *   Thomas Wolf <thomas.wolf@paranor.ch> - Bug 477248
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.reflog;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicReference;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.CommonUtils;
-import org.eclipse.egit.ui.internal.PreferenceBasedDateFormatter;
 import org.eclipse.egit.ui.internal.UIIcons;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.actions.ResetMenu;
@@ -42,9 +40,7 @@ import org.eclipse.jface.layout.TreeColumnLayout;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
-import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.OpenStrategy;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.ColumnWeightData;
@@ -125,13 +121,10 @@ public class ReflogView extends ViewPart implements RefsChangedListener, IShowIn
 
 	private ListenerHandle addRefsChangedListener;
 
-	private IPropertyChangeListener uiPrefsListener;
-
-	private final AtomicReference<PreferenceBasedDateFormatter> dateFormatter = new AtomicReference<>();
+	private final DateFormat absoluteFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //$NON-NLS-1$
 
 	@Override
 	public void createPartControl(Composite parent) {
-		dateFormatter.set(PreferenceBasedDateFormatter.create());
 		GridLayoutFactory.fillDefaults().applyTo(parent);
 
 		toolkit = new FormToolkit(parent.getDisplay());
@@ -233,7 +226,7 @@ public class ReflogView extends ViewPart implements RefsChangedListener, IShowIn
 				final ReflogEntry entry = (ReflogEntry) element;
 				final PersonIdent who = entry.getWho();
 				// TODO add option to use RelativeDateFormatter
-				return dateFormatter.get().formatDate(who);
+				return absoluteFormatter.format(who.getWhen());
 			}
 
 			@Override
@@ -324,19 +317,6 @@ public class ReflogView extends ViewPart implements RefsChangedListener, IShowIn
 			}
 		};
 
-		uiPrefsListener = new IPropertyChangeListener() {
-			@Override
-			public void propertyChange(PropertyChangeEvent event) {
-				String property = event.getProperty();
-				if (UIPreferences.DATE_FORMAT.equals(property)
-						|| UIPreferences.DATE_FORMAT_CHOICE.equals(property)) {
-					dateFormatter.set(PreferenceBasedDateFormatter.create());
-					refLogTableTreeViewer.refresh();
-				}
-			}
-		};
-		Activator.getDefault().getPreferenceStore()
-				.addPropertyChangeListener(uiPrefsListener);
 		selectionChangedListener = new ISelectionListener() {
 			@Override
 			public void selectionChanged(IWorkbenchPart part,
@@ -394,11 +374,8 @@ public class ReflogView extends ViewPart implements RefsChangedListener, IShowIn
 		super.dispose();
 		ISelectionService service = CommonUtils.getService(getSite(), ISelectionService.class);
 		service.removePostSelectionListener(selectionChangedListener);
-		if (addRefsChangedListener != null) {
+		if (addRefsChangedListener != null)
 			addRefsChangedListener.remove();
-		}
-		Activator.getDefault().getPreferenceStore()
-				.removePropertyChangeListener(uiPrefsListener);
 	}
 
 	private void reactOnSelection(ISelection selection) {
