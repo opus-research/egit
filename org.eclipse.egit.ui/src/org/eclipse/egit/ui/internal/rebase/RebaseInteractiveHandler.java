0c32fd23617e5d8c7ac851b5cc57eb5216051c15
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:52:47 +0000

    First commited as before_17770_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveHandler.java
@@ -12,12 +12,8 @@ package org.eclipse.egit.ui.internal.rebase;
 
 import java.util.List;
 
-import org.eclipse.egit.ui.internal.UIText;
-import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.api.RebaseCommand.InteractiveHandler;
 import org.eclipse.jgit.lib.RebaseTodoLine;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * Singleton {@link InteractiveHandler}
@@ -28,23 +24,9 @@ public enum RebaseInteractiveHandler implements InteractiveHandler {
 	 */
 	INSTANCE;
 
-	public String modifyCommitMessage(final String commitMessage) {
-		final String[] result = new String[1];
-		result[0] = commitMessage;
-		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-
-			public void run() {
-				InputDialog dialog = new InputDialog(PlatformUI.getWorkbench()
-						.getDisplay().getActiveShell(),
-						UIText.RebaseInteractiveHandler_EditMessageDialogTitle,
-						UIText.RebaseInteractiveHandler_EditMessageDialogText,
-						commitMessage, null);
-				if (dialog.open() == Window.OK) {
-					result[0] = dialog.getValue();
-				}
-			}
-		});
-		return result[0];
+	public String modifyCommitMessage(String commit) {
+		// TODO show a CommitMessageDialog to change the commit message
+		return commit;
 	}
 
 	public void prepareSteps(List<RebaseTodoLine> steps) {
