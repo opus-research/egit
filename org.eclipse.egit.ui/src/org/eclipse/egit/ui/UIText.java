39af0a596249efebbee0bf53f38c1f4a7a4dac95
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:13:06 +0000

    First commited as before_524_rev8

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -832,6 +832,33 @@ public class UIText extends NLS {
 	public static String ResetQuickdiffBaselineHeadParentAction_0;
 
 	/** */
+	public static String ResetTargetSelectionDialog_ResetButton;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetConfirmQuestion;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetQuestion;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetTitle;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetTypeGroup;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetTypeHardButton;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetTypeMixedButton;
+
+	/** */
+	public static String ResetTargetSelectionDialog_ResetTypeSoftButton;
+
+	/** */
+	public static String ResetTargetSelectionDialog_SelectBranchForResetMessage;
+
+	/** */
 	public static String ResourceHistory_toggleCommentWrap;
 
 	/** */
@@ -1079,12 +1106,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitDialog_StatusUntracked;
-
-	/** */
-	public static String CommitDialog_AddChangeIdLabel;
-
-	/** */
-	public static String CommitDialog_AddChangeIdTooltip;
 
 	/** */
 	public static String ConfigureKeysDialog_AddStandardButton;
@@ -1420,34 +1441,10 @@ public class UIText extends NLS {
 	public static String BranchSelectionDialog_TitleCheckout;
 
 	/** */
-	public static String BranchSelectionDialog_TitleReset;
-
-	/** */
-	public static String BranchSelectionDialog_OkReset;
-
-	/** */
 	public static String BranchSelectionDialog_ErrorCouldNotCreateNewRef;
 
 	/** */
 	public static String BranchSelectionDialog_ErrorCouldNotRenameRef;
-
-	/** */
-	public static String BranchSelectionDialog_ResetType;
-
-	/** */
-	public static String BranchSelectionDialog_ResetTypeSoft;
-
-	/** */
-	public static String BranchSelectionDialog_ResetTypeMixed;
-
-	/** */
-	public static String BranchSelectionDialog_ResetTypeHard;
-
-	/** */
-	public static String BranchSelectionDialog_ReallyResetTitle;
-
-	/** */
-	public static String BranchSelectionDialog_ReallyResetMessage;
 
 	/** */
 	public static String BranchSelectionDialog_QuestionNewBranchTitle;
@@ -2018,6 +2015,15 @@ public class UIText extends NLS {
 
 	/** */
 	public static String MergeAction_JobNameMerge;
+
+	/** */
+	public static String MergeAction_MergeCanceledMessage;
+
+	/** */
+	public static String MergeAction_MergeCanceledTitle;
+
+	/** */
+	public static String MergeAction_MergeResultTitle;
 
 	/** */
 	public static String MergeAction_ProblemMerge;
