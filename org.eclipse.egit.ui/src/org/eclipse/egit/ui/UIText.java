49bb36b75f68160203ba9518b8a0fec2b6eb39b5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:06:51 +0000

    First commited as before_5119_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -541,6 +541,9 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_ListIncompleteWarningMessage;
 
 	/** */
+	public static String GitHistoryPage_pushCommit;
+
+	/** */
 	public static String GitHistoryPage_ShowSubMenuLabel;
 
 	/** */
@@ -2548,6 +2551,9 @@ public class UIText extends NLS {
 	public static String WindowCachePreferencePage_packedGitMMAP;
 
 	/** */
+	public static String WindowCachePreferencePage_streamFileThreshold;
+
+	/** */
 	public static String BasicConfigurationDialog_DialogMessage;
 
 	/** */
@@ -4007,6 +4013,18 @@ public class UIText extends NLS {
 	public static String SimplePushActionHandler_NothingToPushDialogTitle;
 
 	/** */
+	public static String SimplePushSpecPage_message;
+
+	/** */
+	public static String SimplePushSpecPage_pushAheadInfo;
+
+	/** */
+	public static String SimplePushSpecPage_TargetRefName;
+
+	/** */
+	public static String SimplePushSpecPage_title;
+
+	/** */
 	public static String SkipRebaseCommand_CancelDialogMessage;
 
 	/** */
@@ -4217,6 +4235,9 @@ public class UIText extends NLS {
 	public static String StagingView_Commit;
 
 	/** */
+	public static String StagingView_checkoutFailed;
+
+	/** */
 	public static String StagingView_commitFailed;
 
 	/** */
@@ -4248,6 +4269,12 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_exceptionMessage;
+
+	/** */
+	public static String StagingView_replaceWithFileInGitIndex;
+
+	/** */
+	public static String StagingView_replaceWithHeadRevision;
 
 	/** */
 	public static String StagingView_UnstageItemMenuLabel;
