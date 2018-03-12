1a28ab3551185055a91660b8feb541cf014a91d8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:59:13 +0000

    First commited as before_4774_rev5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/command/OpenInCommitViewerHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/command/OpenInCommitViewerHandler.java
@@ -10,12 +10,10 @@ package org.eclipse.egit.ui.internal.reflog.command;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.commit.CommitEditor;
 import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.ui.PartInitException;
 
 /**
  * Handler to open commit in commit viewer
@@ -25,13 +23,8 @@ public class OpenInCommitViewerHandler extends AbstractReflogCommandHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Repository repository = getRepository(event);
 		RevCommit commit = getSelectedCommit(event, repository);
-		if (commit != null) {
-			try {
-				CommitEditor.open(new RepositoryCommit(repository, commit));
-			} catch (PartInitException e) {
-				Activator.showError("Error opening commit viewer", e); //$NON-NLS-1$
-			}
-		}
+		if (commit != null)
+				CommitEditor.openQuiet((new RepositoryCommit(repository, commit)));
 		return null;
 	}
 
