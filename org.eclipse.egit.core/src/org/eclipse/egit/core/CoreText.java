45fdc5041cd35d58988b3b55f5d811a064060567
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:26:55 +0000

    First commited as before_2306_rev3

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -60,9 +60,6 @@ public class CoreText extends NLS {
 	public static String ConnectProviderOperation_ConnectingProject;
 
 	/** */
-	public static String ContainerTreeIterator_exceptionMessage;
-
-	/** */
 	public static String DeleteBranchOperation_TaskName;
 
 	/** */
