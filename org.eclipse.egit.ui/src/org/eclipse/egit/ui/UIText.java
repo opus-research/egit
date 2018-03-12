80633611e22db2a9bc0268a581b98607fc8eaa25
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:10:00 +0000

    First commited as before_5345_rev19

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -426,6 +426,9 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_CheckoutMenuLabel;
 
 	/** */
+	public static String GitHistoryPage_CheckoutMenuLabel2;
+
+	/** */
 	public static String GitHistoryPage_CompareModeMenuLabel;
 
 	/** */
@@ -898,27 +901,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RepositorySearchDialog_SomeDirectoriesHiddenMessage;
-
-	/** */
-	public static String CleanRepositoryPage_cleanDirs;
-
-	/** */
-	public static String CleanRepositoryPage_cleanFiles;
-
-	/** */
-	public static String CleanRepositoryPage_cleaningItems;
-
-	/** */
-	public static String CleanRepositoryPage_findingItems;
-
-	/** */
-	public static String CleanRepositoryPage_includeIgnored;
-
-	/** */
-	public static String CleanRepositoryPage_message;
-
-	/** */
-	public static String CleanRepositoryPage_title;
 
 	/** */
 	public static String ClearCredentialsCommand_clearingCredentialsFailed;
