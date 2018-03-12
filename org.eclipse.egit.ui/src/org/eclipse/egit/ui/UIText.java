04c5bb23ba59372a5cf143d894047f632f03cec1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:33:05 +0000

    First commited as before_2897_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -538,6 +538,9 @@ public class UIText extends NLS {
 	public static String RebaseResultDialog_FastForward;
 
 	/** */
+	public static String RebaseResultDialog_Failed;
+
+	/** */
 	public static String RebaseResultDialog_NextSteps;
 
 	/** */
@@ -1676,6 +1679,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GlobalConfigurationPreferencePage_repositorySettingNoRepositories;
+
+	/** */
+	public static String ConfigureRemoteWizard_WizardTitle_Change;
 
 	/** */
 	public static String ConfigureRemoteWizard_WizardTitle_New;
@@ -3345,6 +3351,12 @@ public class UIText extends NLS {
 
 	/** */
 	public static String SkipRebaseCommand_JobName;
+
+	/** */
+	public static String SwitchToMenu_NewBranchMenuLabel;
+
+	/** */
+	public static String SwitchToMenu_OtherMenuLabel;
 
 	/** */
 	public static String SynchronizeWithAction_localRepoName;
