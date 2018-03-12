1bad88a2d4313e0065318e62ca14ee87e20d9bab
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:23:03 +0000

    First commited as before_8403_rev4

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -1,7 +1,6 @@
 /*******************************************************************************
  * Copyright (C) 2008, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
- * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -65,9 +64,6 @@ public class CoreText extends NLS {
 
 	/** */
 	public static String CommitOperation_couldNotFindRepositoryMapping;
-
-	/** */
-	public static String CommitOperation_errorParsingPersonIdent;
 
 	/** */
 	public static String ConfigureFetchAfterCloneTask_couldNotFetch;
