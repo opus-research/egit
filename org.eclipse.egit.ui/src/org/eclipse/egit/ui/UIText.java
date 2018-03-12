9848b6be4687b20610978430c30df31472011a9f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:20:24 +0000

    First commited as before_1678_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2188,12 +2188,6 @@ public class UIText extends NLS {
 	public static String CreateTagDialog_CreateTagOnCommitTitle;
 
 	/** */
-	public static String CreateTagDialog_GetTagJobName;
-
-	/** */
-	public static String CreateTagDialog_LoadingMessageText;
-
-	/** */
 	public static String CreateTagDialog_Message;
 
 	/** */
