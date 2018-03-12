30c1cc31475470298bb1088095270752ecd997e1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:49:06 +0000

    First commited as before_16759_rev5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -516,9 +516,6 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_rebaseMenuItem;
 
 	/** */
-	public static String GitHistoryPage_rebaseInteractiveMenuItem;
-
-	/** */
 	public static String GitHistoryPage_SetAsBaselineMenuLabel;
 
 	/** */
