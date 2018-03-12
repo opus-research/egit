92298597a18f082a23e838a18451007af8c466b5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:04:28 +0000

    First commited as before_5003_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -94,9 +94,6 @@ public class UIText extends NLS {
 	public static String AddConfigEntryDialog_ValueLabel;
 
 	/** */
-	public static String AddSubmoduleWizard_WindowTitle;
-
-	/** */
 	public static String AddToIndexAction_addingFiles;
 
 	/** */
@@ -4188,24 +4185,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_StageItemMenuLabel;
-
-	/** */
-	public static String SubmoduleAddCommand_AddError;
-
-	/** */
-	public static String SubmoduleAddCommand_JobTitle;
-
-	/** */
-	public static String SubmodulePathWizardPage_ErrorPathMustBeEmpty;
-
-	/** */
-	public static String SubmodulePathWizardPage_Message;
-
-	/** */
-	public static String SubmodulePathWizardPage_PathLabel;
-
-	/** */
-	public static String SubmodulePathWizardPage_Title;
 
 	/** */
 	public static String SubmoduleSyncCommand_SyncError;
