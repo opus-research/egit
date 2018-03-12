588dcd6114641a7b54075956748c638414eeb7b2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:21:30 +0000

    First commited as before_1788_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
@@ -37,7 +37,7 @@ public class UIPreferences {
 	/** */
 	public final static String RESOURCEHISTORY_SHOW_ALL_BRANCHES = "resourcehistory_show_all_branches"; //$NON-NLS-1$
 	/** */
-	public final static String RESOURCEHISTORY_COMPARE_MODE = "resourcehistory_compare_more"; //$NON-NLS-1$
+	public final static String RESOURCEHISTORY_COMPARE_MODE = "resourcehistory_compare_mode"; //$NON-NLS-1$
 	/** */
 	public final static String FINDTOOLBAR_IGNORE_CASE = "findtoolbar_ignore_case"; //$NON-NLS-1$
 	/** */
