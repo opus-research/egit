58df23ef62b34558a142975f61c88e6e31278ac8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:33:52 +0000

    First commited as before_2936_rev6

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -159,6 +159,9 @@ public class CoreText extends NLS {
 	public static String ResetOperation_resetMergeFailed;
 
 	/** */
+	public static String ResetOperation_resetCherryPickFailed;
+
+	/** */
 	public static String ResetOperation_updatingFailed;
 
 	/** */
@@ -213,6 +216,15 @@ public class CoreText extends NLS {
 	public static String ProjectUtil_refreshing;
 
 	/** */
+	public static String PullOperation_DetachedHeadMessage;
+
+	/** */
+	public static String PullOperation_PullNotConfiguredMessage;
+
+	/** */
+	public static String PullOperation_TaskName;
+
+	/** */
 	public static String PushOperation_InternalExceptionOccurredMessage;
 
 	/** */
@@ -259,18 +271,6 @@ public class CoreText extends NLS {
 
 	/** */
 	public static String OperationNotYetExecuted;
-
-	/** */
-	public static String GitProjectSetCapability_InvalidTokensCount;
-
-	/** */
-	public static String GitProjectSetCapability_CloneToExistingDirectory;
-
-	/** */
-	public static String GitProjectSetCapability_ExportCouldNotGetBranch;
-
-	/** */
-	public static String GitProjectSetCapability_ExportNoRemote;
 
 	/** */
 	public static String IgnoreOperation_error;
