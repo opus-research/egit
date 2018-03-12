df5f60c2297db5598cdc4d340a5518a12f41e124
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:36:07 +0000

    First commited as before_3187_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2496,12 +2496,6 @@ public class UIText extends NLS {
 	/** */
 	public static String DialogsPreferencePage_ShowInitialConfigCheckbox;
 
-	public static String DiffEditorPage_TaskGeneratingDiff;
-
-	public static String DiffEditorPage_TaskUpdatingViewer;
-
-	public static String DiffEditorPage_Title;
-
 	/** */
 	public static String DiscardChangesAction_confirmActionTitle;
 
