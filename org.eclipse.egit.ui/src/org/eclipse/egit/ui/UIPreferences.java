8273a121df7d20150ba9fbe6d1261708e5886be6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:47:50 +0000

    First commited as before_3966_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
@@ -142,9 +142,13 @@ public class UIPreferences {
 	/** */
 	public static final String HISTORY_SHOW_TAG_SEQUENCE = "HistoryView_ShowTagSequence"; //$NON-NLS-1$
 	/** */
-	public static final String STAGING_SHOW_NEW_COMMITS = "StagingView_ShowNewCommits"; //$NON-NLS-1$
+	public static final String STAGING_VIEW_SHOW_NEW_COMMITS = "StagingView_ShowNewCommits"; //$NON-NLS-1$
+	/** */
+	public static final String STAGING_VIEW_COLUMN_LAYOUT = "StagingView_ColumnLayout"; //$NON-NLS-1$
 	/** */
 	public static final String STAGING_VIEW_SYNC_SELECTION = "StagingView_SyncWithSelection"; //$NON-NLS-1$
+	/** */
+	public static final String STAGING_VIEW_FILENAME_MODE = "StagingView_FileNameMode"; //$NON-NLS-1$
 	/** */
 	public static final String PAGE_COMMIT_PREFERENCES = "org.eclipse.egit.ui.internal.preferences.CommitDialogPreferencePage"; //$NON-NLS-1$
 	/** */
