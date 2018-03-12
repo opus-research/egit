5beb786a874b3546c079f4fb379f02f10eb08b35
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:14:51 +0000

    First commited as before_813_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -301,9 +301,6 @@ public class UIText extends NLS {
 	public static String RepositoryRemotePropertySource_RemotePushUrl_label;
 
 	/** */
-	public static String RepositorySearchDialog_AddGitRepositories;
-
-	/** */
 	public static String RepositorySearchDialog_DeepSearch_button;
 
 	/** */
@@ -314,21 +311,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RepositorySearchDialog_DeselectAll_button;
-
-	/** */
-	public static String RepositorySearchDialog_DirectoryNotFoundMessage;
-
-	/** */
-	public static String RepositorySearchDialog_Search;
-
-	/** */
-	public static String RepositorySearchDialog_SearchCriteriaGroup;
-
-	/** */
-	public static String RepositorySearchDialog_SearchRecursiveToolTip;
-
-	/** */
-	public static String RepositorySearchDialog_SearchTitle;
 
 	/** */
 	public static String RepositorySearchDialog_SelectAll_button;
@@ -358,19 +340,13 @@ public class UIText extends NLS {
 	public static String RepositorySearchDialog_directory;
 
 	/** */
-	public static String RepositorySearchDialog_EnterDirectoryToolTip;
-
-	/** */
 	public static String RepositorySearchDialog_errorOccurred;
 
 	/** */
-	public static String RepositorySearchDialog_NoSearchAvailableMessage;
-
-	/** */
-	public static String RepositorySearchDialog_NothingFoundMessage;
-
-	/** */
 	public static String RepositorySearchDialog_search;
+
+	/** */
+	public static String RepositorySearchDialog_searchRepositories;
 
 	/** */
 	public static String RepositorySearchDialog_searchRepositoriesMessage;
