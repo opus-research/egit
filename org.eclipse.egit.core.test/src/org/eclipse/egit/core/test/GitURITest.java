95b554a977276a7dc06221c3cdecf64b4b062f8f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:07:00 +0000

    First commited as before_28051_rev1

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitURITest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitURITest.java
@@ -38,7 +38,6 @@ public class GitURITest {
 		// expected IAE, " are not allowed in a URI reference
 	}
 
-	@SuppressWarnings("unused")
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidScmUriForCVS() throws Exception {
 		new GitURI(URI.create("scm:cvs:pserver:dev.eclipse.org:/cvsroot/eclipse:org.eclipse.compare"));
