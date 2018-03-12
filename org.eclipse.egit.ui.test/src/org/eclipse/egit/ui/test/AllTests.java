87f244d3200ab27d18896a328ea93f18ef3d4e00
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:15:08 +0000

    First commited as before_874_rev5

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
@@ -8,7 +8,6 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.test;
 
-import org.eclipse.egit.ui.views.synchronize.SynchronizeViewTest;
 import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
 import org.eclipse.egit.ui.wizards.share.SharingWizardTest;
 import org.junit.runner.RunWith;
@@ -16,8 +15,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( { GitCloneWizardTest.class, SharingWizardTest.class,
-		SynchronizeViewTest.class })
+@SuiteClasses({ GitCloneWizardTest.class, SharingWizardTest.class })
 public class AllTests {
 	// empty class, don't need anything here
 }
