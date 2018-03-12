c5243a57caa6659ab881acb56893d16f4d5e0cc6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:18:35 +0000

    First commited as before_1390_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
@@ -11,7 +11,6 @@
 package org.eclipse.egit.ui.internal.history;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,22 +21,18 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.MyersDiff;
 import org.eclipse.jgit.diff.RawText;
-import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.errors.CorruptObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
-import org.eclipse.jgit.errors.LargeObjectException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectLoader;
-import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.util.IO;
 
 class FileDiff {
 
@@ -95,17 +90,11 @@ class FileDiff {
 			return;
 		}
 
-		ObjectReader reader = db.newObjectReader();
-		try {
-			outputEclipseDiff(d, db, reader, diffFmt);
-		} finally {
-			reader.release();
-		}
+		outputEclipseDiff(d, db, diffFmt);
 	}
 
 	private void outputEclipseDiff(final StringBuilder d, final Repository db,
-			final ObjectReader reader, final DiffFormatter diffFmt)
-			throws IOException {
+			final DiffFormatter diffFmt) throws IOException {
 		if (!(getBlobs().length == 2))
 			throw new UnsupportedOperationException(
 					"Not supported yet if the number of parents is different from one"); //$NON-NLS-1$
@@ -126,8 +115,8 @@ class FileDiff {
 			d.append("old mode " + mode1); //$NON-NLS-1$
 			d.append("new mode " + mode2).append("\n"); //$NON-NLS-1$//$NON-NLS-2$
 		}
-		d.append("index ").append(reader.abbreviate(id1).name()). //$NON-NLS-1$
-				append("..").append(reader.abbreviate(id2).name()). //$NON-NLS-1$
+		d.append("index ").append(id1.abbreviate(db, 7).name()). //$NON-NLS-1$
+				append("..").append(id2.abbreviate(db, 7).name()). //$NON-NLS-1$
 				append(mode1.equals(mode2) ? " " + mode1 : "").append("\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		if (id1.equals(ObjectId.zeroId()))
 			d.append("--- /dev/null\n"); //$NON-NLS-1$
@@ -145,8 +134,8 @@ class FileDiff {
 			d.append("\n"); //$NON-NLS-1$
 		}
 
-		final RawText a = getRawText(id1, reader);
-		final RawText b = getRawText(id2, reader);
+		final RawText a = getRawText(id1, db);
+		final RawText b = getRawText(id2, db);
 		final MyersDiff diff = new MyersDiff(a, b);
 		diffFmt.formatEdits(a, b, diff.getEdits());
 	}
@@ -159,25 +148,10 @@ class FileDiff {
 		return resource.getProjectRelativePath().toString();
 	}
 
-	private RawText getRawText(ObjectId id, ObjectReader reader) throws IOException {
+	private RawText getRawText(ObjectId id, Repository db) throws IOException {
 		if (id.equals(ObjectId.zeroId()))
 			return new RawText(new byte[] { });
-		ObjectLoader ldr = reader.open(id);
-		if (!ldr.isLarge())
-			return new RawText(ldr.getCachedBytes());
-
-		long sz = ldr.getSize();
-		if (Integer.MAX_VALUE <= sz)
-			throw new LargeObjectException(id);
-
-		byte[] buf = new byte[(int) sz];
-		InputStream in = ldr.openStream();
-		try {
-			IO.readFully(in, buf, 0, buf.length);
-		} finally {
-			in.close();
-		}
-		return new RawText(buf);
+		return new RawText(db.open(id).getCachedBytes());
 	}
 
 
