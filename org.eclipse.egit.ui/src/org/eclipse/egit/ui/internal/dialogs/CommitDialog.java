8d71f43e69646e5922d750ef1d6ed6619597a42b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:37:27 +0000

    First commited as before_54672_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -23,10 +23,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
@@ -81,7 +78,6 @@ import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.DecoratingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.DecorationOverlayIcon;
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
-import org.eclipse.jface.window.Window;
 import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -1319,7 +1315,6 @@ public class CommitDialog extends TitleAreaDialog {
 		return IProblemDecoratable.SEVERITY_NONE;
 	}
 
-	@SuppressWarnings("boxing")
 	@Override
 	protected void okPressed() {
 		if (!isCommitWithoutFilesAllowed()) {
@@ -1329,25 +1324,6 @@ public class CommitDialog extends TitleAreaDialog {
 
 		if (!commitMessageComponent.checkCommitInfo())
 			return;
-
-		if (!getSeverityItems().isEmpty()) {
-			int warnings = 0;
-			int errors = 0;
-			if ((getSeverityItems().get(IMarker.SEVERITY_WARNING) != null)) {
-				warnings = getSeverityItems().get(IMarker.SEVERITY_WARNING)
-						.size();
-			}
-
-			if (getSeverityItems().get(IMarker.SEVERITY_ERROR) != null) {
-				errors = getSeverityItems().get(IMarker.SEVERITY_ERROR).size();
-			}
-			CommitWarningDialog dialog = new CommitWarningDialog(PlatformUI
-					.getWorkbench().getActiveWorkbenchWindow().getShell(),
-					warnings, errors);
-			if (dialog.open() == Window.CANCEL) {
-				return;
-			}
-		}
 
 		Object[] checkedElements = filesViewer.getCheckedElements();
 		selectedFiles.clear();
@@ -1365,27 +1341,6 @@ public class CommitDialog extends TitleAreaDialog {
 		settings.put(SHOW_UNTRACKED_PREF, showUntracked);
 		CommitMessageHistory.saveCommitHistory(getCommitMessage());
 		super.okPressed();
-	}
-
-	@SuppressWarnings({ "boxing", "serial" })
-	private Map<Integer, List<CommitItem>> getSeverityItems() {
-		Map<Integer, List<CommitItem>> result = new HashMap<>();
-		for (final CommitItem item : items) {
-			if (item.getProblemSeverity() >= IMarker.SEVERITY_WARNING) {
-				if (result.get(item.getProblemSeverity()) == null) {
-					result.put(item.getProblemSeverity(),
-							new ArrayList<CommitItem>() {
-								{
-
-									add(item);
-								}
-							});
-				} else {
-					result.get(item.getProblemSeverity()).add(item);
-				}
-			}
-		}
-		return result;
 	}
 
 	@Override
