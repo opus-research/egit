203e96ac3237a25ad4149c62d3eed2db489105ae
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:46:21 +0000

    First commited as before_3866_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -445,6 +445,9 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_OpenMenuLabel;
 
 	/** */
+	public static String GitHistoryPage_PreferencesLink;
+
+	/** */
 	public static String GitHistoryPage_ProjectType;
 
 	/** */
@@ -665,6 +668,18 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RebaseTargetSelectionDialog_RebaseTitle;
+
+	/** */
+	public static String ReplaceTargetSelectionDialog_ReplaceButton;
+
+	/** */
+	public static String ReplaceTargetSelectionDialog_ReplaceMessage;
+
+	/** */
+	public static String ReplaceTargetSelectionDialog_ReplaceTitle;
+
+	/** */
+	public static String ReplaceTargetSelectionDialog_ReplaceWindowTitle;
 
 	/** */
 	public static String RepositoryPropertySource_EditConfigButton;
@@ -2062,6 +2077,9 @@ public class UIText extends NLS {
 	public static String PushResultTable_columnMode;
 
 	/** */
+	public static String PushResultTable_MesasgeText;
+
+	/** */
 	public static String PushResultTable_statusUnexpected;
 
 	/** */
@@ -3309,6 +3327,15 @@ public class UIText extends NLS {
 
 	/** */
 	public static String RefreshPreferencesPage_RefreshWhenIndexChange;
+
+	/** */
+	public static String RefUpdateElement_CommitCountDecoration;
+
+	/** */
+	public static String RefUpdateElement_CommitRangeDecoration;
+
+	/** */
+	public static String RefUpdateElement_UrisDecoration;
 
 	/** */
 	public static String RemoteSelectionCombo_destinationName;
