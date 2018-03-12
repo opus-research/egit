d2317a65aee4edaab578b14ee4aba92dffa435ca
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:25:35 +0000

    First commited as before_8852_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CompareWithWorkingTreeHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CompareWithWorkingTreeHandler.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
  * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
- * Copyright (C) 2012, Gunnar Wagenknecht <gunnar@wagenknecht.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -21,7 +20,6 @@ import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
 import org.eclipse.egit.ui.internal.history.GitHistoryPage;
-import org.eclipse.egit.ui.internal.synchronize.GitModelSynchronize;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
@@ -50,40 +48,28 @@ public class CompareWithWorkingTreeHandler extends
 			}
 			if (input instanceof IFile) {
 				IFile file = (IFile) input;
-				if (CompareUtils.canDirectlyOpenInCompare(file)) {
-					final RepositoryMapping mapping = RepositoryMapping
-							.getMapping(file.getProject());
-					final String gitPath = mapping.getRepoRelativePath(file);
-					ITypedElement right = CompareUtils.getFileRevisionTypedElement(
-							gitPath, commit, mapping.getRepository());
-					final ITypedElement ancestor = CompareUtils.
-							getFileRevisionTypedElementForCommonAncestor(
-							gitPath, headCommit, commit, repo);
+				final RepositoryMapping mapping = RepositoryMapping
+						.getMapping(file.getProject());
+				final String gitPath = mapping.getRepoRelativePath(file);
+				ITypedElement right = CompareUtils.getFileRevisionTypedElement(
+						gitPath, commit, mapping.getRepository());
+				final ITypedElement ancestor = CompareUtils.
+						getFileRevisionTypedElementForCommonAncestor(
+						gitPath, headCommit, commit, repo);
 
-					final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
-							SaveableCompareEditorInput.createFileElement(file),
-							right, ancestor, null);
-					openInCompare(event, in);
-				} else {
-					try {
-						GitModelSynchronize.synchronizeModelWithWorkspace(file,
-								repo, commit.getName());
-					} catch (IOException e) {
-						throw new ExecutionException(e.getMessage(), e);
-					}
-				}
+				final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
+						SaveableCompareEditorInput.createFileElement(file),
+						right, ancestor, null);
+				openInCompare(event, in);
 			}
 			if (input instanceof File) {
 				File file = (File) input;
 				// TODO can we create a ITypedElement from the local file?
 				RevCommit leftCommit;
-				RevWalk walk = new RevWalk(repo);
 				try {
-					leftCommit = walk.parseCommit(headCommit);
+					leftCommit = new RevWalk(repo).parseCommit(headCommit);
 				} catch (Exception e) {
 					throw new ExecutionException(e.getMessage(), e);
-				} finally {
-					walk.release();
 				}
 				final String gitPath = getRepoRelativePath(repo, file);
 				ITypedElement left = CompareUtils.getFileRevisionTypedElement(
