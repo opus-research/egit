4ce2688b716e08c7e78f9e27735bf2eca9d7676e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:20:45 +0000

    First commited as before_1725_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1045,24 +1045,6 @@ public class UIText extends NLS {
 	public static String HistoryPreferencePage_title;
 
 	/** */
-	public static String PullCurrentBranchActionHandler_PullCanceledMessage;
-
-	/** */
-	public static String PullCurrentBranchActionHandler_PullCanceledTitle;
-
-	/** */
-	public static String PullCurrentBranchActionHandler_PullJobname;
-
-	/** */
-	public static String PullResultDialog_DialogTitle;
-
-	/** */
-	public static String PullResultDialog_FetchResultGroupHeader;
-
-	/** */
-	public static String PullResultDialog_MergeResultGroupHeader;
-
-	/** */
 	public static String PushAction_wrongURIDescription;
 
 	/** */
@@ -2215,12 +2197,6 @@ public class UIText extends NLS {
 	public static String CreateTagDialog_CreateTagOnCommitTitle;
 
 	/** */
-	public static String CreateTagDialog_GetTagJobName;
-
-	/** */
-	public static String CreateTagDialog_LoadingMessageText;
-
-	/** */
 	public static String CreateTagDialog_Message;
 
 	/** */
@@ -2253,12 +2229,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitActionHandler_repository;
-
-	/** */
-	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
-
-	/** */
-	public static String CommitFileDiffViewer_MergeCommitMultiAncestorMessage;
 
 	/** */
 	public static String CommitFileDiffViewer_notContainedInCommit;
