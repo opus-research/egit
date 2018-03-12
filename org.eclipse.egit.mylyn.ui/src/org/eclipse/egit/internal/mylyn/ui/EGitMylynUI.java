949ea0375854e018f0c1d0f78402c04c404ee10a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:30:32 +0000

    First commited as before_2609_rev6

--- org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/EGitMylynUI.java
+++ org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/EGitMylynUI.java
@@ -24,18 +24,19 @@ public class EGitMylynUI extends AbstractUIPlugin {
 	// The shared instance
 	private static EGitMylynUI plugin;
 
-//	private static ITaskActivationListener listener;
-//	private static ITaskActivityManager manager;
-
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-
-//		manager = TasksUi.getTaskActivityManager();
-//		listener = new TaskActivationListener();
-//		manager.addActivationListener(listener);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
