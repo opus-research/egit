64ea9451bd7002b3052046f20e77307017f65802
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:13:53 +0000

    First commited as before_632_rev5

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitTestCase.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitTestCase.java
@@ -9,20 +9,11 @@
 package org.eclipse.egit.core.test;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jgit.junit.MockSystemReader;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectWriter;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.Tree;
-import org.eclipse.jgit.util.IO;
 import org.eclipse.jgit.util.SystemReader;
 import org.junit.After;
 import org.junit.Before;
@@ -41,9 +32,8 @@ public abstract class GitTestCase {
 				ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile()
 						.getAbsoluteFile().toString());
 		project = new TestProject(true);
-		gitDir = new File(project.getProject().getWorkspace()
-				.getRoot().getRawLocation().toFile(),
-				Constants.DOT_GIT);
+		gitDir = new File(project.getProject().getWorkspace().getRoot()
+				.getRawLocation().toFile(), Constants.DOT_GIT);
 		rmrf(gitDir);
 	}
 
@@ -53,16 +43,7 @@ public abstract class GitTestCase {
 		rmrf(gitDir);
 	}
 
-	/**
-	 * Delete the file d recursively. <strong>Be very careful when executing
-	 * this function. It is intended to cleanup between test case, but could
-	 * really destroy anything.</strong>
-	 *
-	 * @param d
-	 *            the directory to delete
-	 * @throws IOException
-	 */
-	protected void rmrf(File d) throws IOException {
+	private void rmrf(File d) throws IOException {
 		if (!d.exists())
 			return;
 
@@ -78,47 +59,6 @@ public abstract class GitTestCase {
 		if (!d.delete())
 			throw new IOException(d + " in use or undeletable");
 		assert !d.exists();
-	}
-
-	protected ObjectId createFile(Repository repository, IProject project, String name, String content) throws IOException {
-		File file = new File(project.getProject().getLocation().toFile(), name);
-		FileWriter fileWriter = new FileWriter(file);
-		fileWriter.write(content);
-		fileWriter.close();
-		ObjectWriter objectWriter = new ObjectWriter(repository);
-		return objectWriter.writeBlob(file);
-	}
-
-	protected ObjectId createFileCorruptShort(Repository repository, IProject project, String name, String content) throws IOException {
-		ObjectId id = createFile(repository, project, name, content);
-		File file = new File(repository.getDirectory(), "objects/" + id.name().substring(0,2) + "/" + id.name().substring(2));
-		byte[] readFully = IO.readFully(file);
-		file.delete();
-		FileOutputStream fileOutputStream = new FileOutputStream(file);
-		byte[] truncatedData = new byte[readFully.length - 1];
-		System.arraycopy(readFully, 0, truncatedData, 0, truncatedData.length);
-		fileOutputStream.write(truncatedData);
-		fileOutputStream.close();
-		return id;
-	}
-
-	protected ObjectId createEmptyTree(Repository repository) throws IOException {
-		ObjectWriter objectWriter = new ObjectWriter(repository);
-		Tree tree = new Tree(repository);
-		return objectWriter.writeTree(tree);
-	}
-
-	protected String slurpAndClose(InputStream inputStream) throws IOException {
-		StringBuilder stringBuilder = new StringBuilder();
-		try {
-			int ch;
-			while ((ch = inputStream.read()) != -1) {
-				stringBuilder.append((char)ch);
-			}
-		} finally {
-			inputStream.close();
-		}
-		return stringBuilder.toString();
 	}
 
 }
