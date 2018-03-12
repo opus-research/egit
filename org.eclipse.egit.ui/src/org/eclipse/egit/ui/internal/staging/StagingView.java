f1a4d80fe054ec9eb084dd672214ec207a648a2f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:42:53 +0000

    First commited as before_3670_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -855,7 +855,7 @@ public class StagingView extends ViewPart {
 			}
 		};
 
-		job.setUser(false);
+		job.setUser(true);
 		job.setRule(ResourcesPlugin.getWorkspace().getRoot());
 
 		job.addJobChangeListener(new JobChangeAdapter() {
@@ -985,8 +985,8 @@ public class StagingView extends ViewPart {
 	}
 
 	private void addHeadChangedWarning(String commitMessage) {
-		String message = UIText.StagingView_headCommitChanged + Text.DELIMITER
-				+ Text.DELIMITER + commitMessage;
+		String message = UIText.StagingView_headCommitChanged + "\n\n" + //$NON-NLS-1$
+				commitMessage;
 		commitMessageComponent.setCommitMessage(message);
 	}
 
