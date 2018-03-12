3fa93a2ecb65dc4d224fb2af1f92e31281ba1a07
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:48:04 +0000

    First commited as before_3993_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushResultTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushResultTable.java
@@ -183,7 +183,8 @@ class PushResultTable {
 				.applyTo(messageGroup);
 
 		final SpellcheckableMessageArea text = new SpellcheckableMessageArea(
-				messageGroup, EMPTY_STRING, true, SWT.BORDER) {
+				messageGroup, EMPTY_STRING, true, SWT.MULTI | SWT.BORDER | SWT.WRAP
+						| SWT.V_SCROLL) {
 
 			protected void createMarginPainter() {
 				// Disabled intentionally
