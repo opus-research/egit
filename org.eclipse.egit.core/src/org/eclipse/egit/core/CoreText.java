25ed0cdb2cdfdf33f16e67f8c4cecd54d330b391
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:32:30 +0000

    First commited as before_10269_rev4

--- org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -26,16 +26,10 @@ public class CoreText extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.egit.core.coretext"; //$NON-NLS-1$
 
 	/** */
-	public static String Activator_autoIgnoreDerivedResources;
-
-	/** */
 	public static String Activator_AutoShareJobName;
 
 	/** */
 	public static String Activator_AutoSharingFailed;
-
-	/** */
-	public static String Activator_ignoreResourceFailed;
 
 	/** */
 	public static String Activator_ReconfigureWindowCacheError;
