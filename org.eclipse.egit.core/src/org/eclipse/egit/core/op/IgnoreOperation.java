f9622666cb0849f0caba74b6112a455320d1aa68
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 23:43:41 +0000

    First commited as before_60443_rev3

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
@@ -38,6 +38,7 @@ import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.core.internal.CoreText;
 import org.eclipse.egit.core.internal.job.RuleUtil;
 import org.eclipse.egit.core.internal.util.ResourceUtil;
+import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.osgi.util.NLS;
@@ -143,15 +144,16 @@ public class IgnoreOperation implements IEGitOperation {
 		String entry = b.toString();
 
 		if (container == null || container instanceof IWorkspaceRoot) {
-			Repository repository = Activator.getDefault().getRepositoryCache()
-					.getRepository(path);
-			if (repository == null || repository.isBare()) {
+			RepositoryMapping mapping = RepositoryMapping.getMapping(
+					path);
+			if (mapping == null) {
 				String message = NLS.bind(
 						CoreText.IgnoreOperation_parentOutsideRepo,
 						path.toOSString(), null);
 				IStatus status = Activator.error(message, null);
 				throw new CoreException(status);
 			}
+			Repository repository = mapping.getRepository();
 			// .gitignore is not accessible as resource
 			IPath gitIgnorePath = parent.append(Constants.GITIGNORE_FILENAME);
 			IPath repoPath = new Path(repository.getWorkTree()
