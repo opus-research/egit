95522aab210cb5db834431b91c137beb9a4f2906
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:39:42 +0000

    First commited as before_13019_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -2661,9 +2661,6 @@ public class UIText extends NLS {
 	public static String FileDiffContentProvider_errorGettingDifference;
 
 	/** */
-	public static String FileDiffLabelProvider_RenamedFromToolTip;
-
-	/** */
 	public static String FileRevisionEditorInput_NameAndRevisionTitle;
 
 	/** */
