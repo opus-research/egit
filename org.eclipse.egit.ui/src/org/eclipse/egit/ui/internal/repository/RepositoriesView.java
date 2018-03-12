bc573a6147d7e2752911646f3689fa48dc58d0d1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:13:02 +0000

    First commited as before_519_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
@@ -824,7 +824,7 @@ public class RepositoriesView extends ViewPart implements ISelectionProvider {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				Wizard wiz = new ExternalProjectImportWizard() {
+				Wizard wiz = new ExternalProjectImportWizard(path) {
 
 					@Override
 					public void addPages() {
