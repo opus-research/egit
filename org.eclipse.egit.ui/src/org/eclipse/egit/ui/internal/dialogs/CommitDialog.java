728ecfe0edc7d9b04acc86c18c41d38e90e0150b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:14:57 +0000

    First commited as before_829_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -117,7 +117,7 @@ public class CommitDialog extends Dialog {
 
 	}
 
-	class CommitLabelProvider extends WorkbenchLabelProvider implements
+	static class CommitLabelProvider extends WorkbenchLabelProvider implements
 			ITableLabelProvider {
 		public String getColumnText(Object obj, int columnIndex) {
 			CommitItem item = (CommitItem) obj;
@@ -385,6 +385,7 @@ public class CommitDialog extends Dialog {
 			}
 		}
 
+		applyDialogFont(container);
 		container.pack();
 		return container;
 	}
