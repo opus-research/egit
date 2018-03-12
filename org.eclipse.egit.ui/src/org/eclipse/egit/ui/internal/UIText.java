8909450a8d57c2e4589f49071bb9d2f3d3b5183f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:34:27 +0000

    First commited as before_11190_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1806,9 +1806,6 @@ public class UIText extends NLS {
 	public static String PullResultDialog_RebaseStatusLabel;
 
 	/** */
-	public static String PullResultDialog_RebaseStoppedMessage;
-
-	/** */
 	public static String PushAction_wrongURIDescription;
 
 	/** */
@@ -2557,6 +2554,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String FetchGerritChangePage_ProvideRefNameMessage;
+
+	/** */
+	public static String FetchGerritChangePage_RunInBackground;
 
 	/** */
 	public static String FetchGerritChangePage_SuggestedRefNamePattern;
@@ -3430,6 +3430,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String MergeResultDialog_id;
+
+	/** */
+	public static String MergeResultDialog_conflicts;
 
 	/** */
 	public static String MergeResultDialog_failed;
