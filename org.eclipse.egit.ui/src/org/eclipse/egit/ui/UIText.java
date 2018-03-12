98da46018f45495d38b982e89ed147ac8df2b672
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:55:44 +0000

    First commited as before_4559_rev8

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -307,9 +307,6 @@ public class UIText extends NLS {
 	public static String GitCloneWizard_title;
 
 	/** */
-	public static String GitCloneWizard_jobImportProjects;
-
-	/** */
 	public static String GitCloneWizard_jobName;
 
 	/** */
@@ -1120,9 +1117,6 @@ public class UIText extends NLS {
 	public static String CloneDestinationPage_groupConfiguration;
 
 	/** */
-	public static String CloneDestinationPage_groupProjects;
-
-	/** */
 	public static String CloneDestinationPage_promptDirectory;
 
 	/** */
@@ -1148,9 +1142,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CloneDestinationPage_errorRemoteNameRequired;
-
-	/** */
-	public static String CloneDestinationPage_importButton;
 
 	/** */
 	public static String RefContentProposal_blob;
