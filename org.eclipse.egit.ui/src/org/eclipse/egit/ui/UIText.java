f398ac8d13dadc4547d9ab27cea5c726b94a376e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:56:00 +0000

    First commited as before_4568_rev5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2674,18 +2674,6 @@ public class UIText extends NLS {
 	public static String DeleteRepositoryConfirmDialog_DeleteWorkingDirectoryCheckbox;
 
 	/** */
-	public static String DeleteTagCommand_messageConfirmMultipleTag;
-
-	/** */
-	public static String DeleteTagCommand_messageConfirmSingleTag;
-
-	/** */
-	public static String DeleteTagCommand_taskName;
-
-	/** */
-	public static String DeleteTagCommand_titleConfirm;
-
-	/** */
 	public static String IgnoreActionHandler_addToGitignore;
 
 	/** */
