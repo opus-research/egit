d8495807ba9eeae796d93a133d90e801b538a25c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:18:42 +0000

    First commited as before_1413_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelection.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelection.java
@@ -56,12 +56,7 @@ public class RepositorySelection {
 	 */
 	public URIish getURI() {
 		if (isConfigSelected())
-			if (config.getURIs().size() > 0)
-				return config.getURIs().get(0);
-			else if (config.getPushURIs().size() > 0)
-				return config.getPushURIs().get(0);
-			else
-				return null;
+			return config.getURIs().get(0);
 		return uri;
 	}
 
