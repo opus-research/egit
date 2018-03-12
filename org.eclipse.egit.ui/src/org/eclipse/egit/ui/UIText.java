987166035edcda69735ca7ccdd59bede874bc9c8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:38:30 +0000

    First commited as before_3383_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -88,9 +88,6 @@ public class UIText extends NLS {
 	public static String AddToIndexAction_addingFiles;
 
 	/** */
-	public static String AddToIndexCommand_addingFilesFailed;
-
-	/** */
 	public static String AssumeUnchanged_assumeUnchanged;
 
 	/** */
@@ -754,12 +751,6 @@ public class UIText extends NLS {
 	public static String CompareTreeView_WorkspaceVersionText;
 
 	/** */
-	public static String CompareUtils_errorGettingEncoding;
-
-	/** */
-	public static String CompareUtils_errorGettingHeadCommit;
-
-	/** */
 	public static String MergeHandler_SelectBranchMessage;
 
 	/** */
@@ -1313,9 +1304,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ResourceHistory_ShowTagSequence;
-
-	/** */
-	public static String ResourceHistory_toggleRelativeDate;
 
 	/** */
 	public static String ResourceHistory_toggleCommentWrap;
@@ -2954,12 +2942,6 @@ public class UIText extends NLS {
 	public static String CommitEditorPage_LabelParent;
 
 	/** */
-	public static String CommitEditorPage_LabelTags;
-
-	/** */
-	public static String CommitEditorPage_SectionBranches;
-
-	/** */
 	public static String CommitEditorPage_SectionFiles;
 
 	/** */
@@ -2967,18 +2949,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitEditorPage_Title;
-
-	/** */
-	public static String CommitEditorPage_TooltipAuthor;
-
-	/** */
-	public static String CommitEditorPage_TooltipCommitter;
-
-	/** */
-	public static String CommitEditorPage_TooltipSignedOffByAuthor;
-
-	/** */
-	public static String CommitEditorPage_TooltipSignedOffByCommitter;
 
 	/** */
 	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
@@ -3509,54 +3479,6 @@ public class UIText extends NLS {
 	public static String CommitFileDiffViewer_OpenWorkingTreeVersionInEditorMenuLabel;
 
 	/** */
-	public static String CommitResultLabelProvider_SectionAuthor;
-
-	/** */
-	public static String CommitResultLabelProvider_SectionMessage;
-
-	/** */
-	public static String CommitResultLabelProvider_SectionRepository;
-
-	/** */
-	public static String CommitSearchPage_Author;
-
-	/** */
-	public static String CommitSearchPage_CaseSensitive;
-
-	/** */
-	public static String CommitSearchPage_CommitId;
-
-	/** */
-	public static String CommitSearchPage_Committer;
-
-	/** */
-	public static String CommitSearchPage_ContainingText;
-
-	/** */
-	public static String CommitSearchPage_ContainingTextHint;
-
-	/** */
-	public static String CommitSearchPage_Message;
-
-	/** */
-	public static String CommitSearchPage_ParentIds;
-
-	/** */
-	public static String CommitSearchPage_RegularExpression;
-
-	/** */
-	public static String CommitSearchPage_Repositories;
-
-	/** */
-	public static String CommitSearchPage_Scope;
-
-	/** */
-	public static String CommitSearchPage_SearchAllBranches;
-
-	/** */
-	public static String CommitSearchPage_TreeId;
-
-	/** */
 	public static String CommitSearchQuery_Label;
 
 	/** */
@@ -3584,28 +3506,7 @@ public class UIText extends NLS {
 	public static String CommitSelectionDialog_IncompleteListMessage;
 
 	/** */
-	public static String CommitSelectionDialog_LinkSearch;
-
-	/** */
-	public static String CommitSelectionDialog_Message;
-
-	/** */
-	public static String CommitSelectionDialog_SectionMessage;
-
-	/** */
-	public static String CommitSelectionDialog_SectionRepo;
-
-	/** */
-	public static String CommitSelectionDialog_TaskSearching;
-
-	/** */
-	public static String CommitSelectionDialog_Title;
-
-	/** */
 	public static String CommitSelectionDialog_WindowTitle;
-
-	/** */
-	public static String CommitUI_commitFailed;
 
 	/** */
 	public static String EgitUiEditorUtils_openFailed;
@@ -3614,41 +3515,9 @@ public class UIText extends NLS {
 	public static String GitActionContributor_ExpandAll;
 
 	/** */
-	public static String GitActionContributor_Push;
-
-	/** */
-	public static String GitActionContributor_Pull;
-
-	/** */
 	public static String DecoratableResourceHelper_noHead;
 
 	/** */
-	public static String StagingView_UnstagedChanges;
-
-	/** */
-	public static String StagingView_StagedChanges;
-
-	/** */
-	public static String StagingView_CommitMessage;
-
-	/** */
-	public static String StagingView_Committer;
-
-	/** */
-	public static String StagingView_Author;
-
-	/** */
-	public static String StagingView_Ammend_Previous_Commit;
-
-	/** */
-	public static String StagingView_Add_Signed_Off_By;
-
-	/** */
-	public static String StagingView_Add_Change_ID;
-
-	/** */
-	public static String StagingView_Commit;
-
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
 	}
