107af290761c2da77cdaf262bbbe163a8a02bc64
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 21:55:36 +0000

    First commited as before_4557_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/RepositoryUtil.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/RepositoryUtil.java
@@ -282,14 +282,6 @@ public class RepositoryUtil {
 		}
 	}
 
-	private String getPath(File repositoryDir) {
-		try {
-			return repositoryDir.getCanonicalPath();
-		} catch (IOException e) {
-			return repositoryDir.getAbsolutePath();
-		}
-	}
-
 	/**
 	 *
 	 * @param repositoryDir
@@ -305,7 +297,12 @@ public class RepositoryUtil {
 			if (!FileKey.isGitRepository(repositoryDir, FS.DETECTED))
 				throw new IllegalArgumentException();
 
-			String dirString = getPath(repositoryDir);
+			String dirString;
+			try {
+				dirString = repositoryDir.getCanonicalPath();
+			} catch (IOException e) {
+				dirString = repositoryDir.getAbsolutePath();
+			}
 
 			List<String> dirStrings = getConfiguredRepositories();
 			if (dirStrings.contains(dirString)) {
@@ -327,7 +324,12 @@ public class RepositoryUtil {
 	public boolean removeDir(File file) {
 		synchronized (prefs) {
 
-			String dir = getPath(file);
+			String dir;
+			try {
+				dir = file.getCanonicalPath();
+			} catch (IOException e1) {
+				dir = file.getAbsolutePath();
+			}
 
 			Set<String> dirStrings = new HashSet<String>();
 			dirStrings.addAll(getConfiguredRepositories());
@@ -356,26 +358,4 @@ public class RepositoryUtil {
 		}
 	}
 
-	/**
-	 * Does the collection of repository returned by
-	 * {@link #getConfiguredRepositories()} contain the given repository?
-	 *
-	 * @param repository
-	 * @return true if contains repository, false otherwise
-	 */
-	public boolean contains(final Repository repository) {
-		return contains(getPath(repository.getDirectory()));
-	}
-
-	/**
-	 * Does the collection of repository returned by
-	 * {@link #getConfiguredRepositories()} contain the given repository
-	 * directory?
-	 *
-	 * @param repositoryDir
-	 * @return true if contains repository directory, false otherwise
-	 */
-	public boolean contains(final String repositoryDir) {
-		return getConfiguredRepositories().contains(repositoryDir);
-	}
 }
