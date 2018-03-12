26faac798233f481c2474b6fd6947797ce0a1729
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 22:20:30 +0000

    First commited as before_7521_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3934,27 +3934,6 @@ public class UIText extends NLS {
 	public static String RenameBranchOnCommitHandler_SelectBranchDialogTitle;
 
 	/** */
-	public static String RepositoryStatistics_Description;
-
-	/** */
-	public static String RepositoryStatistics_LooseObjects;
-
-	/** */
-	public static String RepositoryStatistics_NrOfObjects;
-
-	/** */
-	public static String RepositoryStatistics_NrOfPackfiles;
-
-	/** */
-	public static String RepositoryStatistics_NrOfRefs;
-
-	/** */
-	public static String RepositoryStatistics_PackedObjects;
-
-	/** */
-	public static String RepositoryStatistics_SpaceNeededOnFilesystem;
-
-	/** */
 	public static String RevertFailureDialog_Message;
 
 	/** */
