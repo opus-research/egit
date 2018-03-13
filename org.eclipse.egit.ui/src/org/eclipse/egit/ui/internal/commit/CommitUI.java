64b1db41811e1212da76261f7b3b5e773056fcb9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:02:19 +0000

    First commited as before_77821_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitUI.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitUI.java
@@ -7,7 +7,6 @@
  * Copyright (C) 2010, Stefan Lay <stefan.lay@sap.com>
  * Copyright (C) 2011, Jens Baumgart <jens.baumgart@sap.com>
  * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
- * Copyright (C) 2016, Thomas Wolf <thomas.wolf@paranor.ch>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -35,7 +34,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.EclipseGitProgressTransformer;
 import org.eclipse.egit.core.IteratorService;
@@ -174,8 +172,7 @@ public class CommitUI  {
 		commitDialog.setAmending(amending);
 		commitDialog.setAmendAllowed(amendAllowed);
 		commitDialog.setFiles(repo, files, indexDiff);
-		commitDialog.setPreselectedFiles(
-				getSelectedFiles(repo, files, selectedResources));
+		commitDialog.setPreselectedFiles(getSelectedFiles());
 		commitDialog.setPreselectAll(preselectAll);
 		commitDialog.setAuthor(commitHelper.getAuthor());
 		commitDialog.setCommitter(commitHelper.getCommitter());
@@ -243,28 +240,19 @@ public class CommitUI  {
 	 * user only selected one folder when the action was performed, if the
 	 * folder contains any files that could be committed, they will be returned.
 	 *
-	 * @param repository
-	 *            being operated on
-	 * @param mayBeCommitted
-	 *            {@link Set} containing all files that may be committed
-	 * @param resourcesSelected
-	 *            the resources currently selected
-	 *
 	 * @return a collection of files that is eligible to be committed based on
 	 *         the user's selection
 	 */
-	public static Set<String> getSelectedFiles(Repository repository,
-			Set<String> mayBeCommitted,
-			IResource[] resourcesSelected) {
+	private Set<String> getSelectedFiles() {
 		Set<String> preselectionCandidates = new LinkedHashSet<>();
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		// iterate through all the files that may be committed
-		for (String fileName : mayBeCommitted) {
-			URI uri = new File(repository.getWorkTree(), fileName).toURI();
+		for (String fileName : files) {
+			URI uri = new File(repo.getWorkTree(), fileName).toURI();
 			IFile[] workspaceFiles = root.findFilesForLocationURI(uri);
 			if (workspaceFiles.length > 0) {
 				IFile file = workspaceFiles[0];
-				for (IResource resource : resourcesSelected) {
+				for (IResource resource : selectedResources) {
 					// if any selected resource contains the file, add it as a
 					// preselection candidate
 					if (resource.contains(file)) {
@@ -274,7 +262,7 @@ public class CommitUI  {
 				}
 			} else {
 				// could be file outside of workspace
-				for (IResource resource : resourcesSelected) {
+				for (IResource resource : selectedResources) {
 					IPath location = resource.getLocation();
 					if(location != null && location.toFile().equals(new File(uri))) {
 						preselectionCandidates.add(fileName);
@@ -285,29 +273,13 @@ public class CommitUI  {
 		return preselectionCandidates;
 	}
 
-	/**
-	 * Calculates a fresh {@link IndexDiff} for the given repository.
-	 *
-	 * @param repository
-	 *            to compute the {@link IndexDiff} for
-	 * @param selectedProjects
-	 *            of the repository; used to get an estimate for the progress
-	 *            monitor; may be empty
-	 * @param monitor
-	 *            for progress reporting and cancellation
-	 * @return the {@link IndexDiff}
-	 * @throws IOException
-	 *             if an error occurred
-	 * @throws OperationCanceledException
-	 *             if the operation was cancelled
-	 */
-	public static IndexDiff getIndexDiff(Repository repository,
-			IProject[] selectedProjects, IProgressMonitor monitor)
-			throws IOException, OperationCanceledException {
-		SubMonitor progress = SubMonitor.convert(monitor,
-				UIText.CommitActionHandler_calculatingChanges, 1000);
+	private void buildIndexHeadDiffList(IProject[] selectedProjects,
+			IProgressMonitor monitor) throws IOException,
+			OperationCanceledException {
+
+		monitor.beginTask(UIText.CommitActionHandler_calculatingChanges, 1000);
 		EclipseGitProgressTransformer jgitMonitor = new EclipseGitProgressTransformer(
-				progress);
+				monitor);
 		CountingVisitor counter = new CountingVisitor();
 		for (IProject p : selectedProjects) {
 			try {
@@ -316,33 +288,22 @@ public class CommitUI  {
 				// ignore
 			}
 		}
-		WorkingTreeIterator it = IteratorService
-				.createInitialIterator(repository);
-		if (it == null) {
-			// Workspace is closed
-			throw new OperationCanceledException();
-		}
-		IndexDiff diff = new IndexDiff(repository, Constants.HEAD, it);
-		diff.diff(jgitMonitor, counter.count, 0,
-				NLS.bind(UIText.CommitActionHandler_repository,
-						repository.getDirectory().getPath()));
-		if (progress.isCanceled()) {
-			throw new OperationCanceledException();
-		}
-		return diff;
-	}
+		WorkingTreeIterator it = IteratorService.createInitialIterator(repo);
+		if (it == null)
+			throw new OperationCanceledException(); // workspace is closed
+		indexDiff = new IndexDiff(repo, Constants.HEAD, it);
+		indexDiff.diff(jgitMonitor, counter.count, 0, NLS.bind(
+				UIText.CommitActionHandler_repository, repo.getDirectory()
+						.getPath()));
 
-	private void buildIndexHeadDiffList(IProject[] selectedProjects,
-			IProgressMonitor monitor) throws IOException,
-			OperationCanceledException {
-
-		indexDiff = getIndexDiff(repo, selectedProjects, monitor);
 		includeList(indexDiff.getAdded(), indexChanges);
 		includeList(indexDiff.getChanged(), indexChanges);
 		includeList(indexDiff.getRemoved(), indexChanges);
 		includeList(indexDiff.getMissing(), notIndexed);
 		includeList(indexDiff.getModified(), notIndexed);
 		includeList(indexDiff.getUntracked(), notTracked);
+		if (monitor.isCanceled())
+			throw new OperationCanceledException();
 		monitor.done();
 	}
 
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/AbstractHistoryCommandHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/AbstractHistoryCommandHandler.java
@@ -81,16 +81,17 @@ abstract class AbstractHistoryCommandHandler extends AbstractHandler {
 		if (input instanceof RepositoryTreeNode) {
 			return ((RepositoryTreeNode) input).getRepository();
 		}
-		Repository repo = AdapterUtils.adapt(input, Repository.class);
-		if (repo != null) {
-			return repo;
-		}
-		IResource resource = AdapterUtils.adaptToAnyResource(input);
+		IResource resource = AdapterUtils.adapt(input, IResource.class);
 		if (resource != null) {
 			Repository repository = ResourceUtil.getRepository(resource);
 			if (repository != null) {
 				return repository;
 			}
+		}
+
+		Repository repo = AdapterUtils.adapt(input, Repository.class);
+		if (repo != null) {
+			return repo;
 		}
 
 		throw new ExecutionException(
