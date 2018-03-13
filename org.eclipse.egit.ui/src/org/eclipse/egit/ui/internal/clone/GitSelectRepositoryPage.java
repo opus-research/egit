6a16612c1c0d46d63e518601332a2f332f5e8cf7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:07:23 +0000

    First commited as before_84152_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitSelectRepositoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitSelectRepositoryPage.java
@@ -231,13 +231,10 @@ public class GitSelectRepositoryPage extends WizardPage {
 				if (RepositoryUtil.PREFS_DIRECTORIES_REL
 						.equals(event.getKey())) {
 					Display display = tv.getControl().getDisplay();
-					display.asyncExec(new Runnable() {
-						@Override
-						public void run() {
-							if (!tv.getControl().isDisposed()) {
-								refreshRepositoryList();
-								checkPage();
-							}
+					display.asyncExec(() -> {
+						if (!tv.getControl().isDisposed()) {
+							refreshRepositoryList();
+							checkPage();
 						}
 					});
 				}
