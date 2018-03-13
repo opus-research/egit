b052af471c0d05635708ba76a80a5b60567f10be
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:17:39 +0000

    First commited as before_95665_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1568,6 +1568,9 @@ public class UIText extends NLS {
 	public static String ReflogView_DateColumnHeader;
 
 	/** */
+	public static String ReflogView_ErrorOnLoad;
+
+	/** */
 	public static String ReflogView_ErrorOnOpenCommit;
 
 	/** */
@@ -2852,6 +2855,9 @@ public class UIText extends NLS {
 	public static String FetchGerritChangePage_CreatingTagTaskName;
 
 	/** */
+	public static String FetchGerritChangePage_FetchingRemoteRefsMessage;
+
+	/** */
 	public static String FetchGerritChangePage_FetchingTaskName;
 
 	/** */
@@ -2876,7 +2882,7 @@ public class UIText extends NLS {
 	public static String FetchGerritChangePage_PageTitle;
 
 	/** */
-	public static String FetchGerritChangePage_RunInBackground;
+	public static String FetchGerritChangePage_ShowingProposalsJobName;
 
 	/** */
 	public static String FetchGerritChangePage_SuggestedRefNamePattern;
@@ -4592,6 +4598,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String NewRepositoryWizard_WizardTitle;
+
+	/** */
+	public static String NonBlockingWizardDialog_BackgroundJobName;
 
 	/** */
 	public static String NonDeletedFilesDialog_NonDeletedFilesMessage;
