f5541d5f308fef0f466979494ba308a172ebd52a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:23:05 +0000

    First commited as before_101120_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1322,12 +1322,6 @@ public class UIText extends NLS {
 	public static String RepositoryAction_multiRepoSelectionTitle;
 
 	/** */
-	public static String RepositoryToolbarAction_label;
-
-	/** */
-	public static String RepositoryToolbarAction_tooltip;
-
-	/** */
 	public static String RepositoryCommit_AuthorDate;
 
 	/** */
@@ -2427,6 +2421,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ConfigurationEditorComponent_NoConfigLocationKnown;
+
+	/** */
+	public static String ConfigurationEditorComponent_NoSectionSubsectionMessage;
 
 	/** */
 	public static String ConfigurationEditorComponent_OpenEditorButton;
@@ -5348,9 +5345,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StagingView_MessageErrors;
-
-	/** */
-	public static String StagingView_LoadJob;
 
 	/** */
 	public static String StashApplyCommand_applyFailed;
