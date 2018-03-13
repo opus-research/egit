e595133ae88fd64e981743fad11ff517c728480b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:12:22 +0000

    First commited as before_89157_rev5

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
@@ -215,9 +215,6 @@ public class CoreText extends NLS {
 	public static String RepositoryPathChecker_errNoURL;
 
 	/** */
-	public static String RepositoryUtil_autoIgnoreMetaData;
-
-	/** */
 	public static String RepositoryUtil_DirectoryIsNotGitDirectory;
 
 	/** */
