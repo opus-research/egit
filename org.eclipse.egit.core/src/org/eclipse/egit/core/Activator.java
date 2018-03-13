227f7e9f57d4c398bb803a6d6acfe93ed18cc04d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:07:47 +0000

    First commited as before_84438_rev1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
@@ -41,7 +41,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IRegistryEventListener;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
@@ -317,9 +316,6 @@ public class Activator extends Plugin implements DebugOptionsListener {
 	 * @since 4.1
 	 */
 	public Collection<MergeStrategyDescriptor> getRegisteredMergeStrategies() {
-		if (mergeStrategyRegistryListener == null) {
-			return Collections.emptyList();
-		}
 		return mergeStrategyRegistryListener.getStrategies();
 	}
 
@@ -361,11 +357,16 @@ public class Activator extends Plugin implements DebugOptionsListener {
 
 	@Override
 	public void stop(final BundleContext context) throws Exception {
-		if (mergeStrategyRegistryListener != null) {
-			Platform.getExtensionRegistry()
-					.removeListener(mergeStrategyRegistryListener);
-			mergeStrategyRegistryListener = null;
-		}
+		GitProjectData.detachFromWorkspace();
+		repositoryCache.clear();
+		repositoryCache = null;
+		indexDiffCache.dispose();
+		indexDiffCache = null;
+		repositoryUtil.dispose();
+		repositoryUtil = null;
+		secureStore = null;
+		super.stop(context);
+		plugin = null;
 		if (preDeleteProjectListener != null) {
 			ResourcesPlugin.getWorkspace().removeResourceChangeListener(preDeleteProjectListener);
 			preDeleteProjectListener = null;
@@ -373,25 +374,13 @@ public class Activator extends Plugin implements DebugOptionsListener {
 		if (ignoreDerivedResourcesListener != null) {
 			ResourcesPlugin.getWorkspace().removeResourceChangeListener(
 					ignoreDerivedResourcesListener);
-			ignoreDerivedResourcesListener.stop();
 			ignoreDerivedResourcesListener = null;
 		}
 		if (shareGitProjectsJob != null) {
 			ResourcesPlugin.getWorkspace().removeResourceChangeListener(
 					shareGitProjectsJob);
-			shareGitProjectsJob.stop();
 			shareGitProjectsJob = null;
 		}
-		GitProjectData.detachFromWorkspace();
-		indexDiffCache.dispose();
-		indexDiffCache = null;
-		repositoryCache.clear();
-		repositoryCache = null;
-		repositoryUtil.dispose();
-		repositoryUtil = null;
-		secureStore = null;
-		super.stop(context);
-		plugin = null;
 	}
 
 	private void registerAutoShareProjects() {
@@ -419,22 +408,6 @@ public class Activator extends Plugin implements DebugOptionsListener {
 			return p.getBoolean(GitCorePreferences.core_autoShareProjects, d
 					.getBoolean(GitCorePreferences.core_autoShareProjects,
 							true));
-		}
-
-		public void stop() {
-			boolean isRunning = !checkProjectsJob.cancel();
-			Job.getJobManager().cancel(JobFamilies.AUTO_SHARE);
-			try {
-				if (isRunning) {
-					checkProjectsJob.join();
-				}
-				Job.getJobManager().join(JobFamilies.AUTO_SHARE,
-						new NullProgressMonitor());
-			} catch (OperationCanceledException e) {
-				// Ignore
-			} catch (InterruptedException e) {
-				logError(e.getLocalizedMessage(), e);
-			}
 		}
 
 		@Override
@@ -523,9 +496,6 @@ public class Activator extends Plugin implements DebugOptionsListener {
 
 			final Map<IProject, File> projects = new HashMap<IProject, File>();
 			for (IProject project : projectsToCheck) {
-				if (monitor.isCanceled()) {
-					return Status.CANCEL_STATUS;
-				}
 				if (project.isAccessible()) {
 					try {
 						visitConnect(project, projects);
@@ -638,17 +608,6 @@ public class Activator extends Plugin implements DebugOptionsListener {
 	private static class IgnoreDerivedResources implements
 			IResourceChangeListener {
 
-		public void stop() {
-			Job.getJobManager().cancel(JobFamilies.AUTO_IGNORE);
-			try {
-				Job.getJobManager().join(JobFamilies.AUTO_IGNORE,
-						new NullProgressMonitor());
-			} catch (OperationCanceledException e) {
-				// Ignore
-			} catch (InterruptedException e) {
-				logError(e.getLocalizedMessage(), e);
-			}
-		}
 
 		@Override
 		public void resourceChanged(IResourceChangeEvent event) {
