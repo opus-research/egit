2d91036a8af783ed217d4eaa39b682de28340dad
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:11:50 +0000

    First commited as before_88545_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/SimpleConfigureFetchDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/SimpleConfigureFetchDialog.java
@@ -556,13 +556,29 @@ public class SimpleConfigureFetchDialog extends TitleAreaDialog {
 				Activator.handleError(e.getMessage(), e, true);
 			}
 			GerritDialogSettings.updateRemoteConfig(repository, config);
-			if (buttonId == OK) {
-				int timeout = Activator.getDefault().getPreferenceStore()
-						.getInt(UIPreferences.REMOTE_CONNECTION_TIMEOUT);
-				FetchOperationUI op = new FetchOperationUI(repository, config,
-						timeout, false);
-				op.start();
-			}
+			if (buttonId == OK)
+				try {
+					new ProgressMonitorDialog(getShell()).run(true, true,
+							new IRunnableWithProgress() {
+								@Override
+								public void run(IProgressMonitor monitor)
+										throws InvocationTargetException,
+										InterruptedException {
+									int timeout = Activator
+											.getDefault()
+											.getPreferenceStore()
+											.getInt(
+													UIPreferences.REMOTE_CONNECTION_TIMEOUT);
+									FetchOperationUI op = new FetchOperationUI(
+											repository, config, timeout, false);
+									op.start();
+								}
+							});
+				} catch (InvocationTargetException e) {
+					Activator.handleError(e.getMessage(), e, true);
+				} catch (InterruptedException e) {
+					Activator.handleError(e.getMessage(), e, true);
+				}
 			okPressed();
 			return;
 		}
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/SimpleConfigurePushDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/SimpleConfigurePushDialog.java
@@ -629,11 +629,25 @@ public class SimpleConfigurePushDialog extends TitleAreaDialog {
 				Activator.handleError(e.getMessage(), e, true);
 			}
 			GerritDialogSettings.updateRemoteConfig(repository, config);
-			if (buttonId == OK) {
-				PushOperationUI op = new PushOperationUI(repository,
-						config.getName(), false);
-				op.start();
-			}
+			if (buttonId == OK)
+				try {
+					new ProgressMonitorDialog(getShell()).run(true, true,
+							new IRunnableWithProgress() {
+								@Override
+								public void run(IProgressMonitor monitor)
+										throws InvocationTargetException,
+										InterruptedException {
+									PushOperationUI op = new PushOperationUI(
+											repository, config.getName(),
+											false);
+									op.start();
+								}
+							});
+				} catch (InvocationTargetException e) {
+					Activator.handleError(e.getMessage(), e, true);
+				} catch (InterruptedException e) {
+					Activator.handleError(e.getMessage(), e, true);
+				}
 			okPressed();
 			return;
 		}
