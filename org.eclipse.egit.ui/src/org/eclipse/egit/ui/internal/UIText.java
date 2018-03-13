98ea33d0cedaee7430cfe730661995932c6d6a33
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:08:17 +0000

    First commited as before_84539_rev4

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -3250,9 +3250,6 @@ public class UIText extends NLS {
 	public static String CommittingPreferencePage_autoStageDeletion;
 
 	/** */
-	public static String CommittingPreferencePage_autoStageMoves;
-
-	/** */
 	public static String CommittingPreferencePage_AutoStageOnCommit;
 
 	/** */
