1440e2c4c08632f729f288baa2f6eeec2133ea1f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:25:16 +0000

    First commited as before_103983_rev2

--- org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/dialogs/InitDialog.java
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/dialogs/InitDialog.java
@@ -147,7 +147,6 @@ public class InitDialog extends TitleAreaDialog {
 		super.create();
 		setTitle(InitDialog_initializeRepository);
 		setMessage(InitDialog_chooseBranchNamesAndPrefixes);
-		getShell().setText(InitDialog_initializeRepository);
 	}
 
 	@Override
