c433baa4afa5a1e5109e3c1f5d15b1c83dd4f62a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:00:22 +0000

    First commited as before_76475_rev1

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -507,10 +507,7 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_FindMenuLabel;
 
 	/** */
-	public static String GitHistoryPage_FindShowTooltip;
-
-	/** */
-	public static String GitHistoryPage_FindHideTooltip;
+	public static String GitHistoryPage_FindTooltip;
 
 	/** */
 	public static String GitHistoryPage_FolderType;
@@ -1963,9 +1960,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String HistoryPage_findbar_find;
-
-	/** */
-	public static String HistoryPage_findbar_find_msg;
 
 	/** */
 	public static String HistoryPage_findbar_next;
@@ -3859,9 +3853,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitSelectRepositoryPage_PageMessage;
-
-	/** */
-	public static String GitSelectRepositoryPage_BareRepositoriesHidden;
 
 	/** */
 	public static String GitSelectRepositoryPage_PageTitle;
