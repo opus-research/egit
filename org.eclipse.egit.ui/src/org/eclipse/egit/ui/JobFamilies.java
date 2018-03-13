0e8aa4fe44832aaecdf0e7f46f43fcf36e801e57
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:22:07 +0000

    First commited as before_100045_rev3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/JobFamilies.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/JobFamilies.java
@@ -216,10 +216,4 @@ public class JobFamilies {
 	 * Stash git job
 	 */
 	public static final Object STASH = new JobFamily(UIIcons.STASH);
-
-	/**
-	 * Staging view reload
-	 */
-	public static final Object STAGING_VIEW_RELOAD = new JobFamily();
-
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1322,12 +1322,6 @@ public class UIText extends NLS {
 	public static String RepositoryAction_multiRepoSelectionTitle;
 
 	/** */
-	public static String RepositoryToolbarAction_label;
-
-	/** */
-	public static String RepositoryToolbarAction_tooltip;
-
-	/** */
 	public static String RepositoryCommit_AuthorDate;
 
 	/** */
