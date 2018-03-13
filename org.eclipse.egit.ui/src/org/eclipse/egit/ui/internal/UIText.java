68c2dc8423e76bcc236ba86ea43fb3a18f5572ef
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:09:26 +0000

    First commited as before_86419_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -193,6 +193,12 @@ public class UIText extends NLS {
 	public static String WizardProjectsImportPage_CreateProjectsTask;
 
 	/** */
+	public static String SecureStoreUtils_errorClearingCredentials;
+
+	/** */
+	public static String SecureStoreUtils_errorReadingCredentials;
+
+	/** */
 	public static String SecureStoreUtils_writingCredentialsFailed;
 
 	/** */
@@ -266,12 +272,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GerritConfigurationPage_UserLabel;
-
-	/** */
-	public static String EGitCredentialsProvider_errorReadingCredentials;
-
-	/** */
-	public static String EGitCredentialsProvider_FailedToClearCredentials;
 
 	/** */
 	public static String EgitUiUtils_CouldNotOpenEditorMessage;
@@ -3247,6 +3247,12 @@ public class UIText extends NLS {
 	public static String CommittingPreferencePage_AlwaysUseStagingView;
 
 	/** */
+	public static String CommittingPreferencePage_autoStageDeletion;
+
+	/** */
+	public static String CommittingPreferencePage_autoStageMoves;
+
+	/** */
 	public static String CommittingPreferencePage_AutoStageOnCommit;
 
 	/** */
@@ -4317,6 +4323,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String StashEditorPage_LabelParent2;
+
+	/** */
+	public static String MultiPageEditorContentOutlinePage_NoOutline;
 
 	/** */
 	public static String Header_contextMenu_copy;
