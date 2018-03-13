5280a6c8ded6b48b03c964eb066c836eaa67d659
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:06:34 +0000

    First commited as before_82977_rev2

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -3869,19 +3869,11 @@ public class StagingView extends ViewPart implements IShowInSource {
 	@Override
 	public void setFocus() {
 		Tree tree = unstagedViewer.getTree();
-		if (tree.getItemCount() > 0 && !shouldGiveFocusToCommitMessage()) {
+		if (tree.getItemCount() > 0) {
 			unstagedViewer.getControl().setFocus();
 			return;
 		}
 		commitMessageText.setFocus();
-	}
-
-	private boolean shouldGiveFocusToCommitMessage() {
-		Tree stagedTree = stagedViewer.getTree();
-		IPreferenceStore uiPreferences = Activator.getDefault()
-				.getPreferenceStore();
-		return uiPreferences.getBoolean(UIPreferences.AUTO_STAGE_ON_COMMIT)
-				&& stagedTree.getItemCount() > 0;
 	}
 
 	@Override
