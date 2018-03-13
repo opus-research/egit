ad8fd4301a8838329cd6683a4e6112c7b933e9c7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 00:25:35 +0000

    First commited as before_104227_rev2

--- org.eclipse.egit.core/src/org/eclipse/egit/core/NetUtil.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/NetUtil.java
@@ -10,7 +10,6 @@ package org.eclipse.egit.core;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
@@ -23,8 +22,6 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.HttpConfig;
-import org.eclipse.jgit.transport.URIish;
 
 /**
  * Networking utilities
@@ -73,21 +70,17 @@ public class NetUtil {
 			HttpURLConnection conn) throws IOException {
 		if ("https".equals(conn.getURL().getProtocol())) { //$NON-NLS-1$
 			HttpsURLConnection httpsConn = (HttpsURLConnection) conn;
-			try {
-				HttpConfig http = new HttpConfig(repo.getConfig(),
-						new URIish(conn.getURL().toString()));
-				if (!http.isSslVerify()) {
+			if (!repo.getConfig().getBoolean("http", "sslVerify", true)) { //$NON-NLS-1$ //$NON-NLS-2$
+				try {
 					SSLContext ctx = SSLContext.getInstance("TLS"); //$NON-NLS-1$
 					ctx.init(null, trustAllCerts, null);
 					httpsConn.setSSLSocketFactory(ctx.getSocketFactory());
 					httpsConn.setHostnameVerifier(trustAllHostNames);
+				} catch (KeyManagementException e) {
+					throw new IOException(e.getMessage());
+				} catch (NoSuchAlgorithmException e) {
+					throw new IOException(e.getMessage());
 				}
-			} catch (KeyManagementException e) {
-				throw new IOException(e.getMessage());
-			} catch (NoSuchAlgorithmException e) {
-				throw new IOException(e.getMessage());
-			} catch (URISyntaxException e) {
-				// Cannot happen.
 			}
 		}
 	}
